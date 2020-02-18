8ebb5d51fb81f62a9cb9a39aaae772f35881963b
Edi Sinovcic <edi.sinovcic@gmail.com>
2020-02-18 20:41:04 +0100

    Updating circleci config

--- build_and_publish_docker_images.sh
+++ build_and_publish_docker_images.sh
@@ -1,2 +1,18 @@
-docker-compose -f docker-compose-build.yaml --no-cache --parallel
+#!/bin/bash
+set -e
+
+if [ "$CIRCLE_BRANCH" == "develop" ]; then 
+    export TAG="latest"
+    echo $TAG
+fi
+
+if [ "$CIRCLE_BRANCH" == "master" ]; then
+    export TAG="stable"; 
+    echo $TAG
+fi
+echo $TAG
+
+
+docker login --username $DOCKER_USER --password $DOCKER_PASS
+docker-compose -f docker-compose-build.yaml build --no-cache --parallel --build-arg TAG=$TAG
 docker-compose -f docker-compose-build.yaml push
