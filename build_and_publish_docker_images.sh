df1efc558a79c2d6e890a0cf3de743e05d3d835e
Edi Sinovcic <edi.sinovcic@gmail.com>
2020-02-19 17:43:26 +0100

    Updating build and publish script

--- build_and_publish_docker_images.sh
+++ build_and_publish_docker_images.sh
@@ -12,6 +12,8 @@ if [ "$CIRCLE_BRANCH" == "master" ]; then
 fi
 echo $TAG
 
+ls -al
+cat build_and_publish_docker_images.sh
 
 docker login --username $DOCKER_USER --password $DOCKER_PASS
 docker-compose -f docker-compose-build.yaml build --no-cache --parallel --build-arg TAG=$TAG
