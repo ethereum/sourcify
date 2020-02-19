e54d9117b28cf2e1b7660f45d3bb48e245aa3d93
cgewecke <christophergewecke@gmail.com>
2020-02-19 09:59:07 -0800

    Normalize server.js indentation (2 spaces)

--- src/server.ts
+++ src/server.ts
@@ -12,8 +12,8 @@ const port = process.env.SERVER_PORT;
 
 app.use(express.static('ui/dist'))
 app.use(fileUpload({
-    limits: { fileSize: 50 * 1024 * 1024 },
-    abortOnLimit: true
+  limits: { fileSize: 50 * 1024 * 1024 },
+  abortOnLimit: true
 }))
 
 app.use(cors())
@@ -25,44 +25,44 @@ app.use('/repository', express.static(repository), serveIndex(repository, {'icon
 /* tslint:enable:no-unused-variable */
 
 app.post('/', (req, res) => {
-    const files = [];
+  const files = [];
 
-    if (req.files && req.files.files) {
+  if (req.files && req.files.files) {
 
-        for (const x in req.files.files) {
+    for (const x in req.files.files) {
 
-            const data = Array.isArray(req.files.files)
-                ? req.files.files[parseInt(x)].data
-                : null;
+      const data = Array.isArray(req.files.files)
+        ? req.files.files[parseInt(x)].data
+        : null;
 
-            if (data) {
-              try {
-                // Note: metadata files are overly stringified;
-                // this `JSON.parse` still returns a string
-                files.push(JSON.parse(data.toString()))
-              } catch (err) {
-                files.push(data.toString())
-              }
-            } else {
-                console.log(`File ${x} invalid!`)
-            }
+      if (data) {
+        try {
+        // Note: metadata files are overly stringified;
+        // this `JSON.parse` still returns a string
+        files.push(JSON.parse(data.toString()))
+        } catch (err) {
+        files.push(data.toString())
         }
-        injector.inject(
-            repository,
-            req.body.chain,
-            req.body.address,
-            files
-        ).then(result => {
-            res.status(200).send({ result })
-        }).catch(err => {
-            console.log(`Error: ${err}`)
-            res.send({ error: err })
-        })
-    } else {
-        const err = new Error('Request missing expected property: "req.files.files"');
-        console.log(err);
-        res.send({ error: err });
+      } else {
+        console.log(`File ${x} invalid!`)
+      }
     }
+    injector.inject(
+      repository,
+      req.body.chain,
+      req.body.address,
+      files
+    ).then(result => {
+      res.status(200).send({ result })
+    }).catch(err => {
+      console.log(`Error: ${err}`)
+      res.send({ error: err })
+    })
+  } else {
+    const err = new Error('Request missing expected property: "req.files.files"');
+    console.log(err);
+    res.send({ error: err });
+  }
 })
 
 app.listen(port, () => console.log(`Injector listening on port ${port}!`))
