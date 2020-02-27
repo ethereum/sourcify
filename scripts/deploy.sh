f3d74080fc010ccef3ad540f14d66da05d174ffc
Edi Sinovcic <edi.sinovcic@gmail.com>
2020-02-27 22:19:27 +0100

    Adding healthcheck to production environment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,22 +7,20 @@ export TAG="$CIRCLE_BRANCH"
 if [ "$CIRCLE_BRANCH" == "staging" ]; then 
     export TAG="latest"
     export REPO_PATH='/opt/source-verify/staging/source-verify/'
-    echo $TAG
 fi
 
 if [ "$CIRCLE_BRANCH" == "master" ]; then
     export TAG="stable"; 
     export REPO_PATH='/opt/source-verify/master/source-verify/'
-    echo $TAG
 fi
 
 echo $TAG
 # Do ssh to server
-ssh -o "Strict&HostKeyChecking no" source-verify@komputing.org "\
+ssh -o "StrictHostKeyChecking no" source-verify@komputing.org "\
 mkdir -p $REPO_PATH && \
 cd $REPO_PATH && \
 curl https://raw.githubusercontent.com/ethereum/source-verify/${CIRCLE_BRANCH}/docker-compose-prod.yaml > docker-compose.yaml && \
 curl https://raw.githubusercontent.com/ethereum/source-verify/${CIRCLE_BRANCH}/.env.${TAG} > .env && \
-docker-compose pull && \
+TAG=$TAG docker-compose pull && \
 echo $TAG && \
-source .env & TAG=$TAG docker-compose up -d"
+source .env && TAG=$TAG docker-compose up -d"
--- scripts/health_check.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ping server/health
-sleep 120
