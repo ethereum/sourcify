63d9c4e83cf938091d080d6ba20788c692e72352
cgewecke <christophergewecke@gmail.com>
2020-03-02 11:44:43 -0800

    Use $TAG

--- scripts/build_and_publish_docker_images.sh
+++ scripts/build_and_publish_docker_images.sh
@@ -6,18 +6,17 @@ export TAG="$CIRCLE_BRANCH"
 
 if [ "$CIRCLE_BRANCH" == "staging" ]; then
     export TAG="latest"
-    cp .env.latest .env
     echo $TAG
 fi
 
 if [ "$CIRCLE_BRANCH" == "master" ]; then
     export TAG="stable";
-    cp .env.build .env
     echo $TAG
 fi
 
 echo $TAG
 
 docker login --username $DOCKER_USER --password $DOCKER_PASS
+cp .env.$TAG .env
 docker-compose -f docker-compose-build.yaml build
 docker-compose -f docker-compose-build.yaml push
