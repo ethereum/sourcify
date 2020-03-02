a85b77947bb03857c255314f2e786c43d008dd83
cgewecke <christophergewecke@gmail.com>
2020-03-02 11:49:14 -0800

    Use branch specific env files when publishing (#68)

--- scripts/build_and_publish_docker_images.sh
+++ scripts/build_and_publish_docker_images.sh
@@ -4,19 +4,19 @@ set -e
 # If not staging and master branch are existing
 export TAG="$CIRCLE_BRANCH"
 
-if [ "$CIRCLE_BRANCH" == "staging" ]; then 
+if [ "$CIRCLE_BRANCH" == "staging" ]; then
     export TAG="latest"
     echo $TAG
 fi
 
 if [ "$CIRCLE_BRANCH" == "master" ]; then
-    export TAG="stable"; 
+    export TAG="stable";
     echo $TAG
 fi
 
 echo $TAG
 
 docker login --username $DOCKER_USER --password $DOCKER_PASS
-cp .env.build .env
+cp .env.${TAG} .env
 docker-compose -f docker-compose-build.yaml build
 docker-compose -f docker-compose-build.yaml push
