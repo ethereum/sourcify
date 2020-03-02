0659df17d74705bdc3f04fd3e7dbecaeefc4c4f0
cgewecke <christophergewecke@gmail.com>
2020-03-02 11:04:41 -0800

    Use different env for staging / master branches

--- scripts/build_and_publish_docker_images.sh
+++ scripts/build_and_publish_docker_images.sh
@@ -4,19 +4,20 @@ set -e
 # If not staging and master branch are existing
 export TAG="$CIRCLE_BRANCH"
 
-if [ "$CIRCLE_BRANCH" == "staging" ]; then 
+if [ "$CIRCLE_BRANCH" == "staging" ]; then
     export TAG="latest"
+    cp .env.latest .env
     echo $TAG
 fi
 
 if [ "$CIRCLE_BRANCH" == "master" ]; then
-    export TAG="stable"; 
+    export TAG="stable";
+    cp .env.build .env
     echo $TAG
 fi
 
 echo $TAG
 
 docker login --username $DOCKER_USER --password $DOCKER_PASS
-cp .env.build .env
 docker-compose -f docker-compose-build.yaml build
 docker-compose -f docker-compose-build.yaml push
