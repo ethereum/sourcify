a7afbfcb0a6db6e844d9bc36993fb5a49eccd780
cgewecke <christophergewecke@gmail.com>
2020-03-24 20:16:42 -0700

    Save partial bytecode matches

--- test/injector.js
+++ test/injector.js
@@ -3,6 +3,7 @@ const ganache = require('ganache-cli');
 const exec = require('child_process').execSync;
 const pify = require('pify');
 const Web3 = require('web3');
+const path = require('path');
 const read = require('fs').readFileSync;
 
 const Simple = require('./sources/pass/simple.js');
@@ -15,13 +16,13 @@ const Injector = require('../src/injector').default;
 
 describe('injector', function(){
   describe('inject', function(){
-    this.timeout(15000);
+    this.timeout(25000);
 
     let server;
     let port = 8545;
     let chain = 'localhost';
     let mockRepo = 'mockRepository';
-    let injector = new Injector({localChainUrl: process.env.LOCALCHAIN_URL, silent: true});
+    let injector;
     let web3;
     let simpleInstance;
     let simpleWithImportInstance;
@@ -38,10 +39,13 @@ describe('injector', function(){
       server = ganache.server();
       await pify(server.listen)(port);
       web3 = new Web3(`http://${chain}:${port}`);
+    })
 
+    beforeEach(async function(){
       simpleInstance = await deployFromArtifact(web3, Simple);
       simpleWithImportInstance = await deployFromArtifact(web3, SimpleWithImport);
       literalInstance = await deployFromArtifact(web3, Literal);
+      injector = new Injector({ localChainUrl: process.env.LOCALCHAIN_URL, silent: true});
     })
 
     // Clean up repository
@@ -99,6 +103,39 @@ describe('injector', function(){
       assert.equal(literalSavedMetadata, literalMetadata);
     });
 
+    it('verifies a contract when deployed & compiled metadata hashes do not match', async function(){
+      const mismatchedMetadata = MismatchedBytecode.compilerOutput.metadata;
+
+      assert.notEqual(
+        simpleMetadata,
+        mismatchedMetadata
+      );
+
+      // Inject Simple with a metadata that specifies solc 0.6.1 instead of 0.6.0
+      // Functional bytecode of both contracts is identical but metadata hashes will be different.
+      await injector.inject(
+        mockRepo,
+        'localhost',
+        [ simpleInstance.options.address ],
+        [
+          simpleSource,
+          mismatchedMetadata
+        ]
+      );
+
+      // Verify metadata was saved, indexed by address under partial_matches
+      const expectedPath = path.join(
+        mockRepo,
+        'partial_matches',
+        chain,
+        simpleInstance.options.address,
+        '/metadata.json'
+      );
+
+      const savedMetadata = read(expectedPath, 'utf-8');
+      assert.equal(savedMetadata, mismatchedMetadata);
+    })
+
     it('errors if metadata is missing', async function(){
       try {
         await injector.inject(
@@ -130,15 +167,14 @@ describe('injector', function(){
     });
 
     it('errors when recompiled bytecode does not match deployed', async function(){
-      const mismatchedSource = MismatchedBytecode.sourceCodes["Simple.sol"];
-      const mismatchedMetadata = MismatchedBytecode.compilerOutput.metadata;
 
+      // Try to match Simple sources/metadata to SimpleWithImport's address
       try {
         await injector.inject(
           mockRepo,
           'localhost',
-          [ simpleInstance.options.address],
-          [ mismatchedMetadata, mismatchedSource ]
+          [ simpleWithImportInstance.options.address],
+          [ simpleMetadata, simpleSource ]
         );
       } catch(err) {
         assert(err.message.includes('Could not match on-chain deployed bytecode'));
