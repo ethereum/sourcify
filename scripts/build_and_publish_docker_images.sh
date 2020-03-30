5b1c4a0b83ca4efe3a59b3a11e528a89179f2cd3
Edi Sinovcic <edi.sinovcic@gmail.com>
2020-03-30 20:05:06 +0200

    Updating environment

--- scripts/build_and_publish_docker_images.sh
+++ scripts/build_and_publish_docker_images.sh
@@ -6,22 +6,20 @@ export TAG="$CIRCLE_BRANCH"
 
 if [ "$CIRCLE_BRANCH" == "staging" ]; then
     export TAG="latest"
-    echo $TAG
 fi
 
 if [ "$CIRCLE_BRANCH" == "master" ]; then
     export TAG="stable";
-    echo $TAG
 fi
 
 if [ "$CIRCLE_BRANCH" == "volumes-read-write-config" ]; then
     export TAG="testing"
-    echo $TAG
 fi
 
 echo $TAG
 
 cd environments && cp .env.$TAG .env
+echo $SERVICE
 
 docker login --username $DOCKER_USER --password $DOCKER_PASS
 docker-compose -f $SERVICE.yaml -f build-$SERVICE.yaml build
