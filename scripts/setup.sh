98732523e99fa710f0d3a59aaf4dce298f900cb9
Edi Sinovcic <edi.sinovcic@gmail.com>
2020-04-01 23:38:03 +0200

    Moving compose command to script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,20 +4,23 @@ mkdir -p $REPO_PATH
 cd $REPO_PATH
 
 if [[ ! -d source-verify ]]; then
-    echo $PWD
     git clone https://github.com/ethereum/source-verify.git
     cd source-verify
     git checkout ${CIRCLE_BRANCH}
 else
-    echo $PWD
     cd source-verify
     git reset --hard origin/${CIRCLE_BRANCH}
 fi
 
-echo $PWD && ls -al && TAG=$TAG ACCESS_KEY=$ACCESS_KEY SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY ./scripts/find_replace.sh
-cd environments
-source .env
-DATABASE_PATH=$DATABASE_PATH REPOSITORY_PATH=$REPOSITORY_PATH ../scripts/prepare.sh 
+if [ "${TAG}" == "stable" ]; then
+    export COMPOSE_COMMAND="COMPOSE_PROJECT_NAME=${TAG}_source-verify docker-compose -f ipfs.yaml -f monitor.yaml -f repository.yaml -f s3.yaml -f server.yaml -f ui.yaml -f localchain.yaml "
+else
+    export COMPOSE_COMMAND="COMPOSE_PROJECT_NAME=${TAG}_source-verify docker-compose -f ipfs.yaml -f monitor.yaml -f repository.yaml -f s3.yaml -f server.yaml -f ui.yaml"
+fi
+
+TAG=$TAG ACCESS_KEY=$ACCESS_KEY SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY ./scripts/find_replace.sh
+source environments/.env
+DATABASE_PATH=$DATABASE_PATH REPOSITORY_PATH=$REPOSITORY_PATH ./scripts/prepare.sh 
 eval ${COMPOSE_COMMAND} pull
 eval ${COMPOSE_COMMAND} up -d
 ../scripts/clear-repo.sh
