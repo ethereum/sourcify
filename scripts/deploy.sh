b869b74fe533230dc3dfab4e5e3d30f2921a68bf
Edi Sinovcic <edi.sinovcic@gmail.com>
2020-04-01 22:36:42 +0200

    Explicitly starting with bash command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,5 +17,5 @@ fi
 
 # Do ssh to server
 ssh -o "StrictHostKeyChecking no" source-verify@komputing.org "\
-curl https://raw.githubusercontent.com/ethereum/source-verify/${CIRCLE_BRANCH}/scripts/setup.sh > setup.sh && chmod +x setup.sh && chown $USER:$USER setup.sh && ls -al && cat setup.sh && \
-REPO_PATH=$REPO_PATH CIRCLE_BRANCH=$CIRCLE_BRANCH TAG=$TAG ACCESS_KEY=$ACCESS_KEY SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY COMPOSE_COMMAND=$COMPOSE_COMMAND ./setup.sh"
+curl https://raw.githubusercontent.com/ethereum/source-verify/${CIRCLE_BRANCH}/scripts/setup.sh > setup.sh && chmod +x setup.sh && chown $USER:$USER setup.sh && ls -al && \
+REPO_PATH=$REPO_PATH CIRCLE_BRANCH=$CIRCLE_BRANCH TAG=$TAG ACCESS_KEY=$ACCESS_KEY SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY COMPOSE_COMMAND=$COMPOSE_COMMAND bash setup.sh"
