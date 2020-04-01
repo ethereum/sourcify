6838f7597e9a327a47a3c3d8d033bf45be96f3f2
Edi Sinovcic <edi.sinovcic@gmail.com>
2020-04-01 20:08:01 +0200

    Updating wrong paths in scripts

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,21 +1,19 @@
-#!/bin/bash
-set -e
-
-mkdir -p $REPO_PATH 
+mkdir -p $REPO_PATH
 cd $REPO_PATH
 
-if [[ -d source-verify ]];
-then 
-    git clone https://github.com/ethereum/source-verify.git 
-    cd source-verify 
-    git checkout ${CIRCLE_BRANCH} 
-else 
-    cd source-verify 
+if [[ ! -d source-verify ]]; then
+    echo $PWD
+    git clone https://github.com/ethereum/source-verify.git
+    cd source-verify
+    git checkout ${CIRCLE_BRANCH}
+else
+    echo $PWD
+    cd source-verify
     git reset --hard origin/${CIRCLE_BRANCH}
 fi
 
-echo $PWD && ls -al && TAG=$TAG ACCESS_KEY=$ACCESS_KEY SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY ./scripts/find_replace.sh 
+echo $PWD && ls -al && TAG=$TAG ACCESS_KEY=$ACCESS_KEY SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY ./scripts/find_replace.sh
 cd environments
-source .env && eval ${COMPOSE_COMMAND} pull 
-source .env && eval ${COMPOSE_COMMAND} up -d 
+source .env && eval ${COMPOSE_COMMAND} pull
+source .env && eval ${COMPOSE_COMMAND} up -d
 ../scripts/clear-repo.sh
