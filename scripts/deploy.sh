07601d2534d7a3ef607abac18c178015c34dd554
Edi Sinovcic <edi.sinovcic@gmail.com>
2020-04-01 23:38:03 +0200

    Moving compose command to script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,20 +6,14 @@ export TAG="$CIRCLE_BRANCH"
 if [ "$CIRCLE_BRANCH" == "staging" ]; then 
     export TAG='latest'
     export REPO_PATH='/opt/source-verify/staging/'
-    TAG='latest'
-    echo $TAG
-    export COMPOSE_COMMAND="COMPOSE_PROJECT_NAME=${TAG}_source-verify docker-compose -f ipfs.yaml -f localchain.yaml -f monitor.yaml -f repository.yaml -f s3.yaml -f server.yaml -f ui.yaml"
-    echo "${COMPOSE_COMMAND}"
 fi
 
 if [ "$CIRCLE_BRANCH" == "master" ]; then
     export TAG='stable' 
     export REPO_PATH='/opt/source-verify/production/'
-    export COMPOSE_COMMAND="COMPOSE_PROJECT_NAME=${TAG}_source-verify docker-compose -f ipfs.yaml -f localchain.yaml -f monitor.yaml -f s3.yaml -f server.yaml -f ui.yaml"
 fi
 
 # Do ssh to server
 ssh -o "StrictHostKeyChecking no" source-verify@komputing.org "\
 curl https://raw.githubusercontent.com/ethereum/source-verify/${CIRCLE_BRANCH}/scripts/setup.sh > setup.sh && chmod +x setup.sh && chown $USER:$USER setup.sh && ls -al && \
-echo '${REPO_PATH}' && echo '${CIRCLE_BRANCH}' && echo '${COMPOSE_COMMAND}' && echo '${TAG}' && \
 REPO_PATH='${REPO_PATH}' CIRCLE_BRANCH='${CIRCLE_BRANCH}' TAG='${TAG}' ACCESS_KEY='${ACCESS_KEY}' SECRET_ACCESS_KEY='${SECRET_ACCESS_KEY}' ./setup.sh"
