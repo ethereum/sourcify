e071414f021b837f02593f85cfc197e89609330a
Edi Sinovcic <edi.sinovcic@gmail.com>
2020-04-01 19:08:12 +0200

    Moving logic to setup.sh

---
+++ scripts/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+mkdir -p $REPO_PATH 
+cd $REPO_PATH
+
+if [[ -d source-verify ]];
+then 
+    git clone https://github.com/ethereum/source-verify.git 
+    cd source-verify 
+    git checkout ${CIRCLE_BRANCH} 
+else 
+    cd source-verify 
+    git reset --hard origin/${CIRCLE_BRANCH}
+fi
+
+echo $PWD && ls -al && TAG=$TAG ACCESS_KEY=$ACCESS_KEY SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY ./scripts/find_replace.sh 
+cd environments
+eval ${COMPOSE_COMMAND} pull 
+eval ${COMPOSE_COMMAND} up -d 
+../scripts/clear-repo.sh
