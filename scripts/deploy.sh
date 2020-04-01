981de364183cfe1fa5c37524c4eb71ee627768ba
Edi Sinovcic <edi.sinovcic@gmail.com>
2020-04-01 20:08:01 +0200

    Updating wrong paths in scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,18 +4,18 @@
 export TAG="$CIRCLE_BRANCH"
 
 if [ "$CIRCLE_BRANCH" == "staging" ]; then 
-    export TAG="latest"
+    export TAG='latest'
     export REPO_PATH='/opt/source-verify/staging/'
     export COMPOSE_COMMAND='COMPOSE_PROJECT_NAME=${TAG}_source-verify docker-compose -f ipfs.yaml -f localchain.yaml -f monitor.yaml -f repository.yaml -f s3.yaml -f server.yaml -f ui.yaml'
 fi
 
 if [ "$CIRCLE_BRANCH" == "master" ]; then
-    export TAG="stable"; 
+    export TAG='stable' 
     export REPO_PATH='/opt/source-verify/production/'
     export COMPOSE_COMMAND='COMPOSE_PROJECT_NAME=${TAG}_source-verify docker-compose -f ipfs.yaml -f localchain.yaml -f monitor.yaml -f s3.yaml -f server.yaml -f ui.yaml'
 fi
 
 # Do ssh to server
 ssh -o "StrictHostKeyChecking no" source-verify@komputing.org "\
-curl https://raw.githubusercontent.com/ethereum/source-verify/${CIRCLE_BRANCH}/scripts/setup.sh > setup.sh && chmod +x ./setup.sh && \
+curl https://raw.githubusercontent.com/ethereum/source-verify/${CIRCLE_BRANCH}/scripts/setup.sh > setup.sh && chmod +x setup.sh && \
 REPO_PATH=$REPO_PATH CIRCLE_BRANCH=$CIRCLE_BRANCH TAG=$TAG ACCESS_KEY=$ACCESS_KEY SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY COMPOSE_COMMAND=$COMPOSE_COMMAND ./setup.sh"
