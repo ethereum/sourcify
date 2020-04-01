cfaed784de70ffe5382447e794718d4cbd036021
Edi Sinovcic <edi.sinovcic@gmail.com>
2020-04-01 19:25:06 +0200

    Moving env loading to setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,6 +16,6 @@ fi
 
 echo $PWD && ls -al && TAG=$TAG ACCESS_KEY=$ACCESS_KEY SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY ./scripts/find_replace.sh 
 cd environments
-eval ${COMPOSE_COMMAND} pull 
-eval ${COMPOSE_COMMAND} up -d 
+source .env && eval ${COMPOSE_COMMAND} pull 
+source .env && eval ${COMPOSE_COMMAND} up -d 
 ../scripts/clear-repo.sh
