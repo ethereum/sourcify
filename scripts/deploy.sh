49cdd5ccb772cfaa504680543c5a095c7d888d17
Edi Sinovcic <edi.sinovcic@gmail.com>
2020-04-01 23:27:10 +0200

    Removing compose command from sending it to script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,11 +15,11 @@ fi
 if [ "$CIRCLE_BRANCH" == "master" ]; then
     export TAG='stable' 
     export REPO_PATH='/opt/source-verify/production/'
-    export COMPOSE_COMMAND="COMPOSE_PROJECT_NAME="${TAG}"_source-verify docker-compose -f ipfs.yaml -f localchain.yaml -f monitor.yaml -f s3.yaml -f server.yaml -f ui.yaml"
+    export COMPOSE_COMMAND="COMPOSE_PROJECT_NAME=${TAG}_source-verify docker-compose -f ipfs.yaml -f localchain.yaml -f monitor.yaml -f s3.yaml -f server.yaml -f ui.yaml"
 fi
 
 # Do ssh to server
 ssh -o "StrictHostKeyChecking no" source-verify@komputing.org "\
 curl https://raw.githubusercontent.com/ethereum/source-verify/${CIRCLE_BRANCH}/scripts/setup.sh > setup.sh && chmod +x setup.sh && chown $USER:$USER setup.sh && ls -al && \
-echo "${REPO_PATH}" && echo "${CIRCLE_BRANCH}" && echo "${COMPOSE_COMMAND}" && echo ${TAG} && \
-REPO_PATH="${REPO_PATH}" CIRCLE_BRANCH="${CIRCLE_BRANCH}" TAG="${TAG}" ACCESS_KEY="${ACCESS_KEY}" SECRET_ACCESS_KEY="${SECRET_ACCESS_KEY}" COMPOSE_COMMAND="${COMPOSE_COMMAND}" ./setup.sh"
+echo '${REPO_PATH}' && echo '${CIRCLE_BRANCH}' && echo '${COMPOSE_COMMAND}' && echo '${TAG}' && \
+REPO_PATH='${REPO_PATH}' CIRCLE_BRANCH='${CIRCLE_BRANCH}' TAG='${TAG}' ACCESS_KEY='${ACCESS_KEY}' SECRET_ACCESS_KEY='${SECRET_ACCESS_KEY}' ./setup.sh"
