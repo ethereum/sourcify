ef32ff87f2977b2bec8d8ea5d8a98f68a9265484
Edi Sinovcic <edi.sinovcic@gmail.com>
2020-04-02 17:52:38 +0200

    Updating login_sync script

--- src/s3sync/login_sync_s3.sh
+++ src/s3sync/login_sync_s3.sh
@@ -2,6 +2,6 @@
 
 source /app/.env
 /usr/local/bin/aws --version
-/usr/local/bin/aws configure set aws_access_key_id $ACCESS_KEY
-/usr/local/bin/aws configure set aws_secret_access_key $SECRET_ACCESS_KEY
+/usr/local/bin/aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+/usr/local/bin/aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 /usr/local/bin/aws s3 sync /app/repository $BUCKET_NAME_STAGING
