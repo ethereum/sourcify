6634acfefcfa1c499e2ec0a8951c88131fde1b8a
Edi Sinovcic <edi.sinovcic@gmail.com>
2020-04-02 17:39:35 +0200

    Fixing aws access key names

--- scripts/find_replace.sh
+++ scripts/find_replace.sh
@@ -5,12 +5,12 @@ echo $TAG
 echo $ACCESS_KEY
 echo $SECRET_ACCESS_KEY
 
-search="ACCESS_KEY=xxx"
-replace="ACCESS_KEY=$ACCESS_KEY"
+search="AWS_ACCESS_KEY_ID=xxx"
+replace="AWS_ACCESS_KEY_ID=$ACCESS_KEY"
 sed -i "s/${search}/${replace}/g" environments/.env.$TAG
 
-search="SECRET_ACCESS_KEY=xxx"
-replace="SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY"
+search="AWS_SECRET_ACCESS_KEY=xxx"
+replace="AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY"
 sed -i "s/${search}/${replace}/g" environments/.env.$TAG
 
 cp environments/.env.$TAG environments/.env
