6cf82ad61c4a02869851c34560ea75cb85f6d80f
Edi <edi.sinovcic@gmail.com>
2020-04-09 18:57:00 +0200

    Updating error handler

--- src/server.ts
+++ src/server.ts
@@ -2,30 +2,30 @@ import express from 'express';
 import serveIndex from 'serve-index';
 import fileUpload from 'express-fileupload';
 import cors from 'cors';
-import Injector from './injector';
+import Injector, {Match} from './injector';
 import Logger from 'bunyan';
-import { errorMiddleware, BadRequest,  InputData,  findInputFiles, sanatizeInputFiles, inject, HttpException } from "./utils";
+import {errorMiddleware, findInputFiles, InputData, sanatizeInputFiles} from "./utils";
 
 const app = express();
 
 let localChainUrl;
 let silent;
-if (process.env.TESTING){
-  localChainUrl = process.env.LOCALCHAIN_URL;
-  silent = true;
+if (process.env.TESTING) {
+    localChainUrl = process.env.LOCALCHAIN_URL;
+    silent = true;
 }
 
 export const injector = new Injector({
-  localChainUrl: localChainUrl,
-  silent: silent
+    localChainUrl: localChainUrl,
+    silent: silent
 });
 
 export const log = Logger.createLogger({
-  name: "Server",
-  streams: [{
-    stream: process.stdout,
-    level: silent ? 'fatal' : 30
-  }]
+    name: "Server",
+    streams: [{
+        stream: process.stdout,
+        level: silent ? 'fatal' : 30
+    }]
 });
 
 const repository = process.env.REPOSITORY_PATH || './repository/';
@@ -36,13 +36,11 @@ app.use(express.static('ui/dist'))
 
 // TODO: 52MB is the max file size - is this right?
 app.use(fileUpload({
-  limits: { fileSize: 50 * 1024 * 1024 },
-  abortOnLimit: true
+    limits: {fileSize: 50 * 1024 * 1024},
+    abortOnLimit: true
 }))
 
 app.use(cors())
-
-app.use(errorMiddleware);
 
 /* tslint:disable:no-unused-variable */
 app.get('/', (req, res) => res.sendFile('ui/dist/index.html'))
@@ -50,29 +48,30 @@ app.get('/health', (req, res) => res.status(200).send('Alive and kicking!'))
 app.use('/repository', express.static(repository), serveIndex(repository, {'icons': true}))
 /* tslint:enable:no-unused-variable */
 
-app.post('/', (req, res) => {
+app.post('/', (req, res, next) => {
 
-  const inputData: InputData = {
-    repository: repository,
-    files: [],
-    addresses: [req.body.address],
-    chain: req.body.chain,
-  }
+    const inputData: InputData = {
+        repository: repository,
+        files: [],
+        addresses: [req.body.address],
+        chain: req.body.chain,
+    }
 
-  inputData.files = sanatizeInputFiles(findInputFiles(req));
+    inputData.files = sanatizeInputFiles(findInputFiles(req));
 
-  const promises: Promise<any>[] = [];
-  promises.push(inject(inputData));
+    const promises: Promise<Match[]>[] = [];
+    promises.push(injector.inject(inputData));
 
-  //This is so we can have multiple parallel injections, logic still has to be completely implemented
-  Promise.all(promises).then((result) => {
-    res.status(200).send(result);
-  }).catch(err => {
-    throw new HttpException(err.message);
-  })
-
+    //This is so we can have multiple parallel injections, logic still has to be completely implemented
+    Promise.all(promises).then((result) => {
+        res.status(200).send({result});
+    }).catch(err => {
+        next(err); // Just forward it to error middelware
+    })
 })
 
-app.listen(port, () => log.info({loc:'[LISTEN]'}, `Injector listening on port ${port}!`))
+app.use(errorMiddleware);
+
+app.listen(port, () => log.info({loc: '[LISTEN]'}, `Injector listening on port ${port}!`))
 
 export default app;
--- src/utils.ts
+++ src/utils.ts
@@ -1,27 +1,27 @@
 import cbor from 'cbor';
 import Web3 from 'web3';
 import Logger from 'bunyan';
-import { NextFunction, Request, Response } from "express";
-import { injector, log } from './server';
+import {NextFunction, Request, Response} from "express";
+import {log} from './server';
 import util from 'util';
-import svc from 'solc-version-manager';
+
 
 const solc: any = require('solc');
 
 declare interface StringMap {
-  [key: string]: string;
+    [key: string]: string;
 }
 
 declare interface ReformattedMetadata {
-  input: any,
-  fileName: string,
-  contractName: string
+    input: any,
+    fileName: string,
+    contractName: string
 }
 
 export interface RecompilationResult {
-  bytecode: string,
-  deployedBytecode: string,
-  metadata: string
+    bytecode: string,
+    deployedBytecode: string,
+    metadata: string
 }
 
 /**
@@ -34,10 +34,10 @@ export interface RecompilationResult {
  * @param  {number[]} bytecode
  * @return {any}
  */
-export function cborDecode(bytecode: number[]) : any {
-  const cborLength : number = bytecode[bytecode.length - 2] * 0x100 + bytecode[bytecode.length - 1];
-  const bytecodeBuffer = Buffer.from(bytecode.slice(bytecode.length - 2 - cborLength, -2));
-  return cbor.decodeFirstSync(bytecodeBuffer);
+export function cborDecode(bytecode: number[]): any {
+    const cborLength: number = bytecode[bytecode.length - 2] * 0x100 + bytecode[bytecode.length - 1];
+    const bytecodeBuffer = Buffer.from(bytecode.slice(bytecode.length - 2 - cborLength, -2));
+    return cbor.decodeFirstSync(bytecodeBuffer);
 }
 
 /**
@@ -46,8 +46,8 @@ export function cborDecode(bytecode: number[]) : any {
  * @param {string} address contract
  */
 export async function getBytecode(web3: Web3, address: string) {
-  address = web3.utils.toChecksumAddress(address);
-  return await web3.eth.getCode(address);
+    address = web3.utils.toChecksumAddress(address);
+    return await web3.eth.getCode(address);
 };
 
 
@@ -57,10 +57,10 @@ export async function getBytecode(web3: Web3, address: string) {
  * @param  {string} bytecode
  * @return {string}          bytecode minus metadata
  */
-export function getBytecodeWithoutMetadata(bytecode: string) : string {
-  // Last 4 chars of bytecode specify byte size of metadata component,
-  const metadataSize = parseInt(bytecode.slice(-4), 16) * 2 + 4;
-  return bytecode.slice(0, bytecode.length - metadataSize);
+export function getBytecodeWithoutMetadata(bytecode: string): string {
+    // Last 4 chars of bytecode specify byte size of metadata component,
+    const metadataSize = parseInt(bytecode.slice(-4), 16) * 2 + 4;
+    return bytecode.slice(0, bytecode.length - metadataSize);
 }
 
 /**
@@ -70,50 +70,50 @@ export function getBytecodeWithoutMetadata(bytecode: string) : string {
  * @return {ReformattedMetadata}
  */
 function reformatMetadata(
-  metadata : any,
-  sources : StringMap,
-  log: Logger
-) : ReformattedMetadata {
+    metadata: any,
+    sources: StringMap,
+    log: Logger
+): ReformattedMetadata {
 
-  const input : any = {};
-  let fileName : string = '';
-  let contractName : string = '';
+    const input: any = {};
+    let fileName: string = '';
+    let contractName: string = '';
 
-  input.settings = metadata.settings;
+    input.settings = metadata.settings;
 
-  for (fileName in metadata.settings.compilationTarget){
-    contractName = metadata.settings.compilationTarget[fileName];
-  }
+    for (fileName in metadata.settings.compilationTarget) {
+        contractName = metadata.settings.compilationTarget[fileName];
+    }
 
-  delete input['settings']['compilationTarget']
+    delete input['settings']['compilationTarget']
 
-  if (contractName == '') {
-    const err = new Error("Could not determine compilation target from metadata.");
-    log.info({loc: '[REFORMAT]', err: err});
-    throw err;
-  }
+    if (contractName == '') {
+        const err = new Error("Could not determine compilation target from metadata.");
+        log.info({loc: '[REFORMAT]', err: err});
+        throw err;
+    }
 
-  input['sources'] = {}
-  for (const source in sources){
-    input.sources[source] = {'content': sources[source]}
-  }
+    input['sources'] = {}
+    for (const source in sources) {
+        input.sources[source] = {'content': sources[source]}
+    }
 
-  input.language = metadata.language
-  input.settings.metadata = input.settings.metadata || {}
-  input.settings.outputSelection = input.settings.outputSelection || {}
-  input.settings.outputSelection[fileName] = input.settings.outputSelection[fileName] || {}
+    input.language = metadata.language
+    input.settings.metadata = input.settings.metadata || {}
+    input.settings.outputSelection = input.settings.outputSelection || {}
+    input.settings.outputSelection[fileName] = input.settings.outputSelection[fileName] || {}
 
-  input.settings.outputSelection[fileName][contractName] = [
-    'evm.bytecode',
-    'evm.deployedBytecode',
-    'metadata'
-  ];
+    input.settings.outputSelection[fileName][contractName] = [
+        'evm.bytecode',
+        'evm.deployedBytecode',
+        'metadata'
+    ];
 
-  return {
-    input: input,
-    fileName: fileName,
-    contractName: contractName
-  }
+    return {
+        input: input,
+        fileName: fileName,
+        contractName: contractName
+    }
 }
 
 /**
@@ -123,165 +123,149 @@ function reformatMetadata(
  * @return {Promise<RecompilationResult>}
  */
 export async function recompile(
-  metadata : any,
-  sources : StringMap,
-  log: Logger
-) : Promise<RecompilationResult> {
+    metadata: any,
+    sources: StringMap,
+    log: Logger
+): Promise<RecompilationResult> {
 
-  const {
-    input,
-    fileName,
-    contractName
-  } = reformatMetadata(metadata, sources, log);
+    const {
+        input,
+        fileName,
+        contractName
+    } = reformatMetadata(metadata, sources, log);
 
-  const version = metadata.compiler.version;
+    const version = metadata.compiler.version;
 
-  log.info(
-    {
-      loc: '[RECOMPILE]',
-      fileName: fileName,
-      contractName: contractName,
-      version: version
-    },
-    'Recompiling'
-  );
-  
-  const solcjs: any = await new Promise((resolve, reject) => {
-    solc.loadRemoteVersion(`v${version}`, (error: Error, soljson: any) => {
-      (error) ? reject(error) : resolve(soljson);
+    log.info(
+        {
+            loc: '[RECOMPILE]',
+            fileName: fileName,
+            contractName: contractName,
+            version: version
+        },
+        'Recompiling'
+    );
+
+    const solcjs: any = await new Promise((resolve, reject) => {
+        solc.loadRemoteVersion(`v${version}`, (error: Error, soljson: any) => {
+            (error) ? reject(error) : resolve(soljson);
+        });
     });
-  });
 
-  const compiled: any = solcjs.compile(JSON.stringify(input));
-  const output = JSON.parse(compiled);
-  const contract : any = output.contracts[fileName][contractName];
+    const compiled: any = solcjs.compile(JSON.stringify(input));
+    const output = JSON.parse(compiled);
+    const contract: any = output.contracts[fileName][contractName];
 
-  return {
-    bytecode: contract.evm.bytecode.object,
-    deployedBytecode: `0x${contract.evm.deployedBytecode.object}`,
-    metadata: contract.metadata.trim()
-  }
+    return {
+        bytecode: contract.evm.bytecode.object,
+        deployedBytecode: `0x${contract.evm.deployedBytecode.object}`,
+        metadata: contract.metadata.trim()
+    }
 }
 
 export type InputData = {
-  repository: string
-  chain: string,
-  addresses: string[],
-  files: string[],
+    repository: string
+    chain: string,
+    addresses: string[],
+    files: string[],
 }
 
 export function findInputFiles(req: Request): any {
-  const inputs: any = [];
+    const inputs: any = [];
 
-  if (req.files && req.files.files) {
+    if (req.files && req.files.files) {
 
-    // Case: <UploadedFile[]>
-    if (Array.isArray(req.files.files)){
-      req.files.files.forEach(file => {
-        inputs.push(file.data)
-      })
-      return inputs;
+        // Case: <UploadedFile[]>
+        if (Array.isArray(req.files.files)) {
+            req.files.files.forEach(file => {
+                inputs.push(file.data)
+            })
+            return inputs;
 
-    // Case: <UploadedFile>
-    } else if (req.files.files["data"]) {
-      inputs.push(req.files.files["data"]);
-      return inputs;
+            // Case: <UploadedFile>
+        } else if (req.files.files["data"]) {
+            inputs.push(req.files.files["data"]);
+            return inputs;
+        }
+
+        // Case: default
+        const msg = `Invalid file(s) detected: ${util.inspect(req.files.files)}`;
+        log.info({loc: '[POST:INVALID_FILE]'}, msg);
+        throw new BadRequest(msg);
     }
 
-    // Case: default
-    const msg = `Invalid file(s) detected: ${util.inspect(req.files.files)}`;
-    log.info({loc:'[POST:INVALID_FILE]'}, msg);
-    throw new BadRequest(msg);
-  }
-
-  const msg = 'Request missing expected property: "req.files.files"';
-  const err = new Error(msg); //TODO: remove
-  log.info({ loc:'[POST:REQUEST_MISFORMAT]', err: err })
-  throw new BadRequest(err.message);
-}
-
-export function sanatizeInputFiles(inputs: any) {
-  const files = [];
-  if (!inputs.files.length){
-    const msg = 'Unable to extract any files. Your request may be misformatted ' +
-                'or missing some contents.';
-
-    const err = new Error(msg);
-    log.info({ loc:'[POST:NO_FILES]', err: err })
-    throw new BadRequest(msg)
-  }
-
-  for (const data of inputs){
-    try {
-      const val = JSON.parse(data.toString());
-      const type = Object.prototype.toString.call(val);
-
-      (type === '[object Object]')
-        ? files.push(JSON.stringify(val))  // JSON formatted metadata
-        : files.push(val);                 // Stringified metadata
-
-    } catch (err) {
-      files.push(data.toString())          // Solidity files
-    }
-
-    return files as string[];
-  }
-}
-
-export async function inject(inputData: InputData) {
-  injector.inject(
-    inputData
-  ).then(result => {
-    return result
-  }).catch(err => {
-    log.info({ loc:'[POST:INJECT_ERROR]', err: err })
-    //res.status(400).send({ error: err.message })
+    const msg = 'Request missing expected property: "req.files.files"';
+    const err = new Error(msg); //TODO: remove
+    log.info({loc: '[POST:REQUEST_MISFORMAT]', err: err})
     throw new BadRequest(err.message);
-  })
+}
+
+export function sanatizeInputFiles(inputs: any): string[] {
+    const files = [];
+    if (!inputs.length) {
+        const msg = 'Unable to extract any files. Your request may be misformatted ' +
+            'or missing some contents.';
+
+        const err = new Error(msg);
+        log.info({loc: '[POST:NO_FILES]', err: err})
+        throw new BadRequest(msg)
+    }
+
+    for (const data of inputs) {
+        try {
+            const val = JSON.parse(data.toString());
+            const type = Object.prototype.toString.call(val);
+
+            (type === '[object Object]')
+                ? files.push(JSON.stringify(val))  // JSON formatted metadata
+                : files.push(val);                 // Stringified metadata
+
+        } catch (err) {
+            files.push(data.toString())          // Solidity files
+        }
+
+    }
+    return files;
 }
 
 //------------------------------------------------------------------------------------------------------
+
+// TODO: implement response middelware that will automatically handle successful and non successful (error) responses
+
 // Errors
 export class HttpException extends Error {
-  status?: number;
-  message: string;
+    status?: number;
+    message: string;
+    name: string;
 
-  constructor(message: string, status?: number) {
-    super(message);
-    this.status = status;
-    this.message = message;
-  }
+    constructor(message: string, name: string, status?: number) {
+        super(message);
+        this.message = message || "Something went wrong";
+        this.name = name || "HttpException";
+        this.status = status || 500;
+    }
 }
 
 export class BadRequest extends HttpException {
-  constructor(message: string){
-    super(message, 401);
-  }
+    constructor(message: string) {
+        super(message, "BadRequest", 401);
+    }
 }
 
 export class NotFound extends HttpException {
-  constructor(message: string) {
-    super(message, 404);
-  }
+    constructor(message: string) {
+        super(message, "NotFound", 404);
+    }
 }
 
+// All Error and HttpException properties
+export function errorMiddleware(error: Error & HttpException, request: Request, response: Response, next: NextFunction) {
+    const status = error.status || 500;
+    const message = error.message || "Something went wrong";
 
-export function errorMiddleware(error: Error, request: Request, response: Response, next: NextFunction) {
-  if (error.message.includes("call exception")) {
-    castError(new NotFound(error.message), request, response, next);
-  } else if (error.message.includes("network does support") || error.message.includes("invalid input argument")) {
-    castError(new BadRequest(error.message), request, response, next);
-  } else {
-    castError(new HttpException(error.message, 500), request, response, next);
-  }
-}
-
-export function castError(error: HttpException, request: Request, response: Response, next: NextFunction) {
-  const status = error.status || 500;
-  const message = error.message || "Something went wrong";
-  response
-    .status(status)
-    .send({
-      message
-    });
+    response
+        .status(status)
+        .send({
+            error: message
+        });
 }
