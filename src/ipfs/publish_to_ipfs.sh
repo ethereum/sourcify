faeb1ecd8ada84f3ae7636b1dc6ef82cea4172a3
Edi <edi.sinovcic@gmail.com>
2020-04-24 13:36:53 +0200

    Updating cron logic

--- src/ipfs/publish_to_ipfs.sh
+++ src/ipfs/publish_to_ipfs.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-
 source /app/.env
-/usr/local/bin/ipfs name publish `/usr/local/bin/ipfs add -Q -r /app/repository`
+echo $PATH
+export PATH=~$PATH:/usr/local/bin:/usr/local/openjdk-11/bin
+echo $PATH
+
+export JAVA_HOME=/usr/local/openjdk-11/
+
+ipfs name publish `ipfs add -Q -r /app/repository`
+/app/source_verify_ens_updater/bin/source_verify_ens_updater "key" /app/repository
