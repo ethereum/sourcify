6f3d75db0baaedb8fb7a115605d34c021ba564bf
Edi <edi.sinovcic@gmail.com>
2020-05-11 12:45:24 +0200

    Moving schema.sql to seeder

--- src/postgres/schema.sql
+++ src/postgres/schema.sql
@@ -1,16 +1,16 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE bytecode (
+CREATE TABLE metadata (
     id uuid PRIMARY KEY,
     address text NOT NULL,
-    creation_bytecode bytea NOT NULL,
-    chain_id integer NOT NULL,
-    block_number integer NOT NULL,
-    deployed_code_hash bytea NOT NULL
+    creationBytecode bytea NOT NULL,
+    chainId integer NOT NULL,
+    blockNumber integer NOT NULL,
+    deployedCodeHash bytea NOT NULL
     );
 
-CREATE INDEX chain_id_address_idx ON bytecode (address, chain_id);
-CREATE INDEX address_idx ON bytecode (address);
-CREATE INDEX chain_dch_cb_idx on bytecode(chain_id, deployed_code_hash, creation_bytecode);
-CREATE INDEX address_dch_cb_idx ON bytecode(address, deployed_code_hash, creation_bytecode);
-CREATE INDEX address_chain_id_idx ON bytecode(address, chain_id)
+CREATE INDEX chain_id_address_idx ON metadata (address, chain_id);
+CREATE INDEX address_idx ON metadata (address);
+CREATE INDEX chain_dch_cb_idx on metadata(chain_id, deployed_code_hash, creation_bytecode);
+CREATE INDEX address_dch_cb_idx ON metadata(address, deployed_code_hash, creation_bytecode);
+CREATE INDEX address_chain_id_idx ON metadata(address, chain_id)
