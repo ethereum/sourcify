b72ab3faaee2c191ecaa0967772711b67e61b0f6
Edi <edi.sinovcic@gmail.com>
2020-05-23 20:37:18 +0200

    Updating chain to id mapper

--- scripts/chain_name_to_id_mapper.sh
+++ scripts/chain_name_to_id_mapper.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
+source ../environments/.env # You can also use some other environment
 
 path="./synced"
 logPath="."
 
-/usr/local/bin/aws s3 sync $BUCKET_NAME_STAGING/repository $path
+mkdir $path
+
+aws s3 sync $BUCKET_NAME_STAGING/repository $path
 
 echo "Mainnet" >> $logPath/sort.log
 echo "########################################"  >> $logPath/sort.log
-cp -urv $path/contract/byChainId/1 $path/contract/mainnet/ $path/contract/1 >> $logPath/sort.log
+cp -urv $path/contract/byChainId/1/* $path/contract/mainnet/* $path/contract/1/ >> $logPath/sort.log
 echo "########################################"  >> $logPath/sort.log
 echo "Ropsten"  >> $logPath/sort.log
 echo "########################################"  >> $logPath/sort.log
-cp -urv $path/contract/byChainId/3 $path/contract/ropsten/ $path/contract/3  >> $logPath/sort.log
+cp -urv $path/contract/byChainId/3/* $path/contract/ropsten/* $path/contract/3/  >> $logPath/sort.log
 echo "########################################"  >> $logPath/sort.log
 echo "Rinkeby"  >> $logPath/sort.log
 echo "########################################"  >> $logPath/sort.log
-cp -urv $path/contract/byChainId/4 $path/contract/rinkeby/ $path/contract/4  >> $logPath/sort.log
+cp -urv $path/contract/byChainId/4/* $path/contract/rinkeby/* $path/contract/4/  >> $logPath/sort.log
 echo "########################################"  >> $logPath/sort.log
 echo "Goerli"  >> $logPath/sort.log
 echo "########################################"  >> $logPath/sort.log
-cp -urv $path/contract/byChainId/5 $path/contract/goerli/ $path/contract/5  >> $logPath/sort.log
+cp -urv $path/contract/byChainId/5/* $path/contract/goerli/* $path/contract/5/  >> $logPath/sort.log
 echo "########################################"  >> $logPath/sort.log
 echo "Kovan"  >> $logPath/sort.log
 echo "########################################"  >> $logPath/sort.log
-cp -urv $path/contract/byChainId/42 $path/contract/kovan/ $path/contract/42  >> $logPath/sort.log
+cp -urv $path/contract/byChainId/42/* $path/contract/kovan/* $path/contract/42/  >> $logPath/sort.log
 echo "########################################"  >> $logPath/sort.log
 
-/usr/local/bin/aws s3 sync $path $BUCKET_NAME_STAGING/repository
+aws s3 sync $path $BUCKET_NAME_STAGING/repository
