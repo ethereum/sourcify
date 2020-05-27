59ea415cc711d31025ff52d6b4b182117fda53a2
Edi <edi.sinovcic@gmail.com>
2020-05-27 17:28:22 +0200

    Removing repository duplicates

--- src/s3sync/login_sync_s3.sh
+++ src/s3sync/login_sync_s3.sh
@@ -4,5 +4,5 @@ source /app/.env
 /usr/local/bin/aws --version
 /usr/local/bin/aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 /usr/local/bin/aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-/usr/local/bin/aws s3 sync $BUCKET_NAME_STAGING /app/repository
-/usr/local/bin/aws s3 sync /app/repository $BUCKET_NAME_STAGING
+/usr/local/bin/aws s3 sync $BUCKET_NAME /app/repository
+/usr/local/bin/aws s3 sync /app/repository $BUCKET_NAME
