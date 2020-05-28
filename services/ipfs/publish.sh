5dbaa26d0a3dc12febfbf5c827a9684346480c25
Edi <edi.sinovcic@gmail.com>
2020-05-28 15:59:15 +0200

    Updating ipfs to version 0.6

---
+++ services/ipfs/publish.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source /app/.env
+export PATH=~$PATH:/usr/local/bin:/usr/local/openjdk-11/bin:/usr/bin
+export JAVA_HOME=/usr/local/openjdk-11/
+
+hash=$(ipfs add -Q -r /app/repository)
+echo $hash
+curl -X POST "https://ipfs.komputing.org/api/v0/pin/add?arg=$hash&recursive=true"
+ipfs name publish $hash
+
+/app/source_verify_ens_updater/bin/source_verify_ens_updater /app/repository
