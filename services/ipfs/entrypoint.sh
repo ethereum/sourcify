74a5930a5784c860b6f615dcfa44bda72b464b5d
Edi <edi.sinovcic@gmail.com>
2020-05-28 15:59:15 +0200

    Updating ipfs to version 0.6

--- services/ipfs/entrypoint.sh
+++ services/ipfs/entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
+ipfs init
+ipfs daemon &
+
 # Start the run once job.
 echo "Docker container has been started"
 
-# Setup a cron schedule
-echo "*/2 * * * * /app/publish_to_ipfs.sh >> /var/log/cron.log 2>&1
-# This extra line makes it a valid cron" > scheduler.txt
-
-crontab scheduler.txt
+crontab cron.job
 cron -f
