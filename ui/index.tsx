7e439aa030f4b9bfaf9bf0ece4ff6cab34037a93
igor-lid <onaj.igor@gmail.com>
2020-07-02 11:01:47 +0200

    add global context, notification component, global reducer, icons

--- ui/index.tsx
+++ ui/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './src/App';
+import {StateProvider} from "./src/state/State";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => (
+    <StateProvider>
+        <App/>
+    </StateProvider>
+)
+
+ReactDOM.render(<Root/>, document.getElementById('root'));
--- ui/src/types.ts
+++ ui/src/types.ts
@@ -1,3 +1,16 @@
+export type GlobalState = {
+    notification: {
+        isActive: boolean,
+        content: any,
+        type?: "default" | "error" | "success"
+    }
+}
+
+export type GlobalStateActions = {
+    type: "SHOW_NOTIFICATION" | "REMOVE_NOTIFICATION",
+    payload?: any
+}
+
 export type VerifierState = {
     address: string,
     files: []
