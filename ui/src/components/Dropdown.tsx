24e93867bbce0ba828c1936a5351d9dc1c623641
igor-lid <onaj.igor@gmail.com>
2020-07-06 10:18:00 +0200

    close dropdown on outside click

--- ui/src/components/Dropdown.tsx
+++ ui/src/components/Dropdown.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import {ChevronDownIcon, ChevronUpIcon} from "./icons";
+import useOnclickOutside from "react-cool-onclickoutside";
 
 type Item = {
     value: string,
@@ -16,35 +17,32 @@ const Dropdown: React.FC<DropdownProps> = ({items, initialValue}) => {
     const [isOpen, setIsOpen] = useState(false);
     const [selectedItem, setSelectedItem] = useState<Item>(initialValue);
 
-    // clicking outside problem
+    const ref = useOnclickOutside(() => setIsOpen(false));
 
-    // Toggler
     const toggleDropdown = () => setIsOpen(!isOpen);
 
-
-    // handle item selection
     const handleOnClick = (item: Item) => {
         setSelectedItem(item);
         setIsOpen(false);
     }
 
-    // Check if item is selected
     const isItemSelected = (item: Item) => {
         return item.id === selectedItem.id ? `dropdown-list__item--selected` : ""
     }
 
-    return(
+    return (
         <div className="dropdown">
-            <div className="dropdown__header" tabIndex={0} role="button" onKeyPress={toggleDropdown} onClick={toggleDropdown}>
+            <div className="dropdown__header" tabIndex={0} role="button" onKeyPress={toggleDropdown}
+                 onClick={toggleDropdown}>
                 <span>{selectedItem.label}</span>
                 <div className="dropdown-header__indicator">
-                    { isOpen ? <ChevronUpIcon /> : <ChevronDownIcon/>}
+                    {isOpen ? <ChevronUpIcon/> : <ChevronDownIcon/>}
                 </div>
             </div>
             {
                 isOpen && (
-                    <ul className="dropdown__list">
-                        { items.map((item: Item) => (
+                    <ul className="dropdown__list" ref={ref}>
+                        {items.map((item: Item) => (
                             <li className={`dropdown-list__item ${isItemSelected(item)}`} key={item.id}>
                                 <button type="button" onClick={() => handleOnClick(item)}>
                                     <span>{item.label}</span>
