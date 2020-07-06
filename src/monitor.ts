e7234e92a87dae885a22bdee0ec7d6a5440f2381
Jakov Buratovic <kraljipo007@gmail.com>
2020-07-06 16:46:18 +0200

    fixed monitor infura url

--- src/monitor.ts
+++ src/monitor.ts
@@ -7,6 +7,9 @@ import { cborDecode, getChainByName, InputData } from './utils';
 import Injector from './injector';
 import { BlockTransactionObject } from 'web3-eth';
 import Logger from 'bunyan';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: process.cwd().concat("/environments/.env") });
 
 const multihashes = require('multihashes');
 const save = outputFileSync;
@@ -119,7 +122,7 @@ export default class Monitor {
       const options = getChainByName(chain)
       const url : string = customChain
         ? customChain.url
-        : options.web3[0];
+        : options.web3[0].replace("${INFURA_ID}", process.env.INFURA_ID);
 
       this.chains[chain] = {
         web3: new Web3(url),
