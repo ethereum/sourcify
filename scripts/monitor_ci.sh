6721d33bbea0aac6ac764a47580e341d33316263
cgewecke <christophergewecke@gmail.com>
2020-07-08 10:50:21 -0700

    Simplify script (with longer timeout)

--- scripts/monitor_ci.sh
+++ scripts/monitor_ci.sh
@@ -5,22 +5,15 @@
 # Install E2E test
 git clone https://github.com/cgewecke/metacoin-source-verify.git
 cd metacoin-source-verify
-cp ../environments/.env .env
-npm install
+yarn
 
 # Publishes sources to IPFS (via Infura) and deploys contracts to Goerli
 # Account key and Infura project ID are Circle CI env variable settings.
 npm run deploy:goerli
 
+# Give monitor a chance to detect and save.
+sleep 300
+
+# Script which verifies repository write
 cd ..
-for i in `seq 1 10`
-do
-    # Give monitor a chance to detect and save.
-    sleep 30
-    # Script which verifies repository write
-    result=$(./scripts/monitor_ci.js)
-    if [[ $result != *"Error"* ]]; then
-        echo $result
-        break
-    fi
-done
+./scripts/monitor_ci.js
