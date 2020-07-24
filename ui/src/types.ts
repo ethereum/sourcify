1208864ff1bafa9f0d1f54bc0b8bdc65b5dffc8c
igor-lid <onaj.igor@gmail.com>
2020-07-24 18:38:35 +0200

    add quick check addresses feature

--- ui/src/types.ts
+++ ui/src/types.ts
@@ -15,10 +15,14 @@ export type VerifierState = {
     loading: boolean,
     address: string,
     chain: any,
-    files: []
+    files: [],
+    incorrectAddresses: object,
+    isValidationError: boolean,
+    verifyAddressLoading: boolean
 }
 
 export type VerifierActions = {
-    type: "SET_ADDRESS" | "SET_CHAIN" | "SET_FILES" | "CLEAR_FILES" | "SET_LOADING",
+    type: "SET_ADDRESS" | "SET_CHAIN" | "SET_FILES" | "CLEAR_FILES" | "SET_LOADING" | "SET_INCORRECT_ADDRESSES" |
+        "SET_IS_VALIDATION_ERROR" | "SET_VERIFY_ADDRESS_LOADING",
     payload?: any
 }
