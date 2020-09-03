d553eebec55d53a532ef36291431bd51a6c00225
Jakov Buratovic <kraljipo007@gmail.com>
2020-09-03 12:04:22 +0200

    monitor test enabled

--- test/monitor.js
+++ test/monitor.js
@@ -1,121 +1,124 @@
-// const assert = require('assert');
-// const ganache = require('ganache-cli');
-// const exec = require('child_process').execSync;
-// const pify = require('pify');
-// const Web3 = require('web3');
-// const IPFS = require('ipfs')
-// const fs = require('fs');
-// const path = require('path');
-// const request = require('request-promise-native')
+process.env.MOCK_REPOSITORY = './mockRepository';
+process.env.MOCK_DATABASE = './mockDatabase';
 
-// const { deployFromArtifact, waitSecs } = require('./helpers/helpers');
-// const SimpleWithImport = require('./sources/pass/simpleWithImport.js');
-// const Monitor = require('../src/monitor/monitor').default;
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const exec = require('child_process').execSync;
+const pify = require('pify');
+const Web3 = require('web3');
+const IPFS = require('ipfs')
+const fs = require('fs');
+const path = require('path');
+const request = require('request-promise-native')
 
-// describe('monitor', function(){
+const { deployFromArtifact, waitSecs } = require('./helpers/helpers');
+const SimpleWithImport = require('./sources/pass/simpleWithImport.js');
+const Monitor = require('../src/monitor/monitor').default;
 
-//   describe('E2E', function(){
-//     let monitor;
-//     let web3;
-//     let server;
-//     let accounts;
-//     let port = 8545;
-//     let ipfs;
-//     let node;
-//     let chain = 'localhost';
-//     let mockRepo = 'mockRepository';
+describe('monitor', function(){
 
-//     before(async function(){
-//       server = ganache.server({blockTime: 1});
-//       await pify(server.listen)(port);
-//       web3 = new Web3(`http://${chain}:${port}`);
-//       accounts = await web3.eth.getAccounts();
-//     });
+  describe('E2E', function(){
+    let monitor;
+    let web3;
+    let server;
+    let accounts;
+    let port = 8545;
+    let ipfs;
+    let node;
+    let chain = 'localhost';
+    let mockRepo = 'mockRepository';
 
-//     // Clean up server
-//     after(async function(){
-//       monitor.stop();
-//       await pify(server.close)();
-//       await ipfs.stop();
+    before(async function(){
+      server = ganache.server({blockTime: 1});
+      await pify(server.listen)(port);
+      web3 = new Web3(`http://${chain}:${port}`);
+      accounts = await web3.eth.getAccounts();
+    });
 
-//       try { exec(`rm -rf ${mockRepo}`) } catch(err) { /*ignore*/ }
-//     });
+    // Clean up server
+    after(async function(){
+      monitor.stop();
+      await pify(server.close)();
+      await ipfs.stop();
 
-//     it('SimpleWithImport: matches deployed to IPFS sources', async function(){
-//       this.timeout(25000);
+      try { exec(`rm -rf ${mockRepo}`) } catch(err) { /*ignore*/ }
+    });
 
-//       const customChain = {
-//         url: `http://${chain}:${port}`,
-//         name: chain
-//       };
+    it('SimpleWithImport: matches deployed to IPFS sources', async function(){
+      this.timeout(25000);
 
-//       ipfs = await IPFS.create({
-//         offline: true,
-//         silent: true
-//       });
+      const customChain = {
+        url: `http://${chain}:${port}`,
+        name: chain
+      };
 
-//       monitor = new Monitor({
-//         blockTime: 1,
-//         ipfsProvider: ipfs,
-//         repository: mockRepo,
-//         silent: true
-//       });
+      ipfs = await IPFS.create({
+        offline: true,
+        silent: true
+      });
 
-//       const chainId = monitor.injector.fileService.getChainByName(chain).chainId.toString();
+      monitor = new Monitor({
+        blockTime: 1,
+        ipfsProvider: ipfs,
+        repository: mockRepo,
+        silent: true
+      });
 
-//       await monitor.start(customChain);
+      const chainId = monitor.injector.fileService.getChainByName(chain).chainId.toString();
 
-//       const sourceAIpfs = await ipfs.add(SimpleWithImport.sourceCodes["SimpleWithImport.sol"]);
-//       const sourceBIpfs = await ipfs.add(SimpleWithImport.sourceCodes["Import.sol"]);
-//       const metadataIpfs = await ipfs.add(SimpleWithImport.compilerOutput.metadata);
+      await monitor.start(customChain);
 
-//       const instance = await deployFromArtifact(web3, SimpleWithImport);
-//       const address = instance.options.address;
+      const sourceAIpfs = await ipfs.add(SimpleWithImport.sourceCodes["SimpleWithImport.sol"]);
+      const sourceBIpfs = await ipfs.add(SimpleWithImport.sourceCodes["Import.sol"]);
+      const metadataIpfs = await ipfs.add(SimpleWithImport.compilerOutput.metadata);
 
-//       await waitSecs(10);
+      const instance = await deployFromArtifact(web3, SimpleWithImport);
+      const address = instance.options.address;
 
-//       // Verify metadata stored
-//       const addressMetadataPath = path.join(mockRepo, 'contracts', 'full_match', chainId, address, 'metadata.json');
-//       const ipfsMetadataPath = path.join(mockRepo, 'ipfs', metadataIpfs[0].path);
+      await waitSecs(10);
 
-//       const addressMetadata = fs.readFileSync(addressMetadataPath, 'utf-8');
-//       const ipfsMetadata = fs.readFileSync(ipfsMetadataPath, 'utf-8');
+      // Verify metadata stored
+      const addressMetadataPath = path.join(mockRepo, 'contracts', 'full_match', chainId, address, 'metadata.json');
+      const ipfsMetadataPath = path.join(mockRepo, 'ipfs', metadataIpfs[0].path);
 
-//       assert.equal(addressMetadata, SimpleWithImport.compilerOutput.metadata);
-//       assert.equal(ipfsMetadata, SimpleWithImport.compilerOutput.metadata);
+      const addressMetadata = fs.readFileSync(addressMetadataPath, 'utf-8');
+      const ipfsMetadata = fs.readFileSync(ipfsMetadataPath, 'utf-8');
 
-//       // Verify source stored
-//       const metadata = JSON.parse(SimpleWithImport.compilerOutput.metadata);
+      assert.equal(addressMetadata, SimpleWithImport.compilerOutput.metadata);
+      assert.equal(ipfsMetadata, SimpleWithImport.compilerOutput.metadata);
 
-//       // Source keys sorted alpha
-//       const importKey = Object.keys(metadata.sources)[0];
-//       const simpleWithImportKey = Object.keys(metadata.sources)[1];
+      // Verify source stored
+      const metadata = JSON.parse(SimpleWithImport.compilerOutput.metadata);
 
-//       const importPath = path.join(
-//         mockRepo,
-//         'contracts',
-//         'full_match',
-//         chainId,
-//         address,
-//         'sources',
-//         importKey
-//       );
+      // Source keys sorted alpha
+      const importKey = Object.keys(metadata.sources)[0];
+      const simpleWithImportKey = Object.keys(metadata.sources)[1];
 
-//       const simpleWithImportPath = path.join(
-//         mockRepo,
-//         'contracts',
-//         'full_match',
-//         chainId,
-//         address,
-//         'sources',
-//         simpleWithImportKey
-//       );
+      const importPath = path.join(
+        mockRepo,
+        'contracts',
+        'full_match',
+        chainId,
+        address,
+        'sources',
+        importKey
+      );
 
-//       const savedImportSource = fs.readFileSync(importPath, 'utf-8');
-//       const savedSimpleWithImportSource = fs.readFileSync(simpleWithImportPath, 'utf-8');
+      const simpleWithImportPath = path.join(
+        mockRepo,
+        'contracts',
+        'full_match',
+        chainId,
+        address,
+        'sources',
+        simpleWithImportKey
+      );
 
-//       assert.equal(savedImportSource, SimpleWithImport.sourceCodes["Import.sol"]);
-//       assert.equal(savedSimpleWithImportSource, SimpleWithImport.sourceCodes["SimpleWithImport.sol"])
-//     });
-//   });
-// });
+      const savedImportSource = fs.readFileSync(importPath, 'utf-8');
+      const savedSimpleWithImportSource = fs.readFileSync(simpleWithImportPath, 'utf-8');
+
+      assert.equal(savedImportSource, SimpleWithImport.sourceCodes["Import.sol"]);
+      assert.equal(savedSimpleWithImportSource, SimpleWithImport.sourceCodes["SimpleWithImport.sol"])
+    });
+  });
+});
