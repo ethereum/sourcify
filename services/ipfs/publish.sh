cc4733f5eb01f350fd57694f1dddba389da6d6d4
Edi Sinovcic <edi.sinovcic@gmail.com>
2020-09-28 22:45:32 +0200

    Adding support for ipfs 0.7 Adding gpg support for ipfs key

--- services/ipfs/publish.sh
+++ services/ipfs/publish.sh
@@ -3,9 +3,12 @@ source /app/.env
 export PATH=~$PATH:/usr/local/bin:/usr/local/openjdk-11/bin:/usr/bin
 export JAVA_HOME=/usr/local/openjdk-11/
 
+ipfs key import main /app/ipfs.key 
+
 hash=$(ipfs add -Q -r /app/repository)
 echo $hash
 curl -X POST "https://ipfs.komputing.org/api/v0/pin/add?arg=$hash&recursive=true"
-ipfs name publish $hash
+ipfs key list
+ipfs name publish --key=main $hash
 
 /app/source_verify_ens_updater/bin/source_verify_ens_updater /app/repository
