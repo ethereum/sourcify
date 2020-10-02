c2894ae234c81f7682edbee756fa399d645776f9
FabijanC <fabijan.corak@gmail.com>
2020-10-02 17:29:23 +0200

    Validation service multiple format support

--- services/validation/src/index.ts
+++ services/validation/src/index.ts
@@ -5,22 +5,69 @@ import path from 'path';
 import { ValidationService, IValidationService } from './ValidationService';
 export * from './ValidationService';
 
+/**
+ * Recursively traverses the structure of the provided file. If a regular file, it is added to the
+ * files array, if a directory, the same procedure is applied to its children.
+ * If a file cannot be read, it is added to the ignoring array.
+ * 
+ * @param fileName file to be added
+ * @param files array where the file will be added
+ * @param ignoring array containing the names of ignored files
+ */
+function attemptAdding(fileName: string, files: Buffer[], ignoring: string[]): void {
+    try {
+        if (fs.lstatSync(fileName).isFile()) {
+            const data = fs.readFileSync(path.resolve(fileName));
+            files.push(data);
+        }
+    } catch (err) {
+        console.log(`Cannot read "${fileName}"; proceeding without it.`);
+        ignoring.push(fileName);
+        return;
+    }
+
+    try {
+        if (fs.lstatSync(fileName).isDirectory()) {
+            const children = fs.readdirSync(fileName);
+            for (const child of children) {
+                const childJoint = path.join(fileName, child);
+                attemptAdding(childJoint, files, ignoring);
+            }
+        }
+    } catch (err) {
+        console.log(`"${fileName}" is neither a file nor a directory; proceeding without it.`);
+        ignoring.push(fileName);
+        return;
+    }
+}
+
 if (process.argv.length > 2) {
     const fileNames = process.argv.slice(2);
 
-    let files: any[] = [];
+    const files: any[] = [];
+    const ignoring: any[] = [];
     fileNames.forEach(fileName => {
-
-        try {
-            files.push({
-                name: fileName,
-                data: fs.readFileSync(path.resolve(fileName))
-            })
-        } catch (error) {
-            throw new Error("Can not read file " + fileName);
-        }
-
+        attemptAdding(fileName, files, ignoring);
     });
-    const validationService: IValidationService = new ValidationService();
-    validationService.checkFiles(files);
+    
+    if (!files.length) {
+        console.log("No valid files to continue with... Exiting.");
+        process.exit(1);
+    }
+
+    const validationService: IValidationService = new ValidationService(null);
+    try {
+        const checkedContracts = validationService.checkFiles(files);
+        checkedContracts.forEach(contract => {
+            console.log(contract.info);
+            console.log("");
+        });
+    } catch (err) {
+        console.log(err.message);
+    }
+
+    if (ignoring.length) {
+        console.log("Files ignored due to readability issues:");
+        ignoring.forEach(ignored => console.log("\t"+ignored));
+    }
 }
