a9e37e3d28f1abc0dd473b7442bb19085bf5b0ff
Jakov Buratovic <kraljipo007@gmail.com>
2021-03-16 10:26:37 +0000

    alchemy_id added to env

--- scripts/find_replace.sh
+++ scripts/find_replace.sh
@@ -8,24 +8,25 @@ gpg --yes --batch --passphrase=$SECRET_KEY ../environments/.env.secrets.gpg
 gpg --yes --batch --passphrase=$SECRET_KEY ../services/ipfs/ipfs-stable.key.gpg
 gpg --yes --batch --passphrase=$SECRET_KEY ../services/ipfs/ipfs-latest.key.gpg
 source ../environments/.env.secrets
-pwd
 
-export TAG="$CIRCLE_BRANCH"
+TAG="$CIRCLE_BRANCH"
 
 if [ "$CIRCLE_BRANCH" == "staging" ]; then
-    export TAG="latest"
-    export INFURA_ID=$INFURA_ID_STAGING
-    export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_STAGING
-    export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_STAGING
-    export IPFS_SECRET=$IPFS_SECRET_STAGING
+    TAG="latest"
+    INFURA_ID=$INFURA_ID_STAGING
+    ALCHEMY_ID=$ALCHEMY_ID_STAGING
+    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_STAGING
+    AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_STAGING
+    IPFS_SECRET=$IPFS_SECRET_STAGING
 fi
 
 if [ "$CIRCLE_BRANCH" == "master" ]; then
-    export TAG="stable";
-    export INFURA_ID=$INFURA_ID_MASTER
-    export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_MASTER
-    export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_MASTER
-    export IPFS_SECRET=$IPFS_SECRET_MASTER
+    TAG="stable";
+    INFURA_ID=$INFURA_ID_MASTER
+    ALCHEMY_ID=$ALCHEMY_ID_MASTER
+    AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_MASTER
+    AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_MASTER
+    IPFS_SECRET=$IPFS_SECRET_MASTER
 fi
 
 for VAR_NAME in INFURA_ID ALCHEMY_ID AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY IPFS_SECRET NPM_TOKEN ENS_SECRET
