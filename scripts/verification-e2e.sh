42892897a9b83471f7c2624f7f22ce8eecc3ba4b
FabijanC <fabijan.corak@gmail.com>
2021-06-10 15:56:42 +0200

    Disable no-address part of verification-e2e

--- scripts/verification-e2e.sh
+++ scripts/verification-e2e.sh
@@ -9,13 +9,14 @@ npm ci
 # Test WITHOUT providing address or chain
 # Deploys contracts to Rinkeby
 # Account key and Infura project ID are Circle CI env variable settings.
-npm run deploy-with-salt:rinkeby || exit 1
-DEPLOYMENT_ADDRESS=$(truffle networks | grep MetaCoinSalted | sed 's/^.*MetaCoinSalted: \\(.*\\).*$/\\1/')
-cd ..
-node scripts/verification-e2e.js $DEPLOYMENT_ADDRESS || exit 2
+# npm run deploy-with-salt:rinkeby || exit 1
+# DEPLOYMENT_ADDRESS=$(truffle networks | grep MetaCoinSalted | sed 's/^.*MetaCoinSalted: \\(.*\\).*$/\\1/')
+# cd ..
+# node scripts/verification-e2e.js $DEPLOYMENT_ADDRESS || exit 2
 
 # Test WITH providing address and chain
-cd metacoin-source-verify
+# TODO Temporarily commented
+# cd metacoin-source-verify
 npm run deploy-with-salt:rinkeby || exit 3
 DEPLOYMENT_ADDRESS=$(truffle networks | grep MetaCoinSalted | sed 's/^.*MetaCoinSalted: \\(.*\\).*$/\\1/')
 cd ..
