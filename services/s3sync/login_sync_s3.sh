54d729af2d20773dfc371ca92bf0ac72c1017e76
Jakov Buratovic <kraljipo007@gmail.com>
2021-06-22 20:43:06 +0100

    Updated .env mounting and deployment scritps

--- services/s3sync/login_sync_s3.sh
+++ services/s3sync/login_sync_s3.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-source /app/.env
 /usr/local/bin/aws --version
 /usr/local/bin/aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 /usr/local/bin/aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
