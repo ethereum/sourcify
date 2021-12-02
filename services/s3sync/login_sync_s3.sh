f9c1e2755c07846543ce17a6be461de55c1405cf
kuzdogan <kaanuzdogan@hotmail.com>
2021-12-02 18:37:18 +0100

    Update S3 sync

--- services/s3sync/login_sync_s3.sh
+++ services/s3sync/login_sync_s3.sh
@@ -3,4 +3,6 @@
 /usr/local/bin/aws --version
 /usr/local/bin/aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 /usr/local/bin/aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-/usr/local/bin/aws s3 sync /app/repository $BUCKET_NAME
+
+echo "Syncing at $BUCKET_NAME/$TAG"
+/usr/local/bin/aws s3 sync /app/repository $BUCKET_NAME/$TAG
