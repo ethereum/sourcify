a560e5fcf15dac326bc46edee590d34900d0c054
kuzdogan <kaanuzdogan@hotmail.com>
2021-12-20 19:37:18 +0100

    Display added files, checked contracts, two col

---
+++ ui-new/src/pages/Verifier/CheckedContractsView.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type CheckedContractsViewProps = {};
+
+const CheckedContractsView: React.FC<CheckedContractsViewProps> = () => {
+  // return null;
+  return (
+    <div className="flex flex-col flex-grow basis-0 bg-ceruleanBlue-10 cursor-pointer mx-4 px-8">
+      <div>Hello</div>
+    </div>
+  );
+};
+
+export default CheckedContractsView;
