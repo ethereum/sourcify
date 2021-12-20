a414091ce9491b8961bd1b49f8297d500ca773c4
kuzdogan <kaanuzdogan@hotmail.com>
2021-12-20 19:37:18 +0100

    Display added files, checked contracts, two col

--- ui-new/src/pages/Verifier/FileUpload.tsx
+++ ui-new/src/pages/Verifier/FileUpload.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { useDropzone } from "react-dropzone";
+import { DropzoneFile, SendableContract } from "../../types";
 
 type FileUploadProps = {
   handleFilesAdded: (files: []) => void;
-  addedFiles: any[];
+  restartSession: () => void;
+  addedFiles: DropzoneFile[];
+  unusedFiles: string[];
+  checkedContracts: SendableContract[];
+  isLoading: boolean;
 };
 
 const FileUpload: React.FC<FileUploadProps> = ({
   handleFilesAdded,
+  restartSession,
   addedFiles,
+  unusedFiles,
+  checkedContracts,
+  isLoading,
 }) => {
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
     onDrop: (acceptedFiles: any): void => {
@@ -19,22 +28,66 @@ const FileUpload: React.FC<FileUploadProps> = ({
 
   const displayFiles = addedFiles.map((file) => {
     return (
-      <li key={file.path}>
-        {file.path} - {file.size} bytes
+      <li key={file.name}>
+        {file.name} - {file.size} bytes
       </li>
     );
   });
   console.log(addedFiles);
   return (
-    <div
-      {...getRootProps()}
-      className="flex flex-col flex-grow bg-ceruleanBlue-10 cursor-pointer"
-    >
-      <div className="flex-grow bg-ceruleanBlue-10 border-dashed border-ceruleanBlue-100 border-2 m-2 rounded-lg hover:bg-ceruleanBlue-70">
-        <input {...getInputProps()} type="file" />
-        <p>Drag 'n' drop some files here, or click to select files</p>
+    <div className="flex flex-col basis-0 flex-grow bg-ceruleanBlue-40 mx-4 px-8">
+      <div className="my-6 flex justify-center items-center">
+        <p>Drag and drop the Solidity and metadata files below</p>
       </div>
-      <div className="flex flex-col">{displayFiles}</div>
+      <div className="flex justify-end">
+        <button onClick={restartSession}>Clear Files</button>
+      </div>
+      <div
+        {...getRootProps()}
+        className="flex flex-col flex-grow bg-ceruleanBlue-10 cursor-pointer"
+      >
+        <div className="flex-grow bg-ceruleanBlue-10 border-dashed border-ceruleanBlue-100 border-2 m-2 rounded-lg hover:bg-ceruleanBlue-70">
+          <input {...getInputProps()} type="file" />
+          <div className="font-bold text-lg">Added Files</div>
+          <div className="flex flex-col">{displayFiles}</div>
+          {isLoading && (
+            <div className="flex flex-grow items-center justify-center bg-ceruleanBlue-10 border-dashed border-ceruleanBlue-100 border-2 m-2 rounded-lg hover:bg-ceruleanBlue-70"></div>
+          )}
+          {/* {unusedFiles.length > 0 ? (
+            <div>
+              <div className="font-bold underline">Unused Files</div>
+              <ul>
+                {unusedFiles.map((file) => (
+                  <li>{file}</li>
+                ))}
+              </ul>
+            </div>
+          ) : (
+            <p>Drag 'n' drop some files here, or click to select files</p>
+          )}
+          {checkedContracts.length > 0 && (
+            <div className="font-bold underline text-lg">Contract Files</div>
+          )}
+          {
+            <div>
+              {checkedContracts.map((checkedContract) => (
+                <div className="ml-4">
+                  <p className="underline">{checkedContract.name}</p>
+                  {checkedContract.files.found.map((file) => (
+                    <p>{file}</p>
+                  ))}
+                </div>
+              ))}
+            </div>
+          } */}
+        </div>
+      </div>
+      <div className="mt-4">
+        <p className="text-center">or proivide a Github repository</p>
+        <div className="flex justify-center items-center mt-2">
+          <input className="mb-4 w-full" type="text" />
+        </div>
+      </div>
     </div>
   );
 };
--- ui-new/src/types.ts
+++ ui-new/src/types.ts
@@ -3,3 +3,30 @@ export type DropzoneFile = File & {
   size: number;
   path: string;
 };
+
+// Server session
+export type SessionResponse = {
+  contracts: SendableContract[];
+  unused: string[];
+};
+
+export type Status = "perfect" | "partial" | "error";
+
+export type ContractMeta = {
+  compiledPath?: string;
+  name?: string;
+  address?: string;
+  chainId?: string;
+  status?: Status;
+  statusMessage?: string;
+  storageTimestamp?: Date;
+};
+
+// contracts in the server response
+export type SendableContract = ContractMeta & {
+  files: {
+    found: string[];
+    missing: string[];
+  };
+  verificationId?: string;
+};
