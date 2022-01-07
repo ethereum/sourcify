10a681a6e3775a5eb7f925ffd32dd2469552d8ed
kuzdogan <kaanuzdogan@hotmail.com>
2022-01-07 11:28:54 +0300

    Netlify routing config, <Link> instaed of <a>

--- ui-new/src/components/Button/index.tsx
+++ ui-new/src/components/Button/index.tsx
@@ -1,10 +1,9 @@
 type ButtonProps = {
   children: string;
-  href?: string;
   type?: string;
   className?: string;
 };
-const Button = ({ children, href, type, className }: ButtonProps) => {
+const Button = ({ children, type, className }: ButtonProps) => {
   let bg = "bg-ceruleanBlue-500";
   let hoverBg = "hover:bg-ceruleanBlue-600";
   let textColor = "text-white";
@@ -21,13 +20,7 @@ const Button = ({ children, href, type, className }: ButtonProps) => {
     <button
       className={`mt-4 py-3 px-4 ${bg} ${hoverBg} disabled:hover:bg-ceruleanBlue-500 focus:ring-ceruleanBlue-300 focus:ring-offset-ceruleanBlue-100 ${textColor} transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg disabled:opacity-50 disabled:cursor-default uppercase ${className}`}
     >
-      {href ? (
-        <a href={href} target="_blank" rel="noreferrer">
-          {children}
-        </a>
-      ) : (
-        { children }
-      )}
+      {children}
     </button>
   );
 };
