1842b1b8d688752a225c79d32cad1f4510744264
kuzdogan <kaanuzdogan@hotmail.com>
2022-02-15 10:00:49 +0300

    Fallback to public IPFS gateway,sourceFetcher test

--- src/monitor/gateway.ts
+++ src/monitor/gateway.ts
@@ -3,16 +3,20 @@ import { SourceOrigin } from "./util";
 export declare interface IGateway {
     worksWith: (origin: SourceOrigin) => boolean;
     createUrl: (fetchId: string) => string;
+    createFallbackUrl: (fetchId: string) => string;
     baseUrl: string;
+    fallbackUrl?: string;
 }
 
 export class SimpleGateway implements IGateway {
     private origins: SourceOrigin[];
     baseUrl: string;
+    fallbackUrl: string; // A backup gateway in case the local ipfs node fails.
 
-    constructor(origins: SourceOrigin | SourceOrigin[], baseUrl: string) {
+    constructor(origins: SourceOrigin | SourceOrigin[], baseUrl: string, fallbackUrl?: string) {
         this.origins = [].concat(origins);
         this.baseUrl = baseUrl;
+        if (fallbackUrl) this.fallbackUrl = fallbackUrl;
     }
 
     worksWith(origin: SourceOrigin): boolean {
@@ -21,5 +25,9 @@ export class SimpleGateway implements IGateway {
 
     createUrl(fetchId: string): string {
         return this.baseUrl + fetchId;
+    }
+
+    createFallbackUrl(fetchId: string): string {
+        return this.fallbackUrl && this.fallbackUrl + fetchId;
     }
 }
