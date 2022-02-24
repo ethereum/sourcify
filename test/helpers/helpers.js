bdcdf6bb13e9fce23fe0b935da8340a0c33e37fe
kuzdogan <kaanuzdogan@hotmail.com>
2022-02-24 20:58:07 +0300

    Refactor server.js with ganache,refact. monitor.js
    - Refactor server.js to work without Goerli networks. Instead deploy and test networks on local ganache network.q
    - Update ganache package
    - Add local test chain to chainmap when TESTING=true
    - Monitor test chain only when TESTING=true
    - Refactor test/monitor.js with ContractWrapper being separate

--- test/helpers/helpers.js
+++ test/helpers/helpers.js
@@ -1,36 +1,51 @@
-const dagPB = require('ipld-dag-pb');
-const UnixFS = require('ipfs-unixfs');
-const multihashes = require('multihashes');
-const Web3 = require('web3');
-const utils = require('../../services/core/build/utils/utils');
+const dagPB = require("ipld-dag-pb");
+const UnixFS = require("ipfs-unixfs");
+const multihashes = require("multihashes");
+const Web3 = require("web3");
+const utils = require("../../services/core/build/utils/utils");
 
 /**
- * Deploys a contract to testrpc
- * @param  {Object} web3
- * @param  {Object} artifact     ex: const Simple = require('./sources/pass/simple.js');
- * @param  {String} contractName ex: "Simple.sol"
- * @param  {String} from         sender address
- * @param  {Array}  args         constructor args
- * @return {Web3Contract}        deployed contract instance
+ *  Function to deploy contracts from provider unlocked accounts
  */
-async function deployFromArtifact(web3, artifact, from, args){
-  const abi = artifact.compilerOutput.abi;
-
+async function deployFromAbiAndBytecode(web3, abi, bytecode, from, args) {
   // Deploy contract
   const contract = new web3.eth.Contract(abi);
-  return contract.deploy({
-      data: artifact.compilerOutput.evm.bytecode.object,
-      arguments: args || []
-  }).send({ from, gasPrice: '1', gas: 4000000 });
+  const deployment = contract.deploy({
+    data: bytecode,
+    arguments: args || [],
+  });
+  const gas = await deployment.estimateGas({ from });
+  const contractResponse = await deployment.send({
+    from,
+    gas,
+  });
+  return contractResponse.options.address;
 }
 
+/**
+ * Function to deploy contracts from an external account with private key
+ */
+async function deployFromPrivateKey(web3, abi, bytecode, privateKey, args) {
+  const contract = new web3.eth.Contract(abi);
+  const account = web3.eth.accounts.wallet.add(privateKey);
+  const deployment = contract.deploy({
+    data: bytecode,
+    arguments: args || [],
+  });
+  const gas = await deployment.estimateGas({ from: account.address });
+  const contractResponse = await deployment.send({
+    from: account.address,
+    gas,
+  });
+  return contractResponse.options.address;
+}
 /**
  * Await `secs` seconds
  * @param  {Number} secs seconds
  * @return {Promise}
  */
-async function waitSecs(secs=0){
-  return new Promise(resolve => setTimeout(resolve, secs * 1000));
+async function waitSecs(secs = 0) {
+  return new Promise((resolve) => setTimeout(resolve, secs * 1000));
 }
 
 /**
@@ -38,10 +53,10 @@ async function waitSecs(secs=0){
  * @param  {String} str
  * @return {String}     IPFS hash (ex: "Qm")
  */
-async function getIPFSHash(str){
-  const file = new UnixFS('file', Buffer.from(str));
+async function getIPFSHash(str) {
+  const file = new UnixFS("file", Buffer.from(str));
   const node = new dagPB.DAGNode(file.marshal());
-  const metadataLink = await node.toDAGLink()
+  const metadataLink = await node.toDAGLink();
   return multihashes.toB58String(metadataLink._cid.multihash);
 }
 
@@ -50,12 +65,14 @@ async function getIPFSHash(str){
  * @param  {Object} artifact artifact in ex: test/sources/pass
  * @return {String}          hash
  */
-function getBzzr0Hash(artifact){
-  const bytes = Web3.utils.hexToBytes(artifact.compilerOutput.evm.deployedBytecode.object);
+function getBzzr0Hash(artifact) {
+  const bytes = Web3.utils.hexToBytes(
+    artifact.compilerOutput.evm.deployedBytecode.object
+  );
   const data = utils.cborDecode(bytes);
   const val = Web3.utils.bytesToHex(data.bzzr0).slice(2);
 
-  if (!val.length) throw ('Artifact does not support bzzr0');
+  if (!val.length) throw "Artifact does not support bzzr0";
   return val;
 }
 
@@ -64,19 +81,22 @@ function getBzzr0Hash(artifact){
  * @param  {Object} artifact artifact in ex: test/sources/pass
  * @return {String}          hash
  */
-function getBzzr1Hash(artifact){
-  const bytes = Web3.utils.hexToBytes(artifact.compilerOutput.evm.deployedBytecode.object);
+function getBzzr1Hash(artifact) {
+  const bytes = Web3.utils.hexToBytes(
+    artifact.compilerOutput.evm.deployedBytecode.object
+  );
   const data = utils.cborDecode(bytes);
   const val = Web3.utils.bytesToHex(data.bzzr1).slice(2);
 
-  if (!val.length) throw ('Artifact does not support bzzr1');
+  if (!val.length) throw "Artifact does not support bzzr1";
   return val;
 }
 
 module.exports = {
-  deployFromArtifact: deployFromArtifact,
-  waitSecs: waitSecs,
-  getIPFSHash: getIPFSHash,
-  getBzzr0Hash: getBzzr0Hash,
-  getBzzr1Hash: getBzzr1Hash
-}
+  deployFromAbiAndBytecode,
+  deployFromPrivateKey,
+  waitSecs,
+  getIPFSHash,
+  getBzzr0Hash,
+  getBzzr1Hash,
+};
