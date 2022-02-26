e4bab12b0de0704d256e7bbcb9ef1f52f2ea92b1
kuzdogan <kaanuzdogan@hotmail.com>
2022-02-26 21:48:27 +0300

    Save txRegex string to avoid {} in /chains resp

--- services/core/src/sourcify-chains.ts
+++ services/core/src/sourcify-chains.ts
@@ -1,10 +1,9 @@
-import Web3 from "web3";
 import * as dotenv from 'dotenv';
 import path from 'path';
 
 dotenv.config({ path: path.resolve(__dirname, "..", "..", "..", "environments/.env") });
 
-const ETHERSCAN_REGEX = /at txn\s+<a href='\/tx\/(.*?)'/;
+const ETHERSCAN_REGEX = (/at txn\s+<a href='\/tx\/(.*?)'/).source; // save as string to be able to return the txRegex in /chains response. If stored as RegExp returns {}
 const ETHERSCAN_SUFFIX = "address/${ADDRESS}";
 const BLOCKSCOUT_REGEX = "transaction_hash_link\" href=\"${BLOCKSCOUT_PREFIX}/tx/(.*?)\"";
 const BLOCKSCOUT_SUFFIX = "address/${ADDRESS}/transactions";
