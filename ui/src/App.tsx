fb0b83840edbaea7dc31f415bf26b3205292db11
kuzdogan <kaanuzdogan@hotmail.com>
2022-03-29 09:59:51 +0200

    Rename ui folders for legacy

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,69 +1,32 @@
+import { createInstance, MatomoProvider } from "@datapunt/matomo-tracker-react";
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ContextProvider } from "./Context";
+import LandingPage from "./pages/LandingPage";
+import Lookup from "./pages/Lookup";
+import Verifier from "./pages/Verifier";
 
-import "./styles/app.scss";
-import MainLayout from "./components/layout/MainLayout";
-import Verifier from "./components/verifier/Verifier";
-import {GITTER_URL, IPFS_IPNS_GATEWAY_URL, REPOSITORY_URL, SOLIDITY_ETHEREUM_URL} from "./common/constants";
+const instance = createInstance({
+  urlBase: "https://matomo.ethereum.org/",
+  siteId: 30,
+});
 
-const App: React.FC = () => {
-    return (
-        <div id="app" className="grid">
-            <MainLayout>
-                <div className="container">
-                    <div className="container__left">
-                        <h1 className="main-title">
-                            Decentralized Metadata and Source Code Repository
-                        </h1>
-                        <h3 className="sub-title">
-                            Make the most of your Solidity code with metadata files and source
-                            verification.
-                        </h3>
-                        <div className="description">
-                            <p className="description__text">
-                                Sourcify is a decentralized automated contract verification
-                                service, which makes publishing and verifying source code easy.
-                            </p>
-                            <ol className="description__steps">
-                                <li>Choose network from dropdown menu</li>
-                                <li>Input contract address</li>
-                                <li>
-                                    Upload all metadata and contract files that you want to verify
-                                    via the dropzone
-                                </li>
-                            </ol>
-                            <p className="description__text">
-                                Together with the compilation metadata, everyone can re-compile
-                                the smart contract and be sure that the source code is exactly
-                                the same as at deploy time, including every comment.
-                            </p>
-                            <p className="description__text">
-                                You can use this platform to obtain the ABI for each contract.
-                            </p>
-                        </div>
-                        <div className="bottom">
-                            <p>
-                                Browse contract repository <a
-                                href={REPOSITORY_URL} target="_blank" rel="noopener noreferrer">here</a> or
-                                via the <a href={IPFS_IPNS_GATEWAY_URL} target="_blank"
-                                           rel="noopener noreferrer">ipfs/ipns
-                                    gateway</a>.
-                            </p>
-                            <p>
-                                Not sure what all of this is for? Learn why source verification
-                                matters <a href={SOLIDITY_ETHEREUM_URL} target="_blank"
-                                           rel="noopener noreferrer">here</a>.
-                            </p>
-                            <p>Problems or questions? Ask us anything on <a href={GITTER_URL} target="_blank"
-                                                                            rel="noopener noreferrer">Gitter!</a></p>
-                        </div>
-                    </div>
-                    <div className="container__right">
-                        <Verifier/>
-                    </div>
-                </div>
-            </MainLayout>
-        </div>
-    );
-};
+function App() {
+  return (
+    <div className="flex min-h-screen text-gray-800 bg-gray-50">
+      <MatomoProvider value={instance}>
+        <ContextProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/verifier" element={<Verifier />} />
+              <Route path="/lookup" element={<Lookup />} />
+              <Route path="/" element={<LandingPage />} />
+            </Routes>
+          </BrowserRouter>
+        </ContextProvider>
+      </MatomoProvider>
+    </div>
+  );
+}
 
 export default App;
