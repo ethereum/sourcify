5e86d1cf03f26777fa4070396f989d50d41ce2fd
kuzdogan <kaanuzdogan@hotmail.com>
2022-03-29 16:27:43 +0200

    Added Gnosis Safe to featured contracts

--- ui/src/featured.js
+++ ui/src/featured.js
@@ -1,26 +1,33 @@
-import uniswap from "./assets/contracts/uniswap.png";
 import optimism from "./assets/chains/optimism.svg";
+import gnosisSafe from "./assets/contracts/gnosisSafe.svg";
 import synthetix from "./assets/contracts/synthetix.png";
+import uniswap from "./assets/contracts/uniswap.png";
 
 const featured = [
   {
-    "name": "Uniswap",
-    "href": "https://repo.sourcify.dev/contracts/full_match/1/0x1F98431c8aD98523631AE4a59f267346ea31F984/",
-    "logo": uniswap,
-    "displayed": true
+    name: "Uniswap",
+    href: "https://repo.sourcify.dev/contracts/full_match/1/0x1F98431c8aD98523631AE4a59f267346ea31F984/",
+    logo: uniswap,
+    displayed: true,
   },
   {
-    "name": "Synthetix",
-    "href": "https://repo.sourcify.dev/contracts/full_match/10/0x06C6D063896ac733673c4474E44d9268f2402A55/",
-    "logo": synthetix,
-    "displayed": true
+    name: "Synthetix",
+    href: "https://repo.sourcify.dev/contracts/full_match/10/0x06C6D063896ac733673c4474E44d9268f2402A55/",
+    logo: synthetix,
+    displayed: true,
   },
   {
-    "name": "Optimism",
-    "href": "https://repo.sourcify.dev/contracts/full_match/1/0x5e4e65926ba27467555eb562121fac00d24e9dd2/",
-    "logo": optimism,
-    "displayed": true
+    name: "Optimism",
+    href: "https://repo.sourcify.dev/contracts/full_match/1/0x5e4e65926ba27467555eb562121fac00d24e9dd2/",
+    logo: optimism,
+    displayed: true,
   },
-]
+  {
+    name: "Gnosis Safe",
+    href: "https://repo.sourcify.dev/contracts/full_match/1/0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552/",
+    logo: gnosisSafe,
+    displayed: true,
+  },
+];
 
-export default featured
+export default featured;
