2e12c864f8299f8eaff6759b2928c916deb3c337
kuzdogan <kaanuzdogan@hotmail.com>
2022-04-01 17:31:02 +0200

    Matomo via script

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,4 +1,3 @@
-import { createInstance, MatomoProvider } from "@datapunt/matomo-tracker-react";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ContextProvider } from "./Context";
@@ -6,25 +5,18 @@ import LandingPage from "./pages/LandingPage";
 import Lookup from "./pages/Lookup";
 import Verifier from "./pages/Verifier";
 
-const instance = createInstance({
-  urlBase: "https://matomo.ethereum.org/",
-  siteId: 30,
-});
-
 function App() {
   return (
     <div className="flex min-h-screen text-gray-800 bg-gray-50">
-      <MatomoProvider value={instance}>
-        <ContextProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/verifier" element={<Verifier />} />
-              <Route path="/lookup" element={<Lookup />} />
-              <Route path="/" element={<LandingPage />} />
-            </Routes>
-          </BrowserRouter>
-        </ContextProvider>
-      </MatomoProvider>
+      <ContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/verifier" element={<Verifier />} />
+            <Route path="/lookup" element={<Lookup />} />
+            <Route path="/" element={<LandingPage />} />
+          </Routes>
+        </BrowserRouter>
+      </ContextProvider>
     </div>
   );
 }
