e3c61b14fabbdee78226cecfcba4d2f3ad635728
kuzdogan <kaanuzdogan@hotmail.com>
2022-05-17 11:44:18 +0200

    Wait for the ipfs daemon to init
    Because we were not waiting the daemon to init, we were having repo.lock
    errors that the repo is being used by another process. Wait until the
    deamon to init and release the lock, then start ipfs add'ing the repo.

--- services/ipfs/entrypoint.sh
+++ services/ipfs/entrypoint.sh
@@ -35,6 +35,11 @@ ipfs key import main /app/ipfs-${TAG}.key
 
 ipfs daemon --enable-pubsub-experiment --enable-namesys-pubsub --enable-gc &
 
+# Wait for the daemon to initialize
+echo "Sleeping 30 seconds"
+sleep 30
+echo "Sleeped 30 seconds"
+
 # Add the whole repo and publish on start
 date
 echo "Starting ipfs add"
@@ -43,16 +48,18 @@ echo "Finished ipfs add! New ipfs hash: $hash"
 date
 
 # cp the repo under MFS
-echo "Copying to MFS"
+echo "Copying $hash to MFS at /contracts"
 ipfs files cp -p /ipfs/$hash /contracts
-echo "Copied to MFS"
+echo "Copied $hash to MFS at /contracts"
 date
 
 # Add manifest and stats to MFS.
+echo "Adding manifest and stats"
 manifestHash=$(ipfs add -Q /root/.ipfs/repository/manifest.json)
 statsHash=$(ipfs add -Q /root/.ipfs/repository/stats.json)
 ipfs files cp -p /ipfs/$manifestHash /manifest.json
 ipfs files cp -p /ipfs/$statsHash /stats.json
+echo "Added manifest and stats"
 
 rootHash=$(ipfs files stat / --hash)
 
@@ -62,7 +69,7 @@ echo "Published rootHash $rootHash under ipns key"
 date
 
 # Start the run once job.
-echo "Docker container has been started"
+echo "Successfully added and published the repository"
 
 crontab cron.job
 cron -f
