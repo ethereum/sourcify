9b6724c85f27ddf9d0c188dcd29549723fc4bca4
samisbakedham <samsafahi@gmail.com>
2022-05-20 23:53:38 -0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -13,7 +13,7 @@ export default {
         port: process.env.SERVER_PORT || 5000,
         url: process.env.SERVER_URL || "http://localhost:5000",
         maxFileSize: 30 * 1024 * 1024 // 30 MB
-    },
+    }, 
     localchain: {
         port: process.env.LOCALCHAIN_PORT || 8545,
         url: process.env.LOCALCHAIN_URL,
