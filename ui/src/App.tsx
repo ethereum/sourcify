b4e018e83cd080732c5d5347d66b2abf8508bfb6
Kaan Uzdogan <kaanuzdogan@hotmail.com>
2022-06-01 17:43:33 +0200

    Fix BrowserRouter not used

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { ContextProvider } from "./Context";
 import LandingPage from "./pages/LandingPage";
 import Lookup from "./pages/Lookup";
