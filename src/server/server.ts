6df6d17e9483b5f9dbcc472445b88da4550b0d44
Kaan Uzdogan <kaanuzdogan@hotmail.com>
2022-06-13 17:22:20 +0300

    Init optimized CI builds config
    Change the CircleCI config to not build the modules that are not
    changed. Includes a small change in server to test if it will trigger a
    build for server.

--- src/server/server.ts
+++ src/server/server.ts
@@ -42,7 +42,7 @@ export class Server {
       cors({
         origin: '*',
         // Allow follow-up middleware to override this CORS for options. 
-        // v2 API endpoints require non "*" origins because of the session cookies.
+        // v2 API endpoints require non "*" origins because of the session cookies
         preflightContinue: true,
       })
     );
