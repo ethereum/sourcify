79fdb83c69f0f00e33e4bf02b6fda9ff47208c7c
Kaan Uzdogan <kaanuzdogan@hotmail.com>
2022-06-16 12:58:10 +0300

    Fix Chainsafe Storage Access Key

--- services/s3sync/login_sync_s3.sh
+++ services/s3sync/login_sync_s3.sh
@@ -12,7 +12,7 @@ date
 
 # Sync to Chainsafe Storage S3
 /usr/local/bin/aws configure set aws_access_key_id $CHAINSAFE_S3_ACCESS_KEY_ID
-/usr/local/bin/aws configure set aws_secret_access_key $CHAINSAFE_S3_ACCESS_KEY_ID
+/usr/local/bin/aws configure set aws_secret_access_key $CHAINSAFE_S3_SECRET_ACCESS_KEY
 date
 echo "Syncing Chainsafe S3 at $BUCKET_NAME"
 /usr/local/bin/aws s3 sync --quiet /app/repository $BUCKET_NAME --endpoint-url https://buckets.chainsafe.io
