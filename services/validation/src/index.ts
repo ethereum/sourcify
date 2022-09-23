3fd3afdc3bdff8f3bc8b7bf049085e79a91d990f
Kaan Uzdogan <kaanuzdogan@hotmail.com>
2022-09-23 14:03:55 +0200

    Fix validation tests
    Since the `checkFiles` and `checkPaths` functions became async,
    the CLI needs to be fixed, as well as the tests.
    
    The new zip library (or the way we now unzip) changes
    the file order when decompressed.
    Adjust truffle-example.zip file output orders in the tests

--- services/validation/src/index.ts
+++ services/validation/src/index.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-import program from 'commander';
+import program from "commander";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { version, bin } = require("../package.json");
 const name = Object.keys(bin)[0];
-import { ValidationService, IValidationService } from './ValidationService';
-import { CheckedContract } from '@ethereum-sourcify/core';
-export * from './ValidationService';
+import { ValidationService, IValidationService } from "./ValidationService";
+import { CheckedContract } from "@ethereum-sourcify/core";
+export * from "./ValidationService";
 
 const NAME_DELIMITER = ":";
 function parseRecompilationTarget(input: string) {
@@ -14,7 +14,10 @@ function parseRecompilationTarget(input: string) {
     return input;
   }
 
-  return {path: input.slice(0, delimiterIndex), name: input.slice(delimiterIndex+1)};
+  return {
+    path: input.slice(0, delimiterIndex),
+    name: input.slice(delimiterIndex + 1),
+  };
 }
 
 function matches(contract: CheckedContract, recompilationTarget: any): boolean {
@@ -23,7 +26,10 @@ function matches(contract: CheckedContract, recompilationTarget: any): boolean {
   }
 
   if (typeof recompilationTarget === "string") {
-    if (recompilationTarget === contract.name || recompilationTarget === contract.compiledPath) {
+    if (
+      recompilationTarget === contract.name ||
+      recompilationTarget === contract.compiledPath
+    ) {
       return true;
     }
     return false;
@@ -34,25 +40,36 @@ function matches(contract: CheckedContract, recompilationTarget: any): boolean {
   return nameMatches && pathMatches;
 }
 
-function logOutput(recompilableContracts: CheckedContract[], programOptions: any) {
+function logOutput(
+  recompilableContracts: CheckedContract[],
+  programOptions: any
+) {
   switch (recompilableContracts.length) {
-  case 0: {
-    console.log(`Could not find the ${OPTION_NAME} target: ${programOptions.prepareJson}`);
-    process.exitCode = 1;
-    break;
-  }
-  case 1: {
-    const standardJson = recompilableContracts[0].standardJson;
-    const prettifiedStandardJson = JSON.stringify(standardJson, null, programOptions.pretty ? 2 : undefined);
-    console.log(prettifiedStandardJson);
-    break;
-  }
-  default: {
-    console.log(`Ambiguous ${OPTION_NAME} target: ${programOptions.prepareJson}`);
-    console.log(`${recompilableContracts.length} instances encountered.`);
-    process.exitCode = 1;
-    break;
-  }
+    case 0: {
+      console.log(
+        `Could not find the ${OPTION_NAME} target: ${programOptions.prepareJson}`
+      );
+      process.exitCode = 1;
+      break;
+    }
+    case 1: {
+      const standardJson = recompilableContracts[0].standardJson;
+      const prettifiedStandardJson = JSON.stringify(
+        standardJson,
+        null,
+        programOptions.pretty ? 2 : undefined
+      );
+      console.log(prettifiedStandardJson);
+      break;
+    }
+    default: {
+      console.log(
+        `Ambiguous ${OPTION_NAME} target: ${programOptions.prepareJson}`
+      );
+      console.log(`${recompilableContracts.length} instances encountered.`);
+      process.exitCode = 1;
+      break;
+    }
   }
 }
 
@@ -61,81 +78,88 @@ program
   .name(name)
   .helpOption("-h, --help", "Output the help message.")
   .version(version, "-v, --version", "Output the current version.")
-  .description("A tool that validates smart contracts contained in the provided files, directories and zip archives.")
+  .description(
+    "A tool that validates smart contracts contained in the provided files, directories and zip archives."
+  )
   .usage("[options] <path>...")
   .option(
     `-j, --${OPTION_NAME} <path:name>`,
     "Output only a JSON object to be used as standard-json input to solc.\n" +
-        "The expected argument ought to be of the form path:name; ommiting any of the two is tolerated if unambiguous.\n" +
-        `E.g. \`${name} --${OPTION_NAME} foo/bar/Contract.sol:ContractName path/to/dir path/to/zip\`\n`
+      "The expected argument ought to be of the form path:name; ommiting any of the two is tolerated if unambiguous.\n" +
+      `E.g. \`${name} --${OPTION_NAME} foo/bar/Contract.sol:ContractName path/to/dir path/to/zip\`\n`
   )
   .option(
     "-p, --pretty",
     "Output the standard json in a pretty format.\n" +
-        `Only available with the --${OPTION_NAME} option\n`
+      `Only available with the --${OPTION_NAME} option\n`
   );
 
 if (require.main === module) {
-  program.parse(process.argv);
-  const fileNames = program.args; // what's left from process.argv
-  const fileNamesJoint = fileNames.join(" ");
-  if (!fileNames.length) {
-    program.help();
-  }
-
-  const recompilationTarget =
-        program.prepareJson ? parseRecompilationTarget(program.prepareJson) : program.prepareJson;
-    
-  const validationService: IValidationService = new ValidationService();
-  let checkedContracts: CheckedContract[] = [];
-  const ignoring: any[] = [];
-  try {
-    validationService.checkPaths(fileNames, ignoring).then((contracts) => {
-      checkedContracts = contracts;
-    });
-  } catch (err: any) {
-    console.log(err.message);
-    process.exitCode = 1;
-  }
-    
-  const recompilableContracts: CheckedContract[] = [];
-  const contractMessages: string[] = [];
-  let invalidContracts = 0;
-  checkedContracts.forEach(contract => {
-    if (matches(contract, recompilationTarget)) {
-      if (CheckedContract.isValid(contract)) {
-        recompilableContracts.push(contract);
-      } else {
-        console.log(contract.getInfo());
-        console.log(`The ${OPTION_NAME} target is not a valid contract! Exiting.`);
-        process.exit(1);
-      }
-    } else if (recompilationTarget === undefined) {
-      let msg = contract.getInfo(); // should be without a trailing newline
-      if (CheckedContract.isValid(contract)) {
-        msg += "\n";
-        msg += `  To recompile, use: \`${name} --${OPTION_NAME} ${contract.compiledPath}:${contract.name} ${fileNamesJoint}`;
-        msg += ` | solc --standard-json\``;
-      } else {
-        invalidContracts++;
-      }
-      contractMessages.push(msg);
+  (async () => {
+    program.parse(process.argv);
+    const fileNames = program.args; // what's left from process.argv
+    const fileNamesJoint = fileNames.join(" ");
+    if (!fileNames.length) {
+      program.help();
     }
-  });
 
-  if (contractMessages.length) {
-    console.log(contractMessages.join("\n\n"));
-  }
+    const recompilationTarget = program.prepareJson
+      ? parseRecompilationTarget(program.prepareJson)
+      : program.prepareJson;
 
-  if (recompilationTarget !== undefined) {
-    logOutput(recompilableContracts, program.opts());
+    const validationService: IValidationService = new ValidationService();
+    let checkedContracts: CheckedContract[] = [];
+    const ignoring: any[] = [];
+    try {
+      checkedContracts = await validationService.checkPaths(
+        fileNames,
+        ignoring
+      );
+    } catch (err: any) {
+      console.log(err.message);
+      process.exitCode = 1;
+    }
 
-  } else if (ignoring.length) {
-    console.log("\nFiles ignored due to readability issues:");
-    ignoring.forEach(ignored => console.log("  "+ignored));
-  }
+    const recompilableContracts: CheckedContract[] = [];
+    const contractMessages: string[] = [];
+    let invalidContracts = 0;
+    checkedContracts.forEach((contract) => {
+      if (matches(contract, recompilationTarget)) {
+        if (CheckedContract.isValid(contract)) {
+          recompilableContracts.push(contract);
+        } else {
+          console.log(contract.getInfo());
+          console.log(
+            `The ${OPTION_NAME} target is not a valid contract! Exiting.`
+          );
+          process.exit(1);
+        }
+      } else if (recompilationTarget === undefined) {
+        let msg = contract.getInfo(); // should be without a trailing newline
+        if (CheckedContract.isValid(contract)) {
+          msg += "\n";
+          msg += `  To recompile, use: \`${name} --${OPTION_NAME} ${contract.compiledPath}:${contract.name} ${fileNamesJoint}`;
+          msg += ` | solc --standard-json\``;
+        } else {
+          invalidContracts++;
+        }
+        contractMessages.push(msg);
+      }
+    });
 
-  if (invalidContracts > 0) {
-    process.exitCode = 1;
-  }
+    if (contractMessages.length) {
+      console.log(contractMessages.join("\n\n"));
+    }
+
+    if (recompilationTarget !== undefined) {
+      logOutput(recompilableContracts, program.opts());
+    } else if (ignoring.length) {
+      console.log("\nFiles ignored due to readability issues:");
+      ignoring.forEach((ignored) => console.log("  " + ignored));
+    }
+
+    if (invalidContracts > 0) {
+      process.exitCode = 1;
+    }
+  })();
 }
