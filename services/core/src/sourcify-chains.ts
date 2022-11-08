f7cb6f3c6021c9a7353f23eeeb9812a21195cd0a
Rim Rakhimov <rimrakhimov@gmail.com>
2022-11-08 15:47:37 +0300

    Add chain 8217

--- services/core/src/sourcify-chains.ts
+++ services/core/src/sourcify-chains.ts
@@ -517,4 +517,11 @@ export default {
     contractFetchAddress: "https://klaytn-testnet.aws-k8s.blockscout.com/" + BLOCKSCOUT_SUFFIX,
     txRegex: getBlockscoutRegex(),
   },
+  "8217": {
+    // Klaytn Mainnet Cypress
+    supported: true,
+    monitored: false,
+    contractFetchAddress: "https://klaytn-mainnet.aws-k8s.blockscout.com/" + BLOCKSCOUT_SUFFIX,
+    txRegex: getBlockscoutRegex(),
+  },
 };
