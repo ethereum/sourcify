b1dfcaf769b614eac7362a66709d5e683b836415
Kaan Uzdogan <kaanuzdogan@hotmail.com>
2023-01-10 09:01:56 +0100

    Refactor verification with creatorTx

--- packages/lib-sourcify/src/lib/verification.ts
+++ packages/lib-sourcify/src/lib/verification.ts
@@ -1,6 +1,7 @@
 import { CheckedContract } from './CheckedContract';
 import { ContextVariables, Match, SourcifyChain, StringMap } from './types';
 import { toChecksumAddress } from 'web3-utils';
+import { Transaction } from 'web3-compilers';
 import Web3 from 'web3';
 import {
   decode as bytecodeDecode,
@@ -12,19 +13,21 @@ import { Address } from '@ethereumjs/util';
 import { Common } from '@ethereumjs/common';
 import { DefaultStateManager } from '@ethereumjs/statemanager';
 import { Blockchain } from '@ethereumjs/blockchain';
+import { encode as rlpEncode } from '@ethersproject/rlp';
+
+const RPC_TIMEOUT = 5000;
 
 export async function verifyDeployed(
   checkedContract: CheckedContract,
   sourcifyChain: SourcifyChain,
   address: string,
-  contextVariables?: ContextVariables
+  contextVariables?: ContextVariables,
+  creatorTxHash?: string
 ): Promise<Match> {
   const match: Match = {
     address,
     chainId: sourcifyChain.chainId.toString(),
     status: null,
-    abiEncodedConstructorArguments: undefined,
-    libraryMap: undefined,
   };
 
   const recompiled = await checkedContract.recompile();
@@ -40,13 +43,10 @@ export async function verifyDeployed(
     return match;
   }
 
-  // Replace the library placeholders in the recompiled bytecode with values from the deployed bytecode
-  const { replaced, libraryMap } = addLibraryAddresses(
-    recompiled.deployedBytecode,
-    deployedBytecode
-  );
-  recompiled.deployedBytecode = replaced;
-  match.libraryMap = libraryMap;
+  if (deployedBytecode.length !== recompiled.deployedBytecode.length) {
+    match.message = `The deployed and the recompiled bytecode don't match. Deployed bytecode length: ${deployedBytecode.length}, recompiled bytecode length: ${recompiled.deployedBytecode.length}.`;
+    return match;
+  }
 
   // Try to match with deployed bytecode directly
   matchWithDeployedBytecode(
@@ -57,7 +57,7 @@ export async function verifyDeployed(
   if (match.status) return match;
 
   // Try to match with simulating the creation bytecode
-  matchWithSimulation(
+  await matchWithSimulation(
     match,
     recompiled.creationBytecode,
     deployedBytecode,
@@ -69,27 +69,39 @@ export async function verifyDeployed(
     match.contextVariables = contextVariables;
     return match;
   }
+
+  // If same length, highly likely these contracts are a match but immutable vars. may be affecting the match
+  // Try to match with creationTx, if available
+  if (creatorTxHash) {
+    await matchWithCreationTx(
+      match,
+      creatorTxHash,
+      recompiled.creationBytecode,
+      sourcifyChain,
+      address
+    );
+    if (match.status) return match;
+  }
   return match;
 }
 
-export async function matchWithDeployedBytecode(
+export function matchWithDeployedBytecode(
   match: Match,
   recompiledDeployedBytecode: string,
   deployedBytecode: string
 ) {
-  if (recompiledDeployedBytecode === deployedBytecode) {
-    // if the bytecode doesn't contain metadata then "partial" match
-    let containsMetadata;
-    try {
-      const decodedCBOR = bytecodeDecode(deployedBytecode);
-      containsMetadata =
-        !!decodedCBOR.ipfs || !!decodedCBOR['bzzr0'] || !!decodedCBOR['bzzr1'];
-    } catch (e) {
-      console.log("Can't decode CBOR");
-      containsMetadata = false;
-    }
+  // Replace the library placeholders in the recompiled bytecode with values from the deployed bytecode
+  const { replaced, libraryMap } = addLibraryAddresses(
+    recompiledDeployedBytecode,
+    deployedBytecode
+  );
+  recompiledDeployedBytecode = replaced;
 
-    if (containsMetadata) {
+  if (recompiledDeployedBytecode === deployedBytecode) {
+    match.libraryMap = libraryMap;
+
+    // if the bytecode doesn't contain metadata then "partial" match
+    if (doesContainMetadataHash(deployedBytecode)) {
       match.status = 'perfect';
     } else {
       match.status = 'partial';
@@ -101,6 +113,7 @@ export async function matchWithDeployedBytecode(
       recompiledDeployedBytecode
     );
     if (trimmedDeployedBytecode === trimmedCompiledRuntimeBytecode) {
+      match.libraryMap = libraryMap;
       match.status = 'partial';
     }
   }
@@ -165,7 +178,70 @@ export async function matchWithSimulation(
     deployedBytecode
   );
 }
+/**
+ * Matches the contract via the transaction that created the contract, if that tx is known.
+ * Checks if the tx.input matches the recompiled creation bytecode. Double checks that the contract address matches the address being verified.
+ *
+ * @param match
+ * @param creatorTxHash
+ * @param recompiledCreationBytecode
+ * @param sourcifyChain
+ * @param address
+ * @returns
+ */
+export async function matchWithCreationTx(
+  match: Match,
+  creatorTxHash: string,
+  recompiledCreationBytecode: string,
+  sourcifyChain: SourcifyChain,
+  address: string
+) {
+  const creatorTx = await getTx(creatorTxHash, sourcifyChain);
+  const creatorTxData = creatorTx.input;
 
+  // Initially we need to check if this contract creation tx actually yields the same contract address https://github.com/ethereum/sourcify/issues/887
+  const createdContractAddress = calculateCreatedContractAddress(creatorTx);
+  if (createdContractAddress !== address) {
+    match.message = `The address being verified ${address} doesn't match the address of the contract ${createdContractAddress} that will be created by the transaction ${creatorTxHash}.`;
+    return;
+  }
+
+  // The reason why this uses `startsWith` instead of `===` is that creationTxData may contain constructor arguments at the end part.
+  // Replace the library placeholders in the recompiled bytecode with values from the deployed bytecode
+  const { replaced, libraryMap } = addLibraryAddresses(
+    recompiledCreationBytecode,
+    creatorTxData
+  );
+  recompiledCreationBytecode = replaced;
+
+  if (creatorTxData.startsWith(recompiledCreationBytecode)) {
+    // if the bytecode doesn't contain metadata then "partial" match
+    if (doesContainMetadataHash(recompiledCreationBytecode)) {
+      match.status = 'perfect';
+    } else {
+      match.status = 'partial';
+    }
+  } else {
+    // Match without metadata hashes
+    const [trimmedCreatorTxData] = splitAuxdata(creatorTxData); // In the case of creationTxData (not deployed bytecode) it is actually not CBOR encoded because of the appended constr. args., but splitAuxdata returns the whole bytecode if it's not CBOR encoded, so will work with startsWith.
+    const [trimmedRecompiledCreationBytecode] = splitAuxdata(
+      recompiledCreationBytecode
+    );
+    if (trimmedCreatorTxData.startsWith(trimmedRecompiledCreationBytecode)) {
+      match.status = 'partial';
+    }
+  }
+
+  if (match.status) {
+    match.libraryMap = libraryMap;
+    const abiEncodedConstructorArguments =
+      extractAbiEncodedConstructorArguments(
+        creatorTxData,
+        recompiledCreationBytecode
+      );
+    match.abiEncodedConstructorArguments = abiEncodedConstructorArguments;
+  }
+}
 /**
  * Fetches the contract's deployed bytecode from SourcifyChain's rpc's.
  * Tries to fetch sequentially if the first RPC is a local eth node. Fetches in parallel otherwise.
@@ -177,8 +253,6 @@ export async function getBytecode(
   sourcifyChain: SourcifyChain,
   address: string
 ): Promise<string> {
-  const RPC_TIMEOUT = 5000;
-
   if (!sourcifyChain?.rpc.length)
     throw new Error('No RPC provider was given for this chain.');
   address = toChecksumAddress(address);
@@ -186,7 +260,17 @@ export async function getBytecode(
   // Request sequentially. Custom node is always before ALCHEMY so we don't waste resources if succeeds.
   for (const rpcURL of sourcifyChain.rpc) {
     try {
-      const bytecode = await raceWithTimeout(rpcURL, RPC_TIMEOUT, address);
+      const web3 = rpcURL.startsWith('http')
+        ? new Web3(new Web3.providers.HttpProvider(rpcURL))
+        : new Web3(new Web3.providers.WebsocketProvider(rpcURL));
+
+      if (!web3.currentProvider) throw new Error('No provider found');
+
+      // Race the RPC call with a timeout
+      const bytecode = await Promise.race([
+        web3.eth.getCode(address),
+        rejectInMs(RPC_TIMEOUT, rpcURL),
+      ]);
       if (bytecode) {
         console.log(
           `Execution bytecode fetched from address ${address} via ${rpcURL}`
@@ -201,15 +285,32 @@ export async function getBytecode(
   throw new Error('None of the RPCs responded');
 }
 
-// Races the web3.eth.getCode call with a timeout promise. Returns a wrapper Promise that rejects if getCode call takes longer than timeout.
-function raceWithTimeout(rpcURL: string, timeout: number, address: string) {
-  const web3 = rpcURL.startsWith('http')
-    ? new Web3(new Web3.providers.HttpProvider(rpcURL))
-    : new Web3(new Web3.providers.WebsocketProvider(rpcURL));
+async function getTx(creatorTxHash: string, sourcifyChain: SourcifyChain) {
+  if (!sourcifyChain?.rpc.length)
+    throw new Error('No RPC provider was given for this chain.');
+  for (const rpcURL of sourcifyChain.rpc) {
+    try {
+      const web3 = rpcURL.startsWith('http')
+        ? new Web3(new Web3.providers.HttpProvider(rpcURL))
+        : new Web3(new Web3.providers.WebsocketProvider(rpcURL));
 
-  if (!web3.currentProvider) throw new Error('No provider found');
+      if (!web3.currentProvider) throw new Error('No provider found');
 
-  return Promise.race([web3.eth.getCode(address), rejectInMs(timeout, rpcURL)]);
+      // Race the RPC call with a timeout
+      const tx = (await Promise.race([
+        web3.eth.getTransaction(creatorTxHash),
+        rejectInMs(RPC_TIMEOUT, rpcURL),
+      ])) as Transaction;
+      if (tx) {
+        console.log(`Transaction ${creatorTxHash} fetched via ${rpcURL}`);
+      }
+      return tx;
+    } catch (err) {
+      // Catch to try the next RPC
+      console.log(err);
+    }
+  }
+  throw new Error('None of the RPCs responded');
 }
 
 const rejectInMs = (ms: number, host: string) =>
@@ -245,4 +346,51 @@ function addLibraryAddresses(
     replaced: template,
     libraryMap,
   };
+}
+
+function extractAbiEncodedConstructorArguments(
+  onchainCreationBytecode: string,
+  compiledCreationBytecode: string
+) {
+  if (onchainCreationBytecode.length === compiledCreationBytecode.length)
+    return undefined;
+
+  const startIndex = onchainCreationBytecode.indexOf(compiledCreationBytecode);
+  return (
+    '0x' +
+    onchainCreationBytecode.slice(startIndex + compiledCreationBytecode.length)
+  );
+}
+
+/**
+ * Calculates the address of the contract that will be created with this tx using `tx.from` and `tx.nonce` according to the yellow paper.
+ *
+ * @param Transaction - creatorTx
+ * @returns string -  calculated address
+ */
+function calculateCreatedContractAddress(creatorTx: Transaction) {
+  const inputArr = [creatorTx.from, creatorTx.nonce];
+  const rlpEncoded = rlpEncode(inputArr);
+  const hash = Web3.utils.keccak256(rlpEncoded);
+  const address = hash.substring(24);
+  return address;
+}
+
+/**
+ * Checks if there's a CBOR encoded metadata hash appended to the bytecode.
+ *
+ * @param bytecode
+ * @returns bool - true if there's a metadata hash
+ */
+function doesContainMetadataHash(bytecode: string) {
+  let containsMetadata: boolean;
+  try {
+    const decodedCBOR = bytecodeDecode(bytecode);
+    containsMetadata =
+      !!decodedCBOR.ipfs || !!decodedCBOR['bzzr0'] || !!decodedCBOR['bzzr1'];
+  } catch (e) {
+    console.log("Can't decode CBOR");
+    containsMetadata = false;
+  }
+  return containsMetadata;
 }
