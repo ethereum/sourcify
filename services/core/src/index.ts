34e0aec9fef993c2f62c268fff9e48e369632823
Marco Castignoli <marco@castignoli.it>
2023-01-30 14:47:44 +0100

    remove all services

--- services/core/src/index.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-export * from "./utils/types";
-export * from "./utils/utils";
-export * from "./utils/CheckedContract";
-export * from "./services/FileService";
-export * from "./services/EventManager";
-export * from "./services/EventManager/listeners/logger";
-export * from "./services/EventManager/listeners/matchStored";
