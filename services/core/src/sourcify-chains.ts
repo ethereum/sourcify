58805fb64d244fbe1fa1a78ee613f9a0d906b541
BearNetwork <105615886+BearNetwork-BRNKC@users.noreply.github.com>
2023-02-17 00:38:29 +0800

    add-chain-641230

--- services/core/src/sourcify-chains.ts
+++ services/core/src/sourcify-chains.ts
@@ -639,6 +639,15 @@ const sourcifyChains: SourcifyChainsObject = {
     rpc: ["https://web3-testnet-rpc.thestratos.org"],
     txRegex: getBlockscoutRegex(),
   },
+  
+    "641230": {
+    // Bear Network Chain Mainnet
+    supported: true,
+    monitored: false,
+    contractFetchAddress: "https://brnkscan.bearnetwork.net/" + BLOCKSCOUT_SUFFIX,
+    rpc: ["https://brnkc-mainnet.bearnetwork.net"],
+    txRegex: getBlockscoutRegex(),
+  },
 };
 
 export default sourcifyChains;
