c4b863216b875a74ceb381df8f76892a3d4294ee
BearNetwork <105615886+BearNetwork-BRNKC@users.noreply.github.com>
2023-02-17 00:39:44 +0800

    add-chain-641230

--- test/chains/chain-tests.js
+++ test/chains/chain-tests.js
@@ -1301,6 +1301,24 @@ describe("Test Supported Chains", function () {
     ["shared/WithImmutables.sol"],
     "shared/withImmutables.metadata.json"
   );
+  
+  // Bear Network Chain Mainnet
+  verifyContract(
+    "0x3e552a8EabfEEEd1dd3e25Be98F9aAbbca8cB200",
+    "641230",
+    "Bear Network Chain Mainnet",
+    ["shared/1_Storage.sol"],
+    "shared/1_Storage.metadata.json"
+  );
+  verifyContractWithImmutables(
+    "0xC9801C4E7774Ea03D3E59052933e4F954338c6C2",
+    "641230",
+    "Bear Network Chain Mainnet",
+    ["uint256"],
+    [641230],
+    ["shared/WithImmutables.sol"],
+    "shared/withImmutables.metadata.json"
+  );
 
 
   //////////////////////
