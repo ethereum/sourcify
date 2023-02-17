066c9a68ec95d57a80784e3e3364edb83f4aebee
BearNetwork <105615886+BearNetwork-BRNKC@users.noreply.github.com>
2023-02-17 08:09:50 +0800

    Update sourcify-chains.ts

--- services/core/src/sourcify-chains.ts
+++ services/core/src/sourcify-chains.ts
@@ -652,7 +652,6 @@ const sourcifyChains: SourcifyChainsObject = {
     supported: true,
     monitored: false,
     contractFetchAddress: "https://brnkscan.bearnetwork.net/" + BLOCKSCOUT_SUFFIX,
-    rpc: ["https://brnkc-mainnet.bearnetwork.net"],
     txRegex: getBlockscoutRegex(),
   },
 };
