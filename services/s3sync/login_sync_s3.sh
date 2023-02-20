ac588f71172bdd7d932c270b8841783f120aee47
Kaan Uzdogan <kaanuzdogan@hotmail.com>
2023-02-20 15:58:22 +0100

    Chainsafe S3 settings
    Add MASTER secrets for Chainsafe S3
    
    Change s3 sync settings

--- services/s3sync/login_sync_s3.sh
+++ services/s3sync/login_sync_s3.sh
@@ -3,18 +3,17 @@
 /usr/local/bin/aws --version
 /usr/local/bin/aws configure set aws_access_key_id $AWS_S3_ACCESS_KEY_ID
 /usr/local/bin/aws configure set aws_secret_access_key $AWS_S3_SECRET_ACCESS_KEY
+aws configure set s3.max_concurrent_requests 10
 
-date
-echo "Syncing AWS at $BUCKET_NAME/$TAG"
+echo "$(date) Syncing AWS at $BUCKET_NAME/$TAG"
 /usr/local/bin/aws s3 sync --quiet /app/repository $BUCKET_NAME/$TAG
-echo "Sync AWS complete"
-date
+echo "$(date) Sync AWS complete"
 
 # Sync to Chainsafe Storage S3
 /usr/local/bin/aws configure set aws_access_key_id $CHAINSAFE_S3_ACCESS_KEY_ID
 /usr/local/bin/aws configure set aws_secret_access_key $CHAINSAFE_S3_SECRET_ACCESS_KEY
-date
-echo "Syncing Chainsafe S3 at $BUCKET_NAME"
+aws configure set s3.max_concurrent_requests 2 # Chainsafe Storage S3 concurrency is limited
+
+echo "$(date) Syncing Chainsafe S3 at $BUCKET_NAME"
 /usr/local/bin/aws s3 sync --quiet /app/repository $BUCKET_NAME/$TAG --endpoint-url https://buckets.chainsafe.io
-echo "Syncing Chainsafe S3 complete"
-date
+echo "$(date) Syncing Chainsafe S3 complete"
