dd389343b7a7a5ecf291649c1bfec277d2401254
Kaan Uzdogan <kaanuzdogan@hotmail.com>
2023-02-24 10:38:16 +0100

    Log Chainsafe S3 output

--- services/s3sync/login_sync_s3.sh
+++ services/s3sync/login_sync_s3.sh
@@ -3,7 +3,7 @@
 /usr/local/bin/aws --version
 /usr/local/bin/aws configure set aws_access_key_id $AWS_S3_ACCESS_KEY_ID
 /usr/local/bin/aws configure set aws_secret_access_key $AWS_S3_SECRET_ACCESS_KEY
-aws configure set s3.max_concurrent_requests 10
+/usr/local/bin/aws configure set s3.max_concurrent_requests 10
 
 echo "$(date) Syncing AWS at $BUCKET_NAME/$TAG"
 /usr/local/bin/aws s3 sync --quiet /app/repository $BUCKET_NAME/$TAG
@@ -12,8 +12,8 @@ echo "$(date) Sync AWS complete"
 # Sync to Chainsafe Storage S3
 /usr/local/bin/aws configure set aws_access_key_id $CHAINSAFE_S3_ACCESS_KEY_ID
 /usr/local/bin/aws configure set aws_secret_access_key $CHAINSAFE_S3_SECRET_ACCESS_KEY
-aws configure set s3.max_concurrent_requests 2 # Chainsafe Storage S3 concurrency is limited
+/usr/local/bin/aws configure set s3.max_concurrent_requests 2 # Chainsafe Storage S3 concurrency is limited
 
 echo "$(date) Syncing Chainsafe S3 at $BUCKET_NAME"
-/usr/local/bin/aws s3 sync --quiet /app/repository $BUCKET_NAME/$TAG --endpoint-url https://buckets.chainsafe.io
+/usr/local/bin/aws s3 sync /app/repository $BUCKET_NAME/$TAG --endpoint-url https://buckets.chainsafe.io > "/app/logs/s3sync-chainsafe-$(date '+%Y-%m-%dZ%H:%M').log"
 echo "$(date) Syncing Chainsafe S3 complete"
