737eed7c61e2b9ed5ae2727c194c1bbd9284b8e2
Marco Castignoli <marco@castignoli.it>
2023-04-05 14:31:44 +0200

    wait 1 second between every etherscan request

--- test/etherscan.js
+++ test/etherscan.js
@@ -31,6 +31,10 @@ chai.use(chaiHttp);
 const CUSTOM_PORT = 5678;
 
 describe("Import From Etherscan and Verify", function () {
+  this.beforeEach(async function () {
+    await waitSecs(1);
+  });
+
   // Don't run if it's an external PR. Etherscan tests need API keys that can't be exposed to external PRs.
   if (process.env.CIRCLE_PR_REPONAME !== undefined) {
     return;
