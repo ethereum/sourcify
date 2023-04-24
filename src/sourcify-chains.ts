854004e344a63d12bfe834c1e4b697457d65e1ec
lolieatapple <54833180+lolieatapple@users.noreply.github.com>
2023-04-24 15:29:08 +0800

    Add chain 999

--- src/sourcify-chains.ts
+++ src/sourcify-chains.ts
@@ -699,6 +699,11 @@ const sourcifyChainsExtensions: SourcifyChainsExtensionsObject = {
     contractFetchAddress: "https://goerli.basescan.org/" + ETHERSCAN_SUFFIX,
     txRegex: ETHERSCAN_REGEX,
   },
+  "999": {
+    supported: true,
+    monitored: false,
+    txRegex: ETHERSCAN_REGEX,
+  },
 };
 
 const sourcifyChainsMap: SourcifyChainMap = {};
@@ -765,8 +770,8 @@ export function getSortedChainsArray(
       getPrimarySortKey(a) > getPrimarySortKey(b)
         ? 1
         : getPrimarySortKey(b) > getPrimarySortKey(a)
-        ? -1
-        : 0
+          ? -1
+          : 0
     );
 
   const sortedChains = etherumChains.concat(otherChains);
