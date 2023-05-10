0dbf84176b8ee4accf0b5abc4454052b2112a17a
oliviachef <116026363+oliviachef@users.noreply.github.com>
2023-05-10 15:32:48 +0530

    commit

---
+++ test/chains/sources/2000/immutable/fixednumber.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract fixednumber {
+
+    uint256 public fixed_number;
+
+    constructor(uint256 _fixed_number){
+
+        fixed_number = _fixed_number;
+
+    }
+
+}
