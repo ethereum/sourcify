5cf258a2e7d7a713ffb785fcba9db6ae4f4d2f7b
oliviachef <116026363+oliviachef@users.noreply.github.com>
2023-05-10 15:30:32 +0530

    commit

---
+++ test/chains/sources/2000/number.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract numbers {
+
+    uint256 public number;
+
+    constructor(uint256 _number){
+
+        number = _number;
+
+    }
+
+    function newNumber(uint256 _newNumber) public {
+
+        number = _newNumber;
+
+    }
+
+    function resetNumber() public {
+
+        number = 0;
+
+    }
+
+    function viewNumber() public view returns(uint256){
+
+        return number;
+
+    }
+
+}
