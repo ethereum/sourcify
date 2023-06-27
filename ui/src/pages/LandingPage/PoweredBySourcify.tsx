2f74618fe333ccfd3def29ef6d2d3009d918b964
Aleksey Ilin <alexis.silent@gmail.com>
2023-06-27 20:43:46 +0700

    added dex.guru to powered by section

--- ui/src/pages/LandingPage/PoweredBySourcify.tsx
+++ ui/src/pages/LandingPage/PoweredBySourcify.tsx
@@ -3,6 +3,7 @@ import ethSdk from "../../assets/integrations/eth-sdk.png";
 import keystone from "../../assets/integrations/keystone.png";
 import otter from "../../assets/integrations/otter.jpg";
 import walleth from "../../assets/integrations/walleth.png";
+import dexGuru from "../../assets/integrations/dexGuru.svg";
 import AppIconName from "../../components/AppIconName";
 
 export default function PoweredBySourcify() {
@@ -36,6 +37,11 @@ export default function PoweredBySourcify() {
           img={ethSdk}
           name="eth-sdk"
           href="https://github.com/dethcrypto/eth-sdk/pull/42"
+        />
+        <AppIconName
+          img={dexGuru}
+          name="DexGuru Block Explorer"
+          href="https://b2b.dex.guru/explorer"
         />
       </div>
     </div>
