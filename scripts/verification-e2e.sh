fe9bf9758bec8c175bc0c79c03e70deb203dcb32
Marco Castignoli <marco@castignoli.it>
2023-07-12 13:26:57 +0200

    use new metacoin-source-verify with hardhat

--- scripts/verification-e2e.sh
+++ scripts/verification-e2e.sh
@@ -8,9 +8,6 @@ npm ci
 
 # Test WITH providing address and chain
 npm run deploy-with-salt:$CHAIN_NAME || exit 3
-echo "Waiting 60 secs"
-sleep 60 # Leave some buffer for blocks to propogate
-echo "Waited 60 secs"
 
 cd ..
 node scripts/verification-e2e.js $CHAIN_ID || exit 4
