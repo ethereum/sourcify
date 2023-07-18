eb3595a2edc8e557627b3b379b8350caf470bb73
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2023-07-18 16:40:59 +0200

    Fix ipfs/server paths

--- scripts/find_replace.sh
+++ scripts/find_replace.sh
@@ -5,8 +5,8 @@ source ~/.profile
 
 cd scripts/
 gpg --yes --batch --passphrase=$SECRET_KEY ../environments/.env.secrets.gpg
-gpg --yes --batch --passphrase=$SECRET_KEY ../services/ipfs/ipfs-stable.key.gpg
-gpg --yes --batch --passphrase=$SECRET_KEY ../services/ipfs/ipfs-latest.key.gpg
+gpg --yes --batch --passphrase=$SECRET_KEY ../services/ipfs/server/ipfs-stable.key.gpg
+gpg --yes --batch --passphrase=$SECRET_KEY ../services/ipfs/server/ipfs-latest.key.gpg
 source ../environments/.env.secrets
 
 TAG="$CIRCLE_BRANCH"
