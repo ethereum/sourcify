10acb3071cb5a4b5820973c28bd79c8f3722d158
dfenstermaker <dannyfenstermaker@gmail.com>
2023-08-28 14:42:07 -0600

    feat: new chain conduit 78432

--- src/sourcify-chains.ts
+++ src/sourcify-chains.ts
@@ -1009,6 +1009,13 @@ const sourcifyChainsExtensions: SourcifyChainsExtensionsObject = {
       "https://glacier-api.avax.network/v1/chains/2037/" +
       AVALANCHE_SUBNET_SUFFIX,
   },
+  "78432": {
+    supported: true,
+    monitored: false,
+    contractFetchAddress:
+      "https://glacier-api.avax.network/v1/chains/78432/" +
+      AVALANCHE_SUBNET_SUFFIX,
+  },
 };
 
 const sourcifyChainsMap: SourcifyChainMap = {};
