8c3c285f6e962b63920face3e715f86e0f33860d
dfenstermaker <dannyfenstermaker@gmail.com>
2023-09-11 08:31:01 -0600

    feat: add chain 2044 shrapnel mainnet

--- src/sourcify-chains.ts
+++ src/sourcify-chains.ts
@@ -1037,6 +1037,20 @@ const sourcifyChainsExtensions: SourcifyChainsExtensionsObject = {
       "https://glacier-api.avax.network/v1/chains/78430/" +
       AVALANCHE_SUBNET_SUFFIX,
   },
+  "2038": {
+    supported: true,
+    monitored: false,
+    contractFetchAddress:
+      "https://glacier-api.avax.network/v1/chains/2038/" +
+      AVALANCHE_SUBNET_SUFFIX,
+  },
+  "2044": {
+    supported: true,
+    monitored: false,
+    contractFetchAddress:
+      "https://glacier-api.avax.network/v1/chains/2044/" +
+      AVALANCHE_SUBNET_SUFFIX,
+  },
 };
 
 const sourcifyChainsMap: SourcifyChainMap = {};
