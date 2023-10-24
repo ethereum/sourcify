74364c57c305d5372fdf263c52f3437b9e436ecc
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2023-10-24 15:32:08 +0300

    Remove unrelated CI scripts

--- scripts/decrypt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gpg --yes --batch --passphrase=$SECRET_KEY ./environments/.env.secrets.gpg
-gpg --yes --batch --passphrase=$SECRET_KEY ./services/ipfs/server/ipfs-stable.key.gpg
-gpg --yes --batch --passphrase=$SECRET_KEY ./services/ipfs/server/ipfs-latest.key.gpg
--- scripts/encrypt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-gpg --yes --batch --passphrase=$SECRET_KEY -c ./environments/.env.secrets
-gpg --yes --batch --passphrase=$SECRET_KEY -c ./services/ipfs/server/ipfs-stable.key
-gpg --yes --batch --passphrase=$SECRET_KEY -c ./services/ipfs/server/ipfs-latest.key
-
