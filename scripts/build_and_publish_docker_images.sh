226cba4429e31c0eb9feafcd164349a5ac20801c
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2023-10-24 15:32:53 +0300

    Fix forgotten comments in build_publish script

--- scripts/build_and_publish_docker_images.sh
+++ scripts/build_and_publish_docker_images.sh
@@ -20,15 +20,15 @@ if [ "$SERVICE" == "ui" ]; then
     cp ui/.env.build.$TAG ui/.env
 fi
 
-# docker login --username $DOCKER_USER --password $DOCKER_PASS
+docker login --username $DOCKER_USER --password $DOCKER_PASS
 docker-compose -f environments/build-$SERVICE.yaml build
-# docker push ethereum/source-verify:$SERVICE-$TAG
+docker push ethereum/source-verify:$SERVICE-$TAG
 
 # Get the tag of the built image
-# image_tag=$(docker inspect --format='{{index .RepoDigests 0}}' ethereum/source-verify:$SERVICE-$TAG | cut -d'@' -f2)
-# echo "Image tag: $image_tag"
+image_tag=$(docker inspect --format='{{index .RepoDigests 0}}' ethereum/source-verify:$SERVICE-$TAG | cut -d'@' -f2)
+echo "Image tag: $image_tag"
 
-# mkdir -p workspace
-# echo "Writing image tag $image_tag to workspace/"$SERVICE"_image_tag.txt"
-# echo -n $image_tag > workspace/"$SERVICE"_image_tag.txt
-# echo "Written image tag $image_tag to workspace/"$SERVICE"_image_tag.txt"
+mkdir -p workspace
+echo "Writing image tag $image_tag to workspace/"$SERVICE"_image_tag.txt"
+echo -n $image_tag > workspace/"$SERVICE"_image_tag.txt
+echo "Written image tag $image_tag to workspace/"$SERVICE"_image_tag.txt"
