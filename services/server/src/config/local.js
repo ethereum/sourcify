9f2667c2e84c1c9749af13798d56966d380aaf87
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2023-12-15 13:11:13 +0100

    Remove local.js

--- services/server/src/config/local.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  repository: {
-    path: "/Users/kuzdogan/sourcify/data/repository",
-  },
-  solcRepo: "/Users/kuzdogan/sourcify/data/solc-bin/linux-amd64",
-  solJsonRepo: "/Users/kuzdogan/sourcify/data/solc-bin/soljson",
-};
