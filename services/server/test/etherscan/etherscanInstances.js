d2d0e2ce5893b9527be65b2e8b3cea8f45c94142
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2023-12-19 12:01:00 +0100

    Double check all etherscans tested

--- services/server/test/etherscan/etherscanInstances.js
+++ services/server/test/etherscan/etherscanInstances.js
@@ -3,9 +3,13 @@
 const Server = require("../../dist/server/server").Server;
 const rimraf = require("rimraf");
 const testContracts = require("../helpers/etherscanInstanceContracts.json");
-const { sourcifyChainsMap } = require("../../dist/sourcify-chains");
+const {
+  sourcifyChainsMap,
+  sourcifyChainsArray,
+} = require("../../dist/sourcify-chains");
 const util = require("util");
 const { verifyAndAssertEtherscan } = require("../helpers/helpers");
+const chai = require("chai");
 
 const CUSTOM_PORT = 5679;
 
@@ -27,8 +31,10 @@ describe("Test each Etherscan instance", function () {
     rimraf.sync(server.repository);
   });
 
+  let testedChains = [];
   for (const chainId in testContracts) {
     if (process.env.TEST_CHAIN && process.env.TEST_CHAIN !== chainId) continue;
+    testedChains.push(parseInt(chainId));
     describe(`#${chainId} ${sourcifyChainsMap[chainId].name}`, () => {
       testContracts[chainId].forEach((contract) => {
         verifyAndAssertEtherscan(
@@ -42,4 +48,26 @@ describe("Test each Etherscan instance", function () {
       });
     });
   }
+  describe("Double check that all supported chains are tested", () => {
+    const supportedEtherscanChains = sourcifyChainsArray.filter(
+      (chain) => chain.etherscanApi && chain.supported
+    );
+
+    it("should have tested all supported chains", function (done) {
+      const untestedChains = supportedEtherscanChains.filter(
+        (chain) => !testedChains.includes(chain.chainId)
+      );
+      if (process.env.TEST_CHAIN) {
+        return this.skip();
+      }
+      chai.assert(
+        untestedChains.length == 0,
+        `There are untested supported chains!: ${untestedChains
+          .map((chain) => `${chain.name} (${chain.chainId})`)
+          .join(", ")}`
+      );
+
+      done();
+    });
+  });
 });
