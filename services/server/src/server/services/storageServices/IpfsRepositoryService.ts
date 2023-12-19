43365b626406271d72357c44958c1599186dad9e
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2023-12-19 21:14:47 +0100

    Turn off IPFS MFS

--- services/server/src/server/services/storageServices/IpfsRepositoryService.ts
+++ services/server/src/server/services/storageServices/IpfsRepositoryService.ts
@@ -434,10 +434,7 @@ export class IpfsRepositoryService implements IStorageService {
       logger.info(
         `Stored ${contract.name} to filesystem address=${match.address} chainId=${match.chainId} match runtimeMatch=${match.runtimeMatch} creationMatch=${match.creationMatch}`
       );
-      await this.addToIpfsMfs(matchQuality, match.chainId, match.address);
-      logger.info(
-        `Stored ${contract.name} to IPFS MFS address=${match.address} chainId=${match.chainId} match runtimeMatch=${match.runtimeMatch} creationMatch=${match.creationMatch}`
-      );
+      // await this.addToIpfsMfs(matchQuality, match.chainId, match.address);
     } else if (match.runtimeMatch === "extra-file-input-bug") {
       return match;
     } else {
