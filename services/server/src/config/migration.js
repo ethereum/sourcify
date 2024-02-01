69044b2554211d90353d639109e263e4a70b24d6
Marco Castignoli <marco@castignoli.it>
2024-02-01 09:56:11 +0100

    add config file for new environment "migration"

---
+++ services/server/src/config/migration.js
@@ -0,0 +1,27 @@
+module.exports = {
+  server: {
+    port: 80,
+  },
+  repositoryV1: {
+    path: "/home/app/repositoryV1",
+    serverUrl: "https://repo.staging.sourcify.dev",
+  },
+  repositoryV2: {
+    path: "/home/app/repositoryV2",
+  },
+  solcRepo: "/home/app/compilers/solc",
+  solJsonRepo: "/home/app/compilers/soljson",
+  session: {
+    secure: true, // Set Secure in the Set-Cookie header i.e. require https
+  },
+  lambdaCompiler: {
+    enabled: false,
+    functionName: "compile",
+    // credentials as env vars
+  },
+  rateLimit: {
+    enabled: true,
+    windowMs: 1 * 1000, // 1 sec
+    max: 2,
+  },
+};
