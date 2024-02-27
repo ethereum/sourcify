1d76b5fea4937462bbdeb89cd10020eb2be6cc88
Carter Carlson <pegahcarter@gmail.com>
2024-02-27 13:35:23 -0800

    feat: fraxtal mainnet and testnet

--- services/server/test/chains/chain-tests.js
+++ services/server/test/chains/chain-tests.js
@@ -61,8 +61,8 @@ describe("Test Supported Chains", function () {
     if (!anyTestsPass && newAddedChainIds.length) {
       throw new Error(
         "There needs to be at least one passing test. Did you forget to add a test for your new chain with the id(s) " +
-          newAddedChainIds.join(",") +
-          "?"
+        newAddedChainIds.join(",") +
+        "?"
       );
     }
   });
@@ -799,6 +799,21 @@ describe("Test Supported Chains", function () {
     "8453",
     "Base Mainnet",
     "shared/"
+  );
+
+  // Fraxtal
+  verifyContract(
+    "0xEe44D634f97d8eE09850Ed04559E068D30276FE7",
+    "252",
+    "Fraxtal",
+    "252/"
+  );
+
+  verifyContract(
+    "0x31D982ebd82Ad900358984bd049207A4c2468640",
+    "2522",
+    "Fraxtal Testnet",
+    "252/"
   );
 
   // Wanchain Mainnet
