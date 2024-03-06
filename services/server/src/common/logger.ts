a8ec0fe73b393d28ffc6624d031dbd18ab2cecf1
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2024-03-06 18:05:06 +0100

    Serialize objects in logs, add NODE_LOG_LEVEL env, add setLogLevel func

--- services/server/src/common/logger.ts
+++ services/server/src/common/logger.ts
@@ -1,16 +1,24 @@
-import { createLogger, transports, format } from "winston";
+import { log } from "console";
+import { createLogger, transports, format, Logger } from "winston";
 
-const loggerInstance = createLogger();
+const loggerInstance: Logger = createLogger();
 
 const rawlineFormat = format.printf(
-  ({ level, message, timestamp, ...metadata }) => {
-    let msg = `${timestamp} [${level}]: ${message}`;
+  ({ level, message, timestamp, ...metadata }: any) => {
+    let msg = `${timestamp} [${level}]: ${message} `;
     if (Object.keys(metadata).length > 0) {
-      msg +=
-        " - " +
-        Object.entries(metadata)
-          .map(([key, value]) => `${key}=${value}`)
-          .join(" ");
+      msg += Object.entries(metadata)
+        .map(([key, value]) => {
+          if (typeof value === "object") {
+            try {
+              value = JSON.stringify(value);
+            } catch (e) {
+              value = "SerializationError: Unable to serialize object";
+            }
+          }
+          return `${key}=${value}`;
+        })
+        .join(", ");
     }
     return msg;
   }
@@ -24,11 +32,22 @@ const lineFormat = format.combine(
 
 const jsonFormat = format.combine(format.timestamp(), format.json());
 
-loggerInstance.add(
-  new transports.Console({
-    level: process.env.NODE_ENV === "production" ? "info" : "debug",
-    format: process.env.NODE_ENV === "production" ? jsonFormat : lineFormat,
-  })
-);
+const consoleTransport = new transports.Console({
+  // NODE_LOG_LEVEL is takes precedence, otherwise use "info" if in production, "debug" otherwise
+  level:
+    process.env.NODE_LOG_LEVEL ||
+    (process.env.NODE_ENV === "production" ? "info" : "debug"),
+  format: process.env.NODE_ENV === "production" ? jsonFormat : lineFormat,
+});
+
+console.log(consoleTransport.level);
+loggerInstance.add(consoleTransport);
 
 export const logger = loggerInstance;
+
+// Function to change the log level dynamically
+export function setLogLevel(level: string): void {
+  console.log(`Setting log level to: ${level}`);
+  logger.warn(`Setting log level to: ${level}`);
+  consoleTransport.level = level;
+}
