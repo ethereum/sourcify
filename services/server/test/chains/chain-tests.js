94ed9ab773ad3dfec73c067f156a68f195ef198d
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2024-03-28 10:14:44 +0100

    Fix Fraxnet test metadata and linting

--- services/server/test/chains/chain-tests.js
+++ services/server/test/chains/chain-tests.js
@@ -61,8 +61,8 @@ describe("Test Supported Chains", function () {
     if (!anyTestsPass && newAddedChainIds.length) {
       throw new Error(
         "There needs to be at least one passing test. Did you forget to add a test for your new chain with the id(s) " +
-        newAddedChainIds.join(",") +
-        "?"
+          newAddedChainIds.join(",") +
+          "?"
       );
     }
   });
@@ -806,14 +806,16 @@ describe("Test Supported Chains", function () {
     "0xEe44D634f97d8eE09850Ed04559E068D30276FE7",
     "252",
     "Fraxtal",
-    "252/"
+    "252/",
+    "partial"
   );
 
   verifyContract(
     "0x31D982ebd82Ad900358984bd049207A4c2468640",
     "2522",
     "Fraxtal Testnet",
-    "252/"
+    "252/",
+    "partial"
   );
 
   // Wanchain Mainnet
