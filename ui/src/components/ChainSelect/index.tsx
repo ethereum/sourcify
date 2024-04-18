d81f5c550b62c7da6ec0af668c4421551c7f61e7
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2024-04-18 10:19:53 +0200

    Show chain.title before chain.name

--- ui/src/components/ChainSelect/index.tsx
+++ ui/src/components/ChainSelect/index.tsx
@@ -34,7 +34,7 @@ export default function ChainSelect({
       onChange={handleChainIdChange}
       value={value}
       options={filteredChains.map((chain) => ({
-        name: `${chain.name || chain.title} (${chain.chainId}) `,
+        name: `${chain.title || chain.name} (${chain.chainId}) `,
         value: chain.chainId,
       }))}
       search
