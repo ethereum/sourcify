0da486512142d2cf573e3f9662eb7315a4b9e8f6
Ian <ianholtz@gmail.com>
2024-05-22 17:54:20 +0800

    PlayFair Testnet Subnet

--- services/server/test/chains/chain-tests.spec.ts
+++ services/server/test/chains/chain-tests.spec.ts
@@ -1490,6 +1490,7 @@ describe("Test Supported Chains", function () {
     "shared/"
   );
 
+
   it("should have included Etherscan contracts for all testedChains having etherscanAPI", function (done) {
     const missingEtherscanTests: ChainApiResponse[] = [];
     supportedChains
