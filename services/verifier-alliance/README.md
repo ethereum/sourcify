502cccafc6604a0ff1fb7408d3a5c3e6969bd697
Marco Castignoli <marco@castignoli.it>
2024-05-22 17:39:55 +0200

    Add script to sync VerA to Sourcify

--- services/verifier-alliance/README.md
+++ services/verifier-alliance/README.md
@@ -2,6 +2,16 @@
 
 This script is needed to push all verified_contracts from one Sourcify to VerA database. VerA is a live database in which other entities are writing while the script is running.
 
+## Configure
+
+1. Copy and paste `.env.template` to `.env` and fill it appropriately.
+
+## Run
+
+`npm run vera:push`
+
+## How it works
+
 0. CURRENT_VERIFIED_CONTRACT is a variable stored in a permanent file, default 1.
 1. Extract N verified_contracts starting from CURRENT_VERIFIED_CONTRACT
 2. For each verified_contract:
@@ -9,3 +19,33 @@ This script is needed to push all verified_contracts from one Sourcify to VerA d
    b. Get all information from verified_contracts
    c. Get all the FK information
    d. Insert all the verified_contracts dependencies (compiled_contracts,contract_deployments,contracts,code) and verified_contracts using the new ids as FKs
+
+# Verifier Alliance -> Sourcify sync
+
+This daemon listens for `new_verified_contract` PostgreSQL notification and sends requests to sourcify-server.
+
+## Configuration
+
+1. Run the following SQL queries on the Verifier Alliance database to set up the notification.
+
+   ```sql
+   CREATE OR REPLACE FUNCTION notify_new_verified_contract()
+   RETURNS TRIGGER AS $$
+   BEGIN
+      PERFORM pg_notify('new_verified_contract', row_to_json(NEW)::text);
+      RETURN NEW;
+   END;
+   $$ LANGUAGE plpgsql;
+
+
+   CREATE TRIGGER new_verified_contract_trigger
+   AFTER INSERT ON verified_contracts
+   FOR EACH ROW
+   EXECUTE FUNCTION notify_new_verified_contract();
+   ```
+
+2. Copy-paste `.env.template` to `.env` and fill it appropriately.
+
+## Run
+
+`npm run vera:pull`
---
+++ services/verifier-alliance/pullFromVera.mjs
@@ -0,0 +1,92 @@
+import pg from "pg";
+import createSubscriber from "pg-listen";
+import dotenv from "dotenv";
+import fetch from "node-fetch";
+
+dotenv.config();
+const { Client } = pg;
+
+const veraClient = new Client({
+  host: process.env.VERA_HOST,
+  database: process.env.VERA_DB,
+  user: process.env.VERA_USER,
+  password: process.env.VERA_PASSWORD,
+  port: process.env.VERA_PORT,
+});
+
+const subscriber = createSubscriber({
+  host: process.env.VERA_HOST,
+  port: parseInt(process.env.VERA_PORT) || 5432,
+  database: process.env.VERA_DB,
+  user: process.env.VERA_USER,
+  password: process.env.VERA_PASSWORD,
+});
+
+async function main() {
+  await veraClient.connect();
+
+  subscriber.notifications.on("new_verified_contract", async (payload) => {
+    console.log("Received notification in 'new_verified_contract':", payload);
+    // Get all FK information
+    const {
+      rows: [deployment],
+    } = await veraClient.query(
+      "SELECT * FROM contract_deployments WHERE id = $1",
+      [payload.deployment_id]
+    );
+    const {
+      rows: [compilation],
+    } = await veraClient.query(
+      "SELECT * FROM compiled_contracts WHERE id = $1",
+      [payload.compilation_id]
+    );
+
+    // For some reason inside `compilation.compiler_settings` there is a compilationTarget parameter that is not supported by solc
+    const settings = compilation.compiler_settings;
+    delete settings.compilationTarget;
+
+    const settingsJson = JSON.stringify({
+      language: "Solidity",
+      sources: Object.keys(compilation.sources).reduce((obj, current) => {
+        obj[current] = {
+          content: compilation.sources[current],
+        };
+        return obj;
+      }, {}),
+      settings: compilation.compiler_settings,
+    });
+
+    console.log(
+      "0x" + deployment.address.toString("hex"),
+      deployment.chain_id,
+      settingsJson
+    );
+    try {
+      const res = await fetch("http://localhost:5555/verify/solc-json", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          compilerVersion: compilation.version,
+          contractName: compilation.name,
+          address: "0x" + deployment.address.toString("hex"),
+          chainId: deployment.chain_id,
+          files: {
+            "settings.json": settingsJson,
+          },
+        }),
+      });
+
+      console.log(await res.json());
+    } catch (e) {
+      console.error(e);
+    }
+  });
+
+  subscriber.connect();
+  console.log("Started listening for VerA verified_contracts...");
+  subscriber.listenTo("new_verified_contract");
+}
+
+main();
