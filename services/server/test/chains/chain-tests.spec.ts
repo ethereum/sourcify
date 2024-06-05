3c0317b8c84e590461efadf1067067c0ffeb2ef9
musclemikiya <musclemikiya@users.noreply.github.com>
2024-06-05 19:46:35 +0900

    add HOME Verse Mainnet (#1421)
    Co-authored-by: Manuel Wedler <34456797+manuelwedler@users.noreply.github.com>

--- services/server/test/chains/chain-tests.spec.ts
+++ services/server/test/chains/chain-tests.spec.ts
@@ -1491,6 +1491,14 @@ describe("Test Supported Chains", function () {
     "shared/"
   );
 
+  // HOME Verse Mainnet
+  verifyContract(
+    "0x988d819a6103f0a9a693BE7Ffd90fe7b499592f5",
+    "19011",
+    "HOME Verse Mainnet",
+    "shared/"
+  );
+  
   // Lamina1
   verifyContract(
     "0x61ACA050011D091A5790de774190cCdD42Ab882F",
