878d496e475e5ce157b5c6371b3e19889b741cde
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2024-07-29 14:18:55 +0200

    Deploy images to GCP via CI

---
+++ .circleci/deploy_to_gcp.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Checks the new tag of the built image for each service (ui, server, monitor, repository)
+# The tag value is persisted in worspace/{service}_image_sha.txt by each respective build job
+# If a new image is built with a new tag, a deploy trigger is sent to the sourcifyeth/infra repo
+
+
+# Define the list of services
+services=("server" "monitor" "repository")
+
+ARTIFACT_REGISTRY_URL = "europe-west1-docker.pkg.dev/sourcify-project/ghcr-proxy/ethereum/sourcify/"
+
+if [ "$CIRCLE_BRANCH" == "staging" ]; then 
+    ENVIRONMENT='staging'
+elif [ "$CIRCLE_BRANCH" == "master" ]; then
+    ENVIRONMENT='production'
+else
+    echo "Invalid branch $CIRCLE_BRANCH. Check your config.yml"
+    exit 1
+fi
+
+# Loop through each service
+for service in "${services[@]}"; do
+    # Deploy from amd64 image
+    filePath="workspace/${service}_amd64_image_sha.txt"
+
+    if [ -f "$filePath" ]; then
+        echo "File $filePath exists."
+
+        image_sha=$(cat "$filePath")
+
+        # Check if the content is not an empty string
+        if [ -n "$image_sha" ]; then
+            echo "File is not empty."
+            
+            image_tag="$CIRCLE_BRANCH"@"$image_sha"
+
+            # sourcify-staging-server or sourcify-production-server
+            cmd="gcloud run deploy sourcify-$ENVIRONMENT-$service --image $ARTIFACT_REGISTRY_URL$service:$image_tag"
+
+            echo "Running cmd"
+            echo $cmd
+
+            # Execute the command
+            eval $cmd
+
+            echo "Deployed with gcloud using:"
+            echo $cmd
+        else
+            echo "File for $service is empty."
+            exit 1;
+        fi
+    else
+        echo "File $filePath does not exist."
+    fi
+done
