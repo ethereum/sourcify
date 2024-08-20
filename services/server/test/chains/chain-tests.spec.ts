9db1601725c286f0090a0d715a924e3ab9840651
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2024-08-20 16:05:05 +0200

    in `readFilesRecursively` use absolute path instead as file key

--- services/server/test/chains/chain-tests.spec.ts
+++ services/server/test/chains/chain-tests.spec.ts
@@ -1731,22 +1731,16 @@ describe("Test Supported Chains", function () {
 function readFilesRecursively(
   directoryPath: string,
   files: Record<string, string>,
-  baseDirectory?: string,
 ) {
-  // If baseDirectory is not provided, set it to directoryPath
-  baseDirectory = baseDirectory || directoryPath;
-
   const filesInDirectory = fs.readdirSync(directoryPath);
 
   filesInDirectory.forEach((file) => {
     const filePath = path.join(directoryPath, file);
 
     if (fs.statSync(filePath).isDirectory()) {
-      readFilesRecursively(filePath, files, baseDirectory);
+      readFilesRecursively(filePath, files);
     } else {
-      // Use the relativePath as key instead of `file` which is just the filename. Needed when there are files with the same name in the directory.
-      const relativeFilePath = path.relative(baseDirectory, filePath);
-      files[relativeFilePath] = fs.readFileSync(filePath).toString();
+      files[filePath] = fs.readFileSync(filePath).toString();
     }
   });
 }
