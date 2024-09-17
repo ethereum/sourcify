2555e19d77a563a27b28b1b330c59f09feda91e2
Marco Castignoli <marco@ethereum.org>
2024-09-17 15:23:54 +0200

    Fix the check for already existing partial match (#1625)
    * Fix the check for already existing partial match
    
    * simplify `getStatusDiff`; add additional information to "Partial match already exists" log
    
    * move `isBetterMatch` to util file
    
    * implement test for upgrading creation match while runtime match is "perfect"
    
    * refactor tests to check specific matchType upgrades
    
    * refactor middleware to check for: already verified contract, validate address, safeHandler
    
    * fix lint
    
    * add missing safeHandler
    
    * add test to check verifyDeployed is not called if there is an already existing perfect match
    
    * add sinon to server's devDependencies

--- services/server/src/server/services/utils/util.ts
+++ services/server/src/server/services/utils/util.ts
@@ -2,6 +2,7 @@ import Path from "path";
 import fs from "fs";
 import { MatchLevelWithoutAny, MatchQuality } from "../../types";
 import { getAddress } from "ethers";
+import { Match, Status } from "@ethereum-sourcify/lib-sourcify";
 
 export const getFileRelativePath = (
   chainId: string,
@@ -52,4 +53,46 @@ export async function readFile(
   } catch (error) {
     return false;
   }
+}
+
+/**
+ * This function returns a positive number if the first Status
+ * is better than the second one; 0 if they are the same; or a
+ * negative number if the first Status is worse than the second one
+ */
+export function getStatusDiff(status1: Status, status2: Status): number {
+  const scores = {
+    error: 0,
+    "extra-file-input-bug": 0,
+    partial: 1,
+    perfect: 2,
+  };
+  const status1Value = status1 != null ? scores[status1] : 0;
+  const status2Value = status2 != null ? scores[status2] : 0;
+  return status1Value - status2Value;
+}
+
+/**
+ * Verify that either the newMatch runtime or creation match is better
+ * ensuring that neither the newMatch runtime nor creation is worse
+ * than the existing match
+ */
+export function isBetterMatch(newMatch: Match, existingMatch: Match): boolean {
+  if (
+    /** if newMatch.creationMatch is better */
+    getStatusDiff(newMatch.creationMatch, existingMatch.creationMatch) > 0 &&
+    /** and newMatch.runtimeMatch is not worse */
+    getStatusDiff(newMatch.runtimeMatch, existingMatch.runtimeMatch) >= 0
+  ) {
+    return true;
+  }
+  if (
+    /** if newMatch.runtimeMatch is better */
+    getStatusDiff(newMatch.runtimeMatch, existingMatch.runtimeMatch) > 0 &&
+    /** and newMatch.creationMatch is not worse */
+    getStatusDiff(newMatch.creationMatch, existingMatch.creationMatch) >= 0
+  ) {
+    return true;
+  }
+  return false;
 }
