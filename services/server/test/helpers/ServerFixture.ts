ca9709d044f747f2fd943631d0828520389ad0f2
Raoul Schaffranek <raoul.schaffranek@runtimeverification.com>
2024-09-20 12:05:40 +0200

    Fix type errors in test cases.

--- services/server/test/helpers/ServerFixture.ts
+++ services/server/test/helpers/ServerFixture.ts
@@ -3,13 +3,24 @@ import { resetDatabase } from "../helpers/helpers";
 import { Server } from "../../src/server/server";
 import config from "config";
 import http from "http";
-import { supportedChainsMap } from "../../src/sourcify-chains";
+import { sourcifyChainsMap } from "../../src/sourcify-chains";
 import {
   RWStorageIdentifiers,
   StorageIdentifiers,
 } from "../../src/server/services/storageServices/identifiers";
 import { Pool } from "pg";
 import { SourcifyDatabaseService } from "../../src/server/services/storageServices/SourcifyDatabaseService";
+import { ChainRepository } from "../../src/sourcify-chain-repository";
+import path from "path";
+import { ISolidityCompiler } from "@ethereum-sourcify/lib-sourcify";
+import logger from "../../src/common/logger";
+import { SolcLambdaWithLocalFallback } from "../../src/server/services/compiler/lambda-with-fallback/SolcLambdaWithLocalFallback";
+import { SolcLocal } from "../../src/server/services/compiler/local/SolcLocal";
+import session from "express-session";
+import genFunc from "connect-pg-simple";
+import createMemoryStore from "memorystore";
+
+const supportedChainsMap = new ChainRepository(sourcifyChainsMap).supportedChainMap;
 
 export type ServerFixtureOptions = {
   port: number;
@@ -77,9 +88,55 @@ export class ServerFixture {
         throw new Error("Not all required environment variables set");
       }
 
+      // Set up the solidity compiler
+      const solcRepoPath =
+        (config.get("solcRepo") as string) || path.join("/tmp", "solc-repo");
+      const solJsonRepoPath =
+        (config.get("solJsonRepo") as string) || path.join("/tmp", "soljson-repo");
+      
+      let selectedSolidityCompiler: ISolidityCompiler;
+      if (config.get("lambdaCompiler.enabled")) {
+        logger.info("Using lambda solidity compiler with local fallback");
+        if (
+          process.env.AWS_REGION === undefined ||
+          process.env.AWS_ACCESS_KEY_ID === undefined ||
+          process.env.AWS_SECRET_ACCESS_KEY === undefined
+        ) {
+          throw new Error(
+            "AWS credentials not set. Please set them to run the compiler on AWS Lambda.",
+          );
+        }
+        selectedSolidityCompiler = new SolcLambdaWithLocalFallback(
+          process.env.AWS_REGION as string,
+          process.env.AWS_ACCESS_KEY_ID as string,
+          process.env.AWS_SECRET_ACCESS_KEY as string,
+          config.get("lambdaCompiler.functionName"),
+          solcRepoPath,
+          solJsonRepoPath,
+        );
+      } else {
+        logger.info("Using local solidity compiler");
+        selectedSolidityCompiler = new SolcLocal(solcRepoPath, solJsonRepoPath);
+      }
+    
+      const solc = selectedSolidityCompiler;
+
       this._server = new Server(
-        options.port!,
-        { supportedChainsMap },
+        {
+          port: options.port,
+          maxFileSize: this.maxFileSize,
+          rateLimit: config.get("rateLimit"),
+          corsAllowedOrigins: config.get("corsAllowedOrigins"),
+          solc,
+          chains: sourcifyChainsMap,
+          verifyDeprecated: config.get("verifyDeprecated"),
+          sessionOptions: getSessionOptions(),
+        },
+        {
+          supportedChainsMap,
+          solcRepoPath,
+          solJsonRepoPath,
+        },
         {
           enabledServices: {
             read: options.read,
@@ -119,8 +176,8 @@ export class ServerFixture {
     });
 
     beforeEach(async () => {
-      rimraf.sync(this.server.repository);
-      rimraf.sync(this.server.repositoryV2);
+      rimraf.sync(config.get('repositoryV1.path'));
+      rimraf.sync(config.get('repositoryV2.path'));
       if (!options.skipDatabaseReset) {
         await resetDatabase(this.sourcifyDatabase);
         console.log("Resetting SourcifyDatabase");
@@ -129,8 +186,102 @@ export class ServerFixture {
 
     after(() => {
       httpServer.close();
-      rimraf.sync(this.server.repository);
-      rimraf.sync(this.server.repositoryV2);
+      rimraf.sync(config.get('repositoryV1.path'));
+      rimraf.sync(config.get('repositoryV2.path'));
     });
   }
+}
+
+function initMemoryStore() {
+  const MemoryStore = createMemoryStore(session);
+
+  logger.warn(
+    "Using memory based session. Don't use memory session in production!",
+  );
+  return new MemoryStore({
+    checkPeriod: config.get("session.maxAge"),
+  });
+}
+
+function initDatabaseStore() {
+  const pool = new Pool({
+    host: process.env.SOURCIFY_POSTGRES_HOST,
+    database: process.env.SOURCIFY_POSTGRES_DB,
+    user: process.env.SOURCIFY_POSTGRES_USER,
+    password: process.env.SOURCIFY_POSTGRES_PASSWORD,
+    port: parseInt(process.env.SOURCIFY_POSTGRES_PORT || "5432"),
+  });
+
+  // This listener is necessary otherwise the sourcify process crashes if the database is closed
+  pool.prependListener("error", (e) => {
+    logger.error("Database connection lost for session pool", {
+      error: e,
+    });
+    throw new Error("Database connection lost for session pool");
+  });
+
+  const PostgresqlStore = genFunc(session);
+
+  logger.info("Using database based session");
+  return new PostgresqlStore({
+    pool: pool,
+    // Pruning expired sessions every 12 hours
+    pruneSessionInterval: 12 * 60 * 60,
+  });
+}
+
+function getSessionStore() {
+  const sessionStoreType = config.get("session.storeType");
+
+  switch (sessionStoreType) {
+    case "database": {
+      if (
+        process.env.SOURCIFY_POSTGRES_HOST &&
+        process.env.SOURCIFY_POSTGRES_DB &&
+        process.env.SOURCIFY_POSTGRES_USER &&
+        process.env.SOURCIFY_POSTGRES_PASSWORD
+      ) {
+        return initDatabaseStore();
+      } else {
+        logger.error(
+          "Database session enabled in config but the environment variables are not specified",
+        );
+        throw new Error(
+          "Database session enabled in config but the environment variables are not specified",
+        );
+      }
+    }
+
+    case "memory": {
+      return initMemoryStore();
+    }
+    default:
+      // Throw an error if an unrecognized session storage type is selected
+      throw new Error(
+        `Selected session storage type '${sessionStoreType}' doesn't exist.`,
+      );
+  }
+}
+
+function getSessionOptions(): session.SessionOptions {
+  if (config.get("session.secret") === "CHANGE_ME") {
+    const msg =
+      "The session secret is not set, please set it in the config file";
+    process.env.NODE_ENV === "production"
+      ? logger.error(msg)
+      : logger.warn(msg);
+  }
+  return {
+    secret: config.get("session.secret"),
+    name: "sourcify_vid",
+    rolling: true,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: config.get("session.maxAge"),
+      secure: config.get("session.secure"),
+      sameSite: "lax",
+    },
+    store: getSessionStore(),
+  };
 }
