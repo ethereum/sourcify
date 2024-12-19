90c6ee9eea910bdbb43ace1409937586df371b60
Manuel Wedler <manuel@wedler.dev>
2024-12-19 17:56:35 +0100

    Add verify endpoint tests for auxdata transformation with wrong vyper version

--- services/server/test/integration/verification-handlers/vyper.stateless.spec.ts
+++ services/server/test/integration/verification-handlers/vyper.stateless.spec.ts
@@ -189,4 +189,104 @@ describe("/verify/vyper", function () {
       },
     );
   });
+
+  it("should have an auxdata transformation for a contract with a wrong cbor auxdata on Vyper 0.3.8", async () => {
+    const creationCodeWithWrongVyperVersion =
+      "0x6100a361000f6000396100a36000f360003560e01c346100915763c605f76c811861008a57602080608052600c6040527f48656c6c6f20576f726c6421000000000000000000000000000000000000000060605260408160800181516020830160208301815181525050808252508051806020830101601f82600003163682375050601f19601f8251602001011690509050810190506080f35b5060006000fd5b600080fda165767970657283000307000b";
+    const { contractAddress: address, txHash: creatorTxHash } =
+      await deployFromAbiAndBytecodeForCreatorTxHash(
+        chainFixture.localSigner,
+        contractArtifact.abi,
+        creationCodeWithWrongVyperVersion,
+      );
+
+    const res = await verifyVyperTestContract(address, {
+      compilerVersion: "0.3.8+commit.036f1536",
+      compilerSettings: {
+        evmVersion: "istanbul",
+        outputSelection: {
+          "*": ["evm.bytecode"],
+        },
+      },
+      creatorTxHash,
+    });
+
+    await assertVerification(
+      serverFixture,
+      null,
+      res,
+      null,
+      address,
+      chainFixture.chainId,
+      "partial",
+    );
+
+    await assertTransformations(
+      serverFixture.sourcifyDatabase,
+      address,
+      chainFixture.chainId,
+      [AuxdataTransformation(150, "1")],
+      {
+        cborAuxdata: {
+          "1": "0xa165767970657283000307000b",
+        },
+      },
+      [AuxdataTransformation(165, "1")],
+      {
+        cborAuxdata: {
+          "1": "0xa165767970657283000307000b",
+        },
+      },
+    );
+  });
+
+  it("should have an auxdata transformation for a contract with a wrong cbor auxdata on Vyper 0.3.4", async () => {
+    const creationCodeWithWrongVyperVersion =
+      "0x6100b761000f6000396100b76000f36003361161000c576100a1565b60003560e01c346100a75763c605f76c811861009f57600436186100a757602080608052600c6040527f48656c6c6f20576f726c6421000000000000000000000000000000000000000060605260408160800181518082526020830160208301815181525050508051806020830101601f82600003163682375050601f19601f8251602001011690509050810190506080f35b505b60006000fd5b600080fda165767970657283000303";
+    const { contractAddress: address, txHash: creatorTxHash } =
+      await deployFromAbiAndBytecodeForCreatorTxHash(
+        chainFixture.localSigner,
+        contractArtifact.abi,
+        creationCodeWithWrongVyperVersion,
+      );
+
+    const res = await verifyVyperTestContract(address, {
+      compilerVersion: "0.3.4+commit.f31f0ec4",
+      compilerSettings: {
+        evmVersion: "istanbul",
+        outputSelection: {
+          "*": ["evm.bytecode"],
+        },
+      },
+      creatorTxHash,
+    });
+
+    await assertVerification(
+      serverFixture,
+      null,
+      res,
+      null,
+      address,
+      chainFixture.chainId,
+      "partial",
+    );
+
+    await assertTransformations(
+      serverFixture.sourcifyDatabase,
+      address,
+      chainFixture.chainId,
+      [AuxdataTransformation(172, "1")],
+      {
+        cborAuxdata: {
+          "1": "0xa165767970657283000303",
+        },
+      },
+      [AuxdataTransformation(187, "1")],
+      {
+        cborAuxdata: {
+          "1": "0xa165767970657283000303",
+        },
+      },
+    );
+  });
 });
