e2094dd254e7892c133f401e0ee8ab155b95d9e2
Marco Castignoli <marco@ethereum.org>
2025-01-22 10:02:00 +0100

    Add Dune upload script (#1866)
    * init dune folder
    
    * Add DuneClients
    
    * Create tables, fetch, format and insert rows for all tables
    
    * Add custom Dune types
    
    * Clean up unchaned rows in format functions
    
    * Implement functionalities to allow sync in batch (#1861)
    
    * Implement functionalities to allow sync in batch
    
    * Refactor database fetching functions to use lastValue for pagination instead of page number; update SourcifyDuneSyncClient to accommodate new pagination logic and improve data fetching efficiency.
    
    * Fix logic in SourcifyDuneSyncClient to ensure proper handling of data response; added missing else clause for logging inserted rows. This improves clarity and correctness in the data insertion process.
    
    * change page size
    
    * change page size
    
    * add delay after table creation
    
    * add README
    
    * remove unused code
    
    ---------
    
    Co-authored-by: Kaan Uzdoğan <kaanuzdogan@hotmail.com>

---
+++ services/database/dune/DuneDataClient.ts
@@ -0,0 +1,178 @@
+/**
+ * Data types when inserting into Dune
+ */
+
+type DuneVarbinary = string;
+type DuneBigint = string;
+type DuneObject = string;
+
+export interface InsertData {
+  code: Array<{
+    code_hash: DuneVarbinary;
+    code: DuneVarbinary | null;
+    code_hash_keccak: DuneVarbinary;
+    created_at: Date;
+    updated_at: Date;
+    created_by: string;
+    updated_by: string;
+  }>;
+  compiled_contracts: Array<{
+    id: string;
+    created_at: Date;
+    updated_at: Date;
+    created_by: string;
+    updated_by: string;
+    compiler: string;
+    version: string;
+    language: string;
+    name: string;
+    fully_qualified_name: string;
+    compiler_settings: DuneObject;
+    compilation_artifacts: DuneObject;
+    creation_code_hash: DuneVarbinary;
+    creation_code_artifacts: DuneObject;
+    runtime_code_hash: DuneVarbinary;
+    runtime_code_artifacts: DuneObject;
+  }>;
+  compiled_contracts_sources: Array<{
+    id: string;
+    compilation_id: string;
+    source_hash: DuneVarbinary;
+    path: string;
+  }>;
+  contract_deployments: Array<{
+    id: string;
+    chain_id: DuneBigint;
+    address: DuneVarbinary;
+    transaction_hash: DuneVarbinary | null;
+    block_number: DuneBigint | null;
+    transaction_index: number | null;
+    deployer: DuneVarbinary | null;
+    contract_id: string;
+    created_at: Date;
+    updated_at: Date;
+    created_by: string;
+    updated_by: string;
+  }>;
+  contracts: Array<{
+    id: string;
+    creation_code_hash: DuneVarbinary | null;
+    runtime_code_hash: DuneVarbinary;
+    created_at: Date;
+    updated_at: Date;
+    created_by: string;
+    updated_by: string;
+  }>;
+  sources: Array<{
+    source_hash: DuneVarbinary;
+    source_hash_keccak: DuneVarbinary;
+    content: DuneVarbinary;
+    created_at: Date;
+    updated_at: Date;
+    created_by: string;
+    updated_by: string;
+  }>;
+  sourcify_matches: Array<{
+    id: DuneBigint;
+    verified_contract_id: DuneBigint;
+    creation_match: DuneVarbinary | null;
+    runtime_match: DuneVarbinary | null;
+    created_at: Date;
+    metadata: DuneObject;
+  }>;
+  verified_contracts: Array<{
+    id: DuneBigint;
+    created_at: Date;
+    updated_at: Date;
+    created_by: string;
+    updated_by: string;
+    deployment_id: string;
+    compilation_id: string;
+    creation_match: boolean;
+    creation_values: DuneObject | null;
+    creation_transformations: DuneObject | null;
+    runtime_match: boolean;
+    runtime_values: DuneObject | null;
+    runtime_transformations: DuneObject | null;
+    runtime_metadata_match: boolean | null;
+    creation_metadata_match: boolean | null;
+  }>;
+}
+
+export default class DuneClient {
+  private readonly baseUrl = "https://api.dune.com/api/v1/table";
+  private readonly headers: { [key: string]: string };
+  private readonly namespace = "sourcify_team";
+
+  constructor(apiKey: string) {
+    if (!apiKey) {
+      throw new Error("API key is required");
+    }
+
+    this.headers = {
+      "x-dune-api-key": apiKey,
+      "Content-Type": "application/x-ndjson",
+    };
+  }
+
+  async insertCode(code: InsertData["code"]) {
+    return this.prepareInsertRequest(code, "code");
+  }
+
+  async insertCompiledContracts(
+    compiledContracts: InsertData["compiled_contracts"],
+  ) {
+    return this.prepareInsertRequest(compiledContracts, "compiled_contracts");
+  }
+
+  async insertCompiledContractsSources(
+    compiledContractsSources: InsertData["compiled_contracts_sources"],
+  ) {
+    return this.prepareInsertRequest(
+      compiledContractsSources,
+      "compiled_contracts_sources",
+    );
+  }
+
+  async insertContractDeployments(
+    contractDeployments: InsertData["contract_deployments"],
+  ) {
+    return this.prepareInsertRequest(
+      contractDeployments,
+      "contract_deployments",
+    );
+  }
+
+  async insertContracts(contracts: InsertData["contracts"]) {
+    return this.prepareInsertRequest(contracts, "contracts");
+  }
+
+  async insertSources(sources: InsertData["sources"]) {
+    return this.prepareInsertRequest(sources, "sources");
+  }
+
+  async insertSourcifyMatches(sourcifyMatches: InsertData["sourcify_matches"]) {
+    return this.prepareInsertRequest(sourcifyMatches, "sourcify_matches");
+  }
+
+  async insertVerifiedContracts(
+    verifiedContracts: InsertData["verified_contracts"],
+  ): Promise<Response> {
+    return this.prepareInsertRequest(verifiedContracts, "verified_contracts");
+  }
+
+  private prepareInsertRequest(data: any, tableName: string) {
+    return fetch(
+      `${this.baseUrl}/${this.namespace}/${tableName}/insert`,
+      this.prepareRequestOptions(data),
+    );
+  }
+
+  private prepareRequestOptions(data: any) {
+    return {
+      method: "POST",
+      headers: this.headers,
+      body: data.map((row: any) => JSON.stringify(row)).join("\n"), // Must send as newline delimited json (NDJSON)
+    };
+  }
+}
---
+++ services/database/dune/SourcifyDuneSyncClient.ts
@@ -0,0 +1,255 @@
+import DuneClient from "./DuneDataClient";
+import DuneTableClient from "./DuneTableClient";
+import { countTotalRows } from "./fetchRows";
+import {
+  fetchCode,
+  fetchCompiledContracts,
+  fetchCompiledContractsSources,
+  fetchContractDeployments,
+  fetchContracts,
+  fetchSources,
+  fetchSourcifyMatches,
+  fetchVerifiedContracts,
+} from "./fetchRows";
+import {
+  formatCode,
+  formatCompiledContracts,
+  formatCompiledContractsSources,
+  formatContractDeployments,
+  formatContracts,
+  formatSources,
+  formatSourcifyMatches,
+  formatVerifiedContracts,
+} from "./formatRows";
+
+export class SourcifyDuneSyncClient {
+  private duneTableClient: DuneTableClient;
+  private duneDataClient: DuneClient;
+
+  constructor(apiKey: string) {
+    this.duneTableClient = new DuneTableClient(apiKey);
+    this.duneDataClient = new DuneClient(apiKey);
+  }
+
+  private getTableFunctions(tableName: string): {
+    createTableFunction: () => Promise<Response>;
+    insertDataFunction: (data: any[]) => Promise<Response>;
+    fetchDataFunction: (
+      lastValue: any,
+      pageSize: number,
+    ) => Promise<any[] | null>;
+    formatDataFunction: (data: any[]) => any[];
+    paginationField: string;
+  } {
+    let createTableFunction;
+    let insertDataFunction;
+    let fetchDataFunction;
+    let formatDataFunction;
+    let paginationField;
+
+    switch (tableName) {
+      case "sourcify_matches":
+        createTableFunction =
+          this.duneTableClient.createSourcifyMatchesTable.bind(
+            this.duneTableClient,
+          );
+        insertDataFunction = this.duneDataClient.insertSourcifyMatches.bind(
+          this.duneDataClient,
+        );
+        fetchDataFunction = fetchSourcifyMatches;
+        formatDataFunction = formatSourcifyMatches;
+        paginationField = "id";
+        break;
+      case "verified_contracts":
+        createTableFunction =
+          this.duneTableClient.createVerifiedContractsTable.bind(
+            this.duneTableClient,
+          );
+        insertDataFunction = this.duneDataClient.insertVerifiedContracts.bind(
+          this.duneDataClient,
+        );
+        fetchDataFunction = fetchVerifiedContracts;
+        formatDataFunction = formatVerifiedContracts;
+        paginationField = "id";
+        break;
+      case "compiled_contracts":
+        createTableFunction =
+          this.duneTableClient.createCompiledContractsTable.bind(
+            this.duneTableClient,
+          );
+        insertDataFunction = this.duneDataClient.insertCompiledContracts.bind(
+          this.duneDataClient,
+        );
+        fetchDataFunction = fetchCompiledContracts;
+        formatDataFunction = formatCompiledContracts;
+        paginationField = "id";
+        break;
+      case "compiled_contracts_sources":
+        createTableFunction =
+          this.duneTableClient.createCompiledContractsSourcesTable.bind(
+            this.duneTableClient,
+          );
+        insertDataFunction =
+          this.duneDataClient.insertCompiledContractsSources.bind(
+            this.duneDataClient,
+          );
+        fetchDataFunction = fetchCompiledContractsSources;
+        formatDataFunction = formatCompiledContractsSources;
+        paginationField = "id";
+        break;
+      case "sources":
+        createTableFunction = this.duneTableClient.createSourcesTable.bind(
+          this.duneTableClient,
+        );
+        insertDataFunction = this.duneDataClient.insertSources.bind(
+          this.duneDataClient,
+        );
+        fetchDataFunction = fetchSources;
+        formatDataFunction = formatSources;
+        paginationField = "source_hash";
+        break;
+      case "contracts":
+        createTableFunction = this.duneTableClient.createContractsTable.bind(
+          this.duneTableClient,
+        );
+        insertDataFunction = this.duneDataClient.insertContracts.bind(
+          this.duneDataClient,
+        );
+        fetchDataFunction = fetchContracts;
+        formatDataFunction = formatContracts;
+        paginationField = "id";
+        break;
+      case "contract_deployments":
+        createTableFunction =
+          this.duneTableClient.createContractDeploymentsTable.bind(
+            this.duneTableClient,
+          );
+        insertDataFunction = this.duneDataClient.insertContractDeployments.bind(
+          this.duneDataClient,
+        );
+        fetchDataFunction = fetchContractDeployments;
+        formatDataFunction = formatContractDeployments;
+        paginationField = "id";
+        break;
+      case "code":
+        createTableFunction = this.duneTableClient.createCodeTable.bind(
+          this.duneTableClient,
+        );
+        insertDataFunction = this.duneDataClient.insertCode.bind(
+          this.duneDataClient,
+        );
+        fetchDataFunction = fetchCode;
+        formatDataFunction = formatCode;
+        paginationField = "code_hash";
+        break;
+      default:
+        throw new Error(`Unknown table: ${tableName}`);
+    }
+
+    return {
+      createTableFunction,
+      insertDataFunction,
+      fetchDataFunction,
+      formatDataFunction,
+      paginationField,
+    };
+  }
+
+  private async syncTableData(tableName: string) {
+    const {
+      createTableFunction,
+      insertDataFunction,
+      fetchDataFunction,
+      formatDataFunction,
+      paginationField,
+    } = this.getTableFunctions(tableName);
+
+    console.log(`[${tableName}] Creating table`);
+    const tableResponse = await createTableFunction();
+    const response = await tableResponse.json();
+
+    if (response.message === "Table already existed and matched the request") {
+      console.log(`[${tableName}] Table already exists`);
+    } else {
+      console.log(`[${tableName}] Table created`);
+    }
+
+    console.log(
+      `[${tableName}] Sleeping for 60 seconds (to avoid Dune cache errors)...`,
+    );
+    await new Promise((resolve) => setTimeout(resolve, 60000));
+
+    const totalRows = await countTotalRows(tableName);
+    if (!totalRows || totalRows === 0) {
+      console.error(`[${tableName}] No rows found`);
+      return;
+    }
+    console.log(`[${tableName}] Total rows to insert: ${totalRows}`);
+
+    const pageSize = 300;
+    let syncedResults = 0;
+    let resultsCount = pageSize;
+    let lastValue = undefined;
+
+    while (resultsCount === pageSize) {
+      const data = await fetchDataFunction(lastValue, pageSize);
+      if (!data) {
+        console.error(`[${tableName}] No data found`);
+        return;
+      }
+
+      resultsCount = data.length;
+      syncedResults += resultsCount;
+      lastValue = data[data.length - 1][paginationField];
+
+      const formattedData = formatDataFunction(data);
+
+      const dataResponse = await insertDataFunction(formattedData);
+      if (dataResponse.status !== 200) {
+        switch (dataResponse.status) {
+          case 402: {
+            const minutesToSleep = 5;
+            console.error(
+              `[${tableName}] Api limit exceeded. Sleeping for ${minutesToSleep} minutes...`,
+            );
+            await new Promise((resolve) =>
+              setTimeout(resolve, minutesToSleep * 60 * 1000),
+            );
+            break;
+          }
+          default:
+            console.error(
+              `[${tableName}] Error inserting: ${dataResponse.status}`,
+            );
+            console.error(
+              `[${tableName}] Error body: ${JSON.stringify(
+                await dataResponse.json(),
+              )}`,
+            );
+        }
+      } else {
+        const insertResponse = await dataResponse.json();
+
+        console.log(
+          `[${tableName}] Inserted on Dune: ${insertResponse.rows_written} rows.`,
+        );
+      }
+
+      const percentage = (syncedResults / totalRows) * 100;
+      console.log(
+        `[${tableName}] Progress: ${syncedResults}/${totalRows} (${percentage.toFixed(2)}%)`,
+      );
+    }
+  }
+
+  async syncAll() {
+    await this.syncTableData("code");
+    await this.syncTableData("compiled_contracts");
+    await this.syncTableData("sources");
+    await this.syncTableData("compiled_contracts_sources");
+    await this.syncTableData("contracts");
+    await this.syncTableData("contract_deployments");
+    await this.syncTableData("verified_contracts");
+    await this.syncTableData("sourcify_matches");
+  }
+}
