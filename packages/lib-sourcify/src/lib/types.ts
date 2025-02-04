ef8399936712dcaed5ca7f8796d05acc2d6d3998
Marco Castignoli <marco@castignoli.it>
2025-02-04 12:23:11 +0100

    restore old `types.ts`

--- packages/lib-sourcify/src/lib/types.ts
+++ packages/lib-sourcify/src/lib/types.ts
@@ -1,11 +1,6 @@
 import { Abi } from 'abitype';
-import { FetchRequest } from 'ethers';
 import SourcifyChain from '../SourcifyChain';
-import {
-  Transformation,
-  TransformationValues,
-} from '../Verification/Transformations';
-
+import { FetchRequest } from 'ethers';
 export interface PathBuffer {
   path: string;
   buffer: Buffer;
@@ -178,6 +173,77 @@ export interface RecompilationResult {
   immutableReferences: ImmutableReferences;
   creationLinkReferences: LinkReferences;
   runtimeLinkReferences: LinkReferences;
+}
+
+export type Transformation = {
+  type: 'insert' | 'replace';
+  reason:
+    | 'constructorArguments'
+    | 'library'
+    | 'immutable'
+    | 'cborAuxdata'
+    | 'callProtection';
+  offset: number;
+  id?: string;
+};
+
+// Call protection is always at the start of the runtime bytecode
+export const CallProtectionTransformation = (): Transformation => ({
+  type: 'replace',
+  reason: 'callProtection',
+  offset: 1, // 1 byte is always the PUSH20 opcode 0x73
+});
+
+// TransformationValues only has one ConstructorTransformatino so no id field is needed
+export const ConstructorTransformation = (offset: number): Transformation => ({
+  type: 'insert',
+  reason: 'constructorArguments',
+  offset,
+});
+
+export const AuxdataTransformation = (
+  offset: number,
+  id: string,
+): Transformation => ({
+  type: 'replace',
+  reason: 'cborAuxdata',
+  offset,
+  id,
+});
+
+export const LibraryTransformation = (
+  offset: number,
+  id: string,
+): Transformation => ({
+  type: 'replace',
+  reason: 'library',
+  offset,
+  id,
+});
+
+export const ImmutablesTransformation = (
+  offset: number,
+  id: string,
+  type: 'replace' | 'insert',
+): Transformation => ({
+  type,
+  reason: 'immutable',
+  offset,
+  id,
+});
+
+export interface TransformationValues {
+  constructorArguments?: string;
+  callProtection?: string;
+  libraries?: {
+    [id: string]: string;
+  };
+  immutables?: {
+    [id: string]: string;
+  };
+  cborAuxdata?: {
+    [id: string]: string;
+  };
 }
 
 export interface Match {
