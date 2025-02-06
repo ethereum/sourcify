d8a1eb4c765dcc301cf282154d7e412621f538eb
Marco Castignoli <marco@castignoli.it>
2025-02-06 17:43:44 +0100

    Refactor error handling and type definitions in Sourcify library

--- packages/lib-sourcify/src/Verification/VerificationTypes.ts
+++ packages/lib-sourcify/src/Verification/VerificationTypes.ts
@@ -16,22 +16,20 @@ export interface BytecodeMatchingResult {
   message?: string;
 }
 
-export enum VerificationErrorCode {
-  CHAIN_UNAVAILABLE = 1001,
-  CONTRACT_NOT_DEPLOYED = 1002,
-  COMPILED_BYTECODE_IS_ZERO = 1003,
-  EXTRA_FILE_INPUT_BUG = 1005,
-  FAILED_TO_CHECK_EXTRA_FILE_INPUT_BUG = 1006,
-  CREATION_BYTECODE_MATCH_ERROR = 1007,
-  NO_MATCH = 1008,
-  ONCHAIN_RUNTIME_BYTECODE_NOT_AVAILABLE = 1009,
-  ONCHAIN_CREATION_BYTECODE_NOT_AVAILABLE = 1010,
-}
+export type VerificationErrorCode =
+  | 'CHAIN_UNAVAILABLE'
+  | 'CONTRACT_NOT_DEPLOYED'
+  | 'COMPILED_BYTECODE_IS_ZERO'
+  | 'EXTRA_FILE_INPUT_BUG'
+  | 'FAILED_TO_CHECK_EXTRA_FILE_INPUT_BUG'
+  | 'CREATION_BYTECODE_MATCH_ERROR'
+  | 'NO_MATCH'
+  | 'ONCHAIN_RUNTIME_BYTECODE_NOT_AVAILABLE'
+  | 'ONCHAIN_CREATION_BYTECODE_NOT_AVAILABLE';
 
 export class VerificationError extends SourcifyLibError {
   constructor(message: string, code: VerificationErrorCode) {
     super(message, code);
-    this.name = 'VerificationError';
   }
 }
 
