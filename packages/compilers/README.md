d79d44d351d2fd315e78ee5588b5c0029720dece
Manuel Wedler <manuel@wedler.dev>
2025-02-07 13:01:14 +0100

    Add content to compilers package

---
+++ packages/compilers/README.md
@@ -0,0 +1,11 @@
+# compilers
+
+Wrapper around multiple compilers to download the right version and invoke the compilation with a common interface.
+
+## Install
+
+```
+npm i @ethereum-sourcify/compilers
+```
+
+## Usage
---
+++ packages/compilers/src/lib/common.ts
@@ -0,0 +1,91 @@
+import { exec } from 'child_process';
+import { logDebug, logError, logSilly } from '../logger';
+
+/**
+ * Fetches a resource with an exponential timeout.
+ * 1) Send req, wait backoff * 2^0 ms, abort if doesn't resolve
+ * 2) Send req, wait backoff * 2^1 ms, abort if doesn't resolve
+ * 3) Send req, wait backoff * 2^2 ms, abort if doesn't resolve...
+ * ...
+ * ...
+ */
+export async function fetchWithBackoff(
+  resource: string,
+  backoff: number = 10000,
+  retries: number = 4,
+) {
+  let timeout = backoff;
+
+  for (let attempt = 0; attempt <= retries; attempt++) {
+    try {
+      logSilly('Start fetchWithBackoff', { resource, timeout, attempt });
+      const controller = new AbortController();
+      const id = setTimeout(() => {
+        logDebug('Aborting request', { resource, timeout, attempt });
+        controller.abort();
+      }, timeout);
+      const response = await fetch(resource, {
+        signal: controller.signal,
+      });
+      logSilly('Success fetchWithBackoff', { resource, timeout, attempt });
+      clearTimeout(id);
+      return response;
+    } catch (error) {
+      if (attempt === retries) {
+        logError('Failed fetchWithBackoff', {
+          resource,
+          attempt,
+          retries,
+          timeout,
+          error,
+        });
+        throw new Error(`Failed fetching ${resource}: ${error}`);
+      } else {
+        timeout *= 2; // exponential backoff
+        logDebug('Retrying fetchWithBackoff', {
+          resource,
+          attempt,
+          timeout,
+          error,
+        });
+        continue;
+      }
+    }
+  }
+  throw new Error(`Failed fetching ${resource}`);
+}
+
+export function asyncExec(
+  command: string,
+  inputStringified: string,
+  maxBuffer: number,
+): Promise<string> {
+  // check if input is valid JSON. The input is untrusted and potentially cause arbitrary execution.
+  JSON.parse(inputStringified);
+
+  return new Promise((resolve, reject) => {
+    const child = exec(
+      command,
+      {
+        maxBuffer,
+      },
+      (error, stdout, stderr) => {
+        if (error) {
+          reject(error);
+        } else if (stderr) {
+          reject(
+            new Error(`Compiler process returned with errors:\n ${stderr}`),
+          );
+        } else {
+          resolve(stdout);
+        }
+      },
+    );
+    if (!child.stdin) {
+      throw new Error('No stdin on child process');
+    }
+    // Write input to child process's stdin
+    child.stdin.write(inputStringified);
+    child.stdin.end();
+  });
+}
