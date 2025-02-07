7564f70d34b7108f16b9288ddc7112cbad912462
Manuel Wedler <manuel@wedler.dev>
2025-02-07 13:01:14 +0100

    Add content to compilers package

---
+++ packages/compilers/src/index.ts
@@ -0,0 +1,8 @@
+import { setLogger, setLevel, ILogger } from './logger';
+export const setCompilersLogger = setLogger;
+export const setCompilersLoggerLevel = setLevel;
+export type ICompilersLogger = ILogger;
+
+export * from './lib/solidityCompiler';
+export * from './lib/vyperCompiler';
+export * from './lib/compilerWorker';
