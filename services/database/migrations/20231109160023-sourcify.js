1613e8e4378850b5aa9e8427eeec9869b479f595
Manuel Wedler <manuel@wedler.dev>
2025-02-18 17:19:22 +0100

    Add onchain bytecodes to verification_jobs_ephemeral table

--- services/database/migrations/20231109160023-sourcify.js
+++ services/database/migrations/20231109160023-sourcify.js
@@ -100,6 +100,8 @@ exports.up = function (db, callback) {
             id uuid NOT NULL DEFAULT gen_random_uuid(),
             recompiled_creation_code bytea,
             recompiled_runtime_code bytea,
+            onchain_creation_code bytea,
+            onchain_runtime_code bytea,
             creator_transaction_hash bytea,
             CONSTRAINT verification_jobs_ephemeral_pkey PRIMARY KEY (id),
             CONSTRAINT verification_jobs_ephemeral_id_fk FOREIGN KEY (id) REFERENCES verification_jobs(id) ON DELETE CASCADE ON UPDATE CASCADE
