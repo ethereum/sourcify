5e2c1608e31e1a2b984494559ebc46312d42aac5
Marco Castignoli <marco@castignoli.it>
2025-03-20 14:14:09 +0100

    fix on conflict clause for insertContractDeployment

--- services/server/src/server/services/utils/Database.ts
+++ services/server/src/server/services/utils/Database.ts
@@ -484,7 +484,7 @@ ${
         block_number,
         transaction_index,
         deployer
-      ) VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFLICT (chain_id, address, transaction_hash, contract_id) DO NOTHING RETURNING *`,
+      ) VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFLICT ON CONSTRAINT contract_deployments_pseudo_pkey DO NOTHING RETURNING *`,
       [
         chain_id,
         address,
