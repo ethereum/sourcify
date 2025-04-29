35c62b2c5e03d572caab019807879dd00e9cd850
Manuel Wedler <manuel@wedler.dev>
2025-04-29 15:04:02 +0200

    Add deprecation header to API v1 endpoints

--- services/server/src/server/apiv1/routes.ts
+++ services/server/src/server/apiv1/routes.ts
@@ -1,4 +1,4 @@
-import { Router } from "express"; // static is a reserved word
+import { Router, Request, Response, NextFunction } from "express";
 import testArtifactsRoutes from "./testartifacts/testartifacts.routes";
 import repositoryRoutes from "./repository/repository.routes";
 import sessionStateRoutes from "./verification/session-state/session-state.routes";
@@ -8,12 +8,29 @@ import etherscanRoutes from "./verification/etherscan/etherscan.routes";
 import vyperRoutes from "./verification/vyper/vyper.routes";
 import { checksumAddresses } from "./controllers.common";
 import privateRoutes from "./verification/private/private.routes";
+
 const router: Router = Router();
 
 // checksum addresses in every request
 router.use(checksumAddresses);
 
 router.use("/chain-tests", testArtifactsRoutes);
+
+// Add deprecation header to all API v1 responses
+router.use(
+  [
+    "/verify",
+    "/repository",
+    "/check-all-by-addresses",
+    "/check-by-addresses",
+    "/files",
+    "/session",
+  ],
+  (req: Request, res: Response, next: NextFunction) => {
+    res.setHeader("Deprecation", "true");
+    next();
+  },
+);
 
 router.use("/", repositoryRoutes);
 router.use("/", sessionStateRoutes);
