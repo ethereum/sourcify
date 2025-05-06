9003a64960354ce420f1e61624a208feb1db0686
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2025-05-06 15:15:40 +0200

    Update the release script (#2139)
    * Stop versioning the monorepo
    
    * Update the release script to do the tagging and releasing on a separate branch
    
    * Allow lerna version on release branches
    
    * Change lerna test command to fix tests
    
    * Fix missing function

--- scripts/release/main.sh
+++ scripts/release/main.sh
@@ -16,37 +16,28 @@ source "${SCRIPT_DIR}/release.sh"
 ###
 ### Main
 ###
-prompt_execute_or_skip "checking current branch" check_current_branch
+prompt_execute_or_skip "checking current branch" is_on_staging_branch
 prompt_execute_or_skip "checking if branches are in sync" check_branch_sync
-prompt_execute_or_skip "creating GitHub PR" create_gh_pr
+prompt_execute_or_skip "creating GitHub PR" create_gh_deploy_pr
 
 # Needed if the user skips creating the PR
 if [ -z "$OPEN_PR_NUMBER" ]; then
   OPEN_PR_NUMBER=$(gh pr list --base master --state open | grep staging | awk '{print $1}')
 fi
 
-# Define arrays to hold package directories, names, and versions. These are global and needed
-declare -a directories
-declare -a packageNames
-declare -a versions
-declare -A selected_versions
-
-check_packages_for_update # Must run this because it sets the above variables
-
-prompt_execute_or_skip "creating new branch because we can't commit to staging directly" create_new_branch "commit-changelogs-$(date +'%Y-%m-%d')"
+prompt_execute_or_skip "creating new branch because we can't commit to staging directly" create_new_branch "release-$(date +'%Y-%m-%d-%H-%M')" # Add hour and minute to the branch name if we do multiple releases in the same day
 prompt_execute_or_skip "choosing versions for each package and writing changelogs" update_packages_and_changelog
 prompt_execute_or_skip "commiting the changelogs" commit_changelogs
-prompt_execute_or_skip "publishing the branch" publish_branch "commit-changelogs-$(date +'%Y-%m-%d')"
-prompt_execute_or_skip "opening PR to staging" open_pr_to_staging "commit-changelogs-$(date +'%Y-%m-%d')" "Update changelogs"
 
-prompt_execute_or_skip "asking if changelogs PR is merged" ask_if_changelog_pr_merged
+## Run lerna version and create git tags
+prompt_execute_or_skip "choosing same versions in lerna to create git tags and updating dependencies" run_lerna_version
+prompt_execute_or_skip "publishing the branch" publish_branch_and_open_pr_to_staging
+
+prompt_execute_or_skip "asking if release PR is merged" ask_if_release_pr_merged
+prompt_execute_or_skip "pushing the tags to GitHub" push_tags_in_order
+prompt_execute_or_skip "creating GitHub releases" create_github_releases
+
 prompt_execute_or_skip "switching to staging and pulling latest" switch_to_staging_and_pull
-
 prompt_execute_or_skip "switching to master" switch_to_master
 prompt_execute_or_skip "merging staging to master locally" merge_locally
-
-prompt_execute_or_skip "choosing same versions in lerna to create git tags and updating dependencies" run_lerna_version
-prompt_execute_or_skip "pushing the tags to GitHub" push_tags_in_order
 prompt_execute_or_skip "pushing the commits to master" push_to_master
-prompt_execute_or_skip "creating GitHub releases" create_github_releases
-prompt_execute_or_skip "creating a PR to merge the new versions to the staging branch" open_pr_to_staging master "Update new package versions in master"
