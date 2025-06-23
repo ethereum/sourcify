95ab62fde593a56ed5a29f7722ec8a964d54b471
Max Wickham <maxwickham@startmail.com>
2025-06-23 15:19:38 +0300

    Update comments

--- services/server/src/config/default.js
+++ services/server/src/config/default.js
@@ -55,5 +55,5 @@ module.exports = {
   // verify-deprecated endpoint used in services/database/scripts.mjs. Used when recreating the DB with deprecated chains that don't have an RPC.
   verifyDeprecated: false,
   upgradeContract: false,
-  throwOnPartialMatch: true, // If true, will not throw an error if contract already verified.
+  throwOnPartialMatch: true, // If true, will throw an error if contract already verified and stored in the database.
 };
